1d4f7f8f1db7adb860b4f4836193115212a6a9cb
Angel <atstojanov@gmail.com>
2020-10-16 00:12:09 +0300

    Remove splitNames utility

--- src/utils/splitNames.ts
+++
@@ -1,7 +0,0 @@
-export const splitNames = (fullName?: string) => {
-  if (!fullName) return { firstName: '', lastName: '' };
-  return {
-    firstName: fullName.split(' ')[0],
-    lastName: fullName.split(' ')[1],
-  };
-};
