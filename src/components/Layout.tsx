b7ad4a595bde7de9fa06da07c922a506eaac2554
Angel Stoyanov <atstojanov@gmail.com>
2020-10-30 14:23:16 +0200

    Fix layout header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,18 +6,20 @@ import Navigation from './Navigation';
 
 export const Layout: FC = ({ children }) => {
   return (
-    <Container fluid>
+    <>
       <Header />
-      <ErrorHandler>
-        {/* <ProgressBar now={60} style={{ height: '1px' }} /> */}
-        <Row>
-          <Col sm={1} className="ct-nav">
-            <Navigation />
-          </Col>
-          <Col>{children ? children : <div></div>}</Col>
-        </Row>
-      </ErrorHandler>
-    </Container>
+      <Container fluid>
+        <ErrorHandler>
+          {/* <ProgressBar now={60} style={{ height: '1px' }} /> */}
+          <Row>
+            <Col sm={1} className="ct-nav">
+              <Navigation />
+            </Col>
+            <Col>{children ? children : <div></div>}</Col>
+          </Row>
+        </ErrorHandler>
+      </Container>
+    </>
   );
 };
 
