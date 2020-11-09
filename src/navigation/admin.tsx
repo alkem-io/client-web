15692a67bd6259bdad8e1057c13cfbefd17d22ef
Angel <atstojanov@gmail.com>
2020-11-09 17:41:15 +0200

    Resolve PR comments

--- src/navigation/admin.tsx
+++ src/navigation/admin.tsx
@@ -1,15 +1,12 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 /*lib imports end*/
 import { AdminLayout, AdminPage, GroupPage, UserPage } from '../components/Admin';
-import { useUpdateNavigation } from '../hooks/useNavigation';
 import { FourOuFour } from '../pages';
 /*local files imports end*/
 
 export const Admin: FC = () => {
   const { path, url } = useRouteMatch();
-  // const currentPaths = useMemo(() => [{ value: url, name: 'admin', real: true }], []);
-  // useUpdateNavigation({ currentPaths });
 
   return (
     <AdminLayout>
