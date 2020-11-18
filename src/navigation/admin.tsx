94c6c915121de238649ad3de6c254440d8256869
olegsubik <olegsubik@gmail.com>
2020-11-18 16:46:08 +0200

    Revert "Trailing spaces removed from url automatically"
    This reverts commit ed677f2cf79936e542cc8cef153e5da52a1e81ff.

--- src/navigation/admin.tsx
+++ src/navigation/admin.tsx
@@ -26,6 +26,9 @@ export const Admin: FC = () => {
   return (
     <Container>
       <Switch>
+        <Route exact strict path={`${path}/`}>
+          <Redirect to={`${path}`} />
+        </Route>
         <Route exact path={`${path}`}>
           <AdminPage paths={currentPaths} />
         </Route>
@@ -55,6 +58,9 @@ const UsersRoute: FC<PageProps> = ({ paths }) => {
   }
   return (
     <Switch>
+      <Route exact strict path={`${path}/`}>
+        <Redirect to={`${path}`} />
+      </Route>
       <Route exact path={`${path}`}>
         <UserList users={users} paths={currentPaths} />
       </Route>
@@ -111,6 +117,9 @@ const GroupsRoute: FC<PageProps> = ({ paths }) => {
 
   return (
     <Switch>
+      <Route exact strict path={`${path}/`}>
+        <Redirect to={`${path}`} />
+      </Route>
       <Route exact path={`${path}`}>
         <h3>Ecoverse/Challenges</h3>
         <ListPage data={challenges} paths={currentPaths} />
@@ -162,6 +171,9 @@ const ChallengesRoute: FC<ChallengesRouteProps> = ({ data, paths }) => {
 
   return (
     <Switch>
+      <Route exact strict path={`${path}/`}>
+        <Redirect to={`${path}`} />
+      </Route>
       <Route exact path={`${path}`}>
         <ListPage paths={currentPaths} data={groupsData.groups} />
       </Route>
