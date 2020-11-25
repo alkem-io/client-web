2e18db44ad78078db27aac877200a198bd317426
Angel <atstojanov@gmail.com>
2020-11-25 18:01:16 +0200

    Generate .env.base dynamically

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
+const { series, parallel } = require('gulp');
 const dotenvFlow = require('dotenv-flow');
 const dotenvExpand = require('dotenv-expand');
 fs = require('fs');
+path = require('path');
 
 const CONFIG_TEXT = `window._env_ = `;
 const CONFIG_FILE_NAME = 'env-config.js';
 
-const buildConfiguration = cb => {
+function buildConfiguration (cb) {
+
   const initialConfig = dotenvFlow.config();
   dotenvExpand(initialConfig);
 
   const env = process.env;
 
   let configuration = {};
-  const nodeEnv= env.NODE_ENV ? env.NODE_ENV : "development";
+  const nodeEnv = env.NODE_ENV ? env.NODE_ENV : 'development';
 
   console.info(`Building for : '${nodeEnv}'`);
 
@@ -26,6 +29,13 @@ const buildConfiguration = cb => {
   configuration['REACT_APP_GRAPHQL_ENDPOINT'] =
     configuration['REACT_APP_GRAPHQL_ENDPOINT'] ||
     (nodeEnv === 'production' ? '/graphql' : 'http://localhost:4000/graphql');
+
+  let envBase = fs.createWriteStream(path.join(__dirname, '.env.deployment', '.env.base'), { flags: 'w' });
+
+  Object.keys(configuration).forEach(k => {
+    envBase.write(`${k}=${configuration[k]}\n`);
+  });
+  envBase.end();
 
   fs.writeFile(`./public/${CONFIG_FILE_NAME}`, `${CONFIG_TEXT}${JSON.stringify(configuration, null, 2)}`, cb);
 };
