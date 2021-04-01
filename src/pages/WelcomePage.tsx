37e559c7c1b8f67e4f50f0bd60b2b395565d424e
Angel <atstojanov@gmail.com>
2021-04-01 16:40:29 +0300

    Update welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,49 +1,89 @@
 import React, { FC, useMemo } from 'react';
 import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Section, { Header, SubHeader } from '../components/core/Section';
 import Typography from '../components/core/Typography';
 import { useEcoverse } from '../hooks/useEcoverse';
 import { useUpdateNavigation } from '../hooks/useNavigation';
+import { LOGO_REFERNCE_NAME } from '../models/Constants';
 
 interface WelcomePageProps {}
 
 export const WelcomePage: FC<WelcomePageProps> = () => {
   const { ecoverse } = useEcoverse();
+
   const currentPaths = useMemo(() => [], []);
   useUpdateNavigation({ currentPaths });
+
+  const ecoverseLogo = ecoverse?.ecoverse.context?.references?.find(ref => ref.name === LOGO_REFERNCE_NAME)?.uri;
+  const name = ecoverse?.ecoverse.name;
+  const tagline = ecoverse?.ecoverse.context?.tagline;
   return (
     <Container>
+      <Section
+        avatar={
+          ecoverseLogo ? (
+            <img
+              src={ecoverseLogo}
+              alt={`${name} logo`}
+              style={{ maxWidth: 320, height: 'initial', margin: '0 auto' }}
+            />
+          ) : (
+            <div />
+          )
+        }
+        hideDetails
+      >
+        <Header text={name} />
+        <SubHeader text={tagline} />
+      </Section>
       <div>
-        <Typography variant={'h2'}>Welcome to the '{ecoverse?.ecoverse?.name}' ecoverse!</Typography>
-
-        <Typography variant={'h3'}>{ecoverse?.ecoverse?.context?.tagline}</Typography>
-
-        <Typography variant={'h5'}>{ecoverse?.ecoverse?.context?.background}</Typography>
+        <Typography variant={'h4'}>
+          Thank you for signing up - you are now a registered user on the platform and can explore the challenges hosted
+          here further.
+        </Typography>
 
         <Typography>
-          In order to get the most value please:
+          Suggested next steps:
           <ul>
             <li>
-              Browse: Have a look through the public information from the hosted challenges, seeing which ones trigger
-              your interest...
+              <Link to={'/'}>
+                <strong>Browse:</strong>
+              </Link>{' '}
+              Review the set of hosted Challenges , as well as the Opportunities within each Challenge.
             </li>
             <li>
-              Profile: Complete your profile [link to profile edit page], this makes it easier for others to find you,
-              and will be used when applying to join various communities.
+              Complete your{' '}
+              <Link to={'/profile'}>
+                <strong>profile:</strong>
+              </Link>{' '}
+              This makes it easier for others to find you, and your profile is also used when applying to join
+              communities.
             </li>
-            <li>Join: Apply to become a full member of the ecoverse [link to ecoverse application page]</li>
             <li>
-              Contribute: After browsing the Challenges and Opportunites, join those that you want to contribute to and
-              apply to join them.
+              <Link to={'/apply'}>
+                <strong>Apply</strong>
+              </Link>{' '}
+              to become a member of the Ecoverse: your application is then reviewed based on your profile and
+              application information.
             </li>
             <li>
-              Use the search [link to search] functionality to find other relevant members , groups, organisations.
+              <Link to={'/community'}>
+                <strong>Search</strong>
+              </Link>{' '}
+              for relevant Challenges and Opportunities
             </li>
           </ul>
         </Typography>
 
-        <Typography>As with any community, the more everyone brings in the more everyone gets out!</Typography>
+        <Typography>
+          Once you are a member of the Ecoverse then there are additional options: * Contribute: After browsing the
+          Challenges and Opportunites, join those that you want to contribute to and apply to join them. * Connect to
+          other members [link to search] functionality to find other relevant members , groups, organisations.
+        </Typography>
 
-        <Typography>We really hope you enjoy engaging.</Typography>
+        <Typography>As with any community, the more everyone brings in the more everyone gets out!</Typography>
+        <Typography>We really hope you enjoy engaging!</Typography>
       </div>
     </Container>
   );
