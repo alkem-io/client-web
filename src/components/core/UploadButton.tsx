2d0102389a467cc1f74a025d7e02c8bc0c53b050
Angel <atstojanov@gmail.com>
2021-04-05 14:51:05 +0300

    Image and UploadButton components

---
+++ src/components/core/UploadButton.tsx
@@ -0,0 +1,38 @@
+import React, { ChangeEvent, ChangeEventHandler, FC, useCallback, useRef } from 'react';
+import Button from './Button';
+
+interface UploadButtonProps extends Record<string, unknown> {
+  onChange?: ChangeEventHandler;
+  accept?: string;
+}
+
+export const UploadButton: FC<UploadButtonProps> = ({ onChange, children, ...props }) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    if (ref && ref.current) {
+      ref.current.value = '';
+      ref.current.click();
+    }
+  };
+
+  const handleChange = useCallback(
+    (e: ChangeEvent) => {
+      if (onChange) {
+        onChange(e);
+      }
+    },
+    [onChange]
+  );
+
+  return (
+    <>
+      <Button onClick={handleButtonClick} {...props}>
+        {children}
+      </Button>
+      <input ref={ref} type="file" style={{ display: 'none' }} onChange={handleChange} />
+    </>
+  );
+};
+
+export default UploadButton;
