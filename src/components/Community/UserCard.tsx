de68a9b024f7819e44360dfc173fdcd2177667b2
hero101 <svetoslavp_23@abv.bg>
2021-07-08 15:54:50 +0300

    search: user card visual improvements

--- src/components/Community/UserCard.tsx
+++ src/components/Community/UserCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo, useState } from 'react';
+import React, { FC, memo, useMemo, useState } from 'react';
 import { Theme } from '../../context/ThemeProvider';
 import { createStyles } from '../../hooks/useTheme';
 import { useUserCardMetadata } from '../../hooks/useUserCardMetadata';
@@ -8,6 +8,9 @@ import Avatar from '../core/Avatar';
 import Card from '../core/Card';
 import { Loading } from '../core/Loading';
 import UserPopUp from './UserPopUp';
+import Tag from '../core/Tag';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import TagContainer from '../core/TagContainer';
 
 export interface UserCardProps extends User {
   terms?: Array<string>;
@@ -26,41 +29,86 @@ const userCardStyles = createStyles(theme => ({
   bgText: {
     color: hexToRGBA(theme.palette.neutral, 0.5),
   },
+  relative: {
+    position: 'relative',
+  },
+  divCentered: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: theme.shape.spacing(1),
+  },
+  section: {
+    padding: `${theme.shape.spacing(1)}px ${theme.shape.spacing(3)}px`,
+  },
+  body: {
+    flexGrow: 0,
+  },
 }));
 
-const UserCardInner: FC<UserCardProps> = ({ displayName: name, terms, id }) => {
+const UserCardInner: FC<UserCardProps> = ({ displayName, terms, id }) => {
   const [isPopUpShown, setIsModalShown] = useState<boolean>(false);
   const styles = userCardStyles();
 
   const { user: userMetadata, loading } = useUserCardMetadata(id);
 
-  const role = userMetadata?.roles[0]?.name || 'Registered';
+  const roleName = userMetadata?.roles[0]?.name;
+  const tagProps = userMetadata?.roles[0]?.hidden ? undefined : { text: roleName };
+
+  const tags = userMetadata?.user?.profile?.tagsets?.flatMap(x => x.tags) || [];
+  const truncatedTags = useMemo(() => tags.slice(0, 3), [tags]);
 
   const avatar = userMetadata?.user.profile?.avatar;
 
   if (loading) return <Loading text={''} />;
   return (
-    <Card
-      bodyProps={{
-        classes: {
-          background: (theme: Theme) => theme.palette.neutralLight,
-        },
-      }}
-      primaryTextProps={{
-        text: name,
-        classes: {
-          lineHeight: '36px',
-        },
-      }}
-      tagProps={{ text: role }}
-      matchedTerms={{ terms }}
-      bgText={{ text: 'user' }}
-      className={styles.card}
-      onClick={() => !isPopUpShown && setIsModalShown(true)}
-    >
-      {avatar && <Avatar size="lg" src={avatar} />}
-      {isPopUpShown && <UserPopUp id={id} onHide={() => setIsModalShown(false)} terms={terms || []} />}
-    </Card>
+    <div className={styles.relative}>
+      <Card
+        className={styles.card}
+        bodyProps={{
+          classes: {
+            background: (theme: Theme) => theme.palette.background,
+            padding: (theme: Theme) =>
+              `${theme.shape.spacing(4)}px ${theme.shape.spacing(3)}px ${theme.shape.spacing(1)}px`,
+          },
+          className: styles.body,
+        }}
+        primaryTextProps={{
+          text: (
+            <div className={styles.divCentered}>
+              {avatar && <Avatar size="md" src={avatar} />}
+              {displayName}
+            </div>
+          ),
+        }}
+        sectionProps={{
+          children: (
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <TagContainer>
+                {truncatedTags.map((t, i) => (
+                  <Tag key={i} text={t} color="neutralMedium" />
+                ))}
+                {tags.length > 3 && (
+                  <OverlayTrigger
+                    placement={'right'}
+                    overlay={<Tooltip id={'more-tags'}>{tags.slice(3).join(', ')}</Tooltip>}
+                  >
+                    <span>
+                      <Tag text={<>{`+ ${tags.length - truncatedTags.length} more`}</>} color="neutralMedium" />
+                    </span>
+                  </OverlayTrigger>
+                )}
+              </TagContainer>
+            </div>
+          ),
+          className: styles.section,
+        }}
+        tagProps={tagProps}
+        matchedTerms={{ terms }}
+        onClick={() => !isPopUpShown && setIsModalShown(true)}
+      >
+        {isPopUpShown && <UserPopUp id={id} onHide={() => setIsModalShown(false)} terms={terms || []} />}
+      </Card>
+    </div>
   );
 };
 
