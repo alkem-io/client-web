a46201af174f84343a39e4974c4f05a418dddc2a
hero101 <svetoslavp_23@abv.bg>
2021-07-08 17:49:45 +0300

    search: user popup visual improvements

--- src/components/Community/UserPopUp.tsx
+++ src/components/Community/UserPopUp.tsx
@@ -1,18 +1,49 @@
-import { ReactComponent as At } from 'bootstrap-icons/icons/at.svg';
-import { ReactComponent as InfoCircle } from 'bootstrap-icons/icons/info-circle.svg';
-import { ReactComponent as People } from 'bootstrap-icons/icons/people.svg';
 import React, { FC } from 'react';
-import { Modal, Table } from 'react-bootstrap';
+import { Modal, OverlayTrigger, Table, Tooltip } from 'react-bootstrap';
 import { createStyles } from '../../hooks/useTheme';
 import { useUserMetadata } from '../../hooks/useUserMetadata';
 import Avatar from '../core/Avatar';
 import Button from '../core/Button';
-import Divider from '../core/Divider';
 import Loading from '../core/Loading';
 import Typography from '../core/Typography';
-import Tags from './Tags';
+import { useTranslation } from 'react-i18next';
+import Tag from '../core/Tag';
+import TagContainer from '../core/TagContainer';
 
 const useUserPopUpStyles = createStyles(theme => ({
+  header: {
+    display: 'flex',
+    gap: theme.shape.spacing(4),
+    alignItems: 'center',
+
+    [theme.media.down('sm')]: {
+      flexWrap: 'wrap',
+      gap: theme.shape.spacing(2),
+    },
+  },
+  profile: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: theme.shape.spacing(1),
+
+    [theme.media.down('sm')]: {
+      gap: 0,
+      flexGrow: 1,
+    },
+  },
+  userName: {
+    whiteSpace: 'nowrap',
+    display: 'flex',
+
+    [theme.media.down('sm')]: {
+      flexGrow: 1,
+      justifyContent: 'center',
+    },
+  },
+  description: {
+    display: 'flex',
+    flexGrow: 1,
+  },
   body: {
     maxHeight: 600,
     overflow: 'auto',
@@ -44,7 +75,8 @@ interface UserPopUpProps {
   terms?: Array<string>;
 }
 
-const UserPopUp: FC<UserPopUpProps> = ({ id, onHide, terms = [] }) => {
+const UserPopUp: FC<UserPopUpProps> = ({ id, onHide }) => {
+  const { t } = useTranslation();
   const styles = useUserPopUpStyles();
 
   const { user: userMetadata, loading } = useUserMetadata(id);
@@ -52,23 +84,36 @@ const UserPopUp: FC<UserPopUpProps> = ({ id, onHide, terms = [] }) => {
 
   const getStringOfNames = arr => arr.join(', ');
 
+  const tags = (userMetadata?.user?.profile?.tagsets || []).flatMap(x => x.tags);
   const groups = userMetadata?.groups || [];
   const challenges = userMetadata?.challenges || [];
   const organizations = userMetadata?.organizations || [];
   const opportunities = userMetadata?.opportunities || [];
 
-  const refs = user?.profile?.references?.filter(r => r.uri.trim() !== '');
+  const noMembership =
+    !(groups && groups.length > 0) &&
+    !(challenges && challenges.length > 0) &&
+    !(organizations && organizations.length > 0) &&
+    !(opportunities && opportunities.length > 0);
 
   return (
     <Modal show={true} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          <div className={'d-flex align-items-center mb-3'}>
-            <Avatar src={user?.profile?.avatar} size={'lg'} />
-            <div className={'ml-3'}>
-              <Typography variant={'h3'}>{user?.displayName}</Typography>
-              <Tags tags={terms} />
+          <div className={styles.header}>
+            <div className={styles.profile}>
+              <Avatar src={user?.profile?.avatar} size={'lg'} />
+              <div className={styles.userName}>
+                <Typography variant={'h3'}>{user?.displayName}</Typography>
+              </div>
             </div>
+            {user?.profile?.description && (
+              <div className={styles.description}>
+                <Typography weight={'medium'} color={'neutral'} as={'p'} clamp={3}>
+                  {user?.profile.description}
+                </Typography>
+              </div>
+            )}
           </div>
         </Modal.Title>
       </Modal.Header>
@@ -77,34 +122,11 @@ const UserPopUp: FC<UserPopUpProps> = ({ id, onHide, terms = [] }) => {
           <Loading text={'Loading user'} />
         ) : (
           <>
-            <Typography weight={'medium'} color={'neutral'} variant={'h4'} className={styles.centeredText}>
-              <InfoCircle width={25} height={25} className={styles.icon} /> General
-            </Typography>
-            {user?.firstName && (
-              <Typography weight={'medium'} color={'neutral'} as={'span'}>
-                {user?.firstName}{' '}
-              </Typography>
-            )}
-            {user?.lastName && (
-              <Typography weight={'medium'} color={'neutral'} as={'span'}>
-                {user?.lastName}{' '}
-              </Typography>
-            )}
-            {user?.gender && (
-              <Typography weight={'medium'} color={'neutral'} as={'span'}>
-                {`(${user?.gender})`}
-              </Typography>
-            )}
-            {user?.profile?.description && (
-              <Typography weight={'medium'} color={'neutral'} as={'p'}>
-                {user?.profile.description}
-              </Typography>
-            )}
-
-            <Divider noPadding />
-            <Typography weight={'medium'} color={'neutral'} variant={'h4'} className={styles.centeredText}>
-              <People width={30} height={30} className={styles.icon} /> Member of
-            </Typography>
+            <TagContainer>
+              {tags.map((t, i) => (
+                <Tag key={i} text={t} color="neutralMedium" />
+              ))}
+            </TagContainer>
             <Table striped bordered hover size="sm" className={styles.table}>
               <thead>
                 <tr>
@@ -157,39 +179,25 @@ const UserPopUp: FC<UserPopUpProps> = ({ id, onHide, terms = [] }) => {
                 )}
               </tbody>
             </Table>
-
-            <Divider noPadding />
-
-            <Typography weight={'medium'} color={'neutral'} variant={'h4'} className={styles.centeredText}>
-              <At width={30} height={30} className={styles.icon} />
-              Contact information
-            </Typography>
-
-            <Typography weight={'medium'} as={'p'}>
-              {user?.email}
-            </Typography>
-            {(user?.country || user?.city) && (
-              <Typography weight={'medium'} as={'p'}>
-                {user?.country} {user?.city}
-              </Typography>
-            )}
-            {refs && refs.length > 0 && (
-              <>
-                <Typography>References: </Typography>
-                {user?.profile?.references?.map((r, index) => (
-                  <Typography key={index}>
-                    <a href={r.uri} rel="noopener noreferrer" target="_blank">
-                      {r.name}
-                    </a>
-                  </Typography>
-                ))}
-              </>
+            {noMembership && (
+              <div className={styles.centeredText}>
+                <Typography weight={'medium'} className={styles.centeredText}>
+                  User has no memberships
+                </Typography>
+              </div>
             )}
           </>
         )}
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={onHide}>Close</Button>
+        <OverlayTrigger placement={'top'} overlay={<Tooltip id={'more-tags'}>Comming soon</Tooltip>}>
+          <span>
+            <Button variant={'primary'} disabled={true}>
+              Send message
+            </Button>
+          </span>
+        </OverlayTrigger>
+        <Button onClick={onHide}>{t('buttons.close')}</Button>
       </Modal.Footer>
     </Modal>
   );
