9386ec59484400eb3e6c21b71a02217ef4826240
hero101 <svetoslav@alkem.io>
2021-09-07 13:24:38 +0300

    The page is showing basic information about the organisation

--- src/pages/organisation/OrganisationPage.tsx
+++ src/pages/organisation/OrganisationPage.tsx
@@ -1,27 +1,46 @@
 import React, { FC, useMemo } from 'react';
-import { PageProps } from '../common';
 import { useRouteMatch } from 'react-router';
-import { useOrganisation } from '../../hooks/useOrganisation';
-import { useUpdateNavigation } from '../../hooks';
+import { Typography } from '@material-ui/core';
+import { PageProps } from '../common';
+import { createStyles, useUpdateNavigation, useOrganisation } from '../../hooks';
+import Section, { Body, Header as SectionHeader, SubHeader } from '../../components/core/Section';
+import { Image } from '../../components/core/Image';
+import Divider from '../../components/core/Divider';
+
+const useStyles = createStyles(() => ({
+  banner: {
+    maxWidth: 320,
+    height: 'initial',
+    margin: '0 auto',
+  },
+}));
 
 const OrganisationPage: FC<PageProps> = ({ paths }) => {
+  const styles = useStyles();
   const { url } = useRouteMatch();
   const { organisation } = useOrganisation();
   const currentPaths = useMemo(
     () => (organisation ? [...paths, { value: url, name: organisation.displayName, real: true }] : paths),
     [paths, organisation]
   );
-
   useUpdateNavigation({ currentPaths });
+
+  const { profile, displayName, legalEntityName, contactEmail } = organisation || {};
+  const { avatar, description } = profile || {};
 
   return (
     <>
-      {organisation?.displayName}
-      <br />
-      {organisation?.nameID}
-      <br />
-      {organisation?.contactEmail}
-      <br />
+      <Section avatar={avatar ? <Image src={avatar} alt={`${displayName} logo`} className={styles.banner} /> : <div />}>
+        <SectionHeader text={displayName} />
+        <SubHeader text={description} />
+        <Body>
+          <Typography variant={'h5'}>{legalEntityName}</Typography>
+          <Typography component={'a'} href={`mailto:${contactEmail}`}>
+            {contactEmail}
+          </Typography>
+        </Body>
+      </Section>
+      <Divider />
     </>
   );
 };
