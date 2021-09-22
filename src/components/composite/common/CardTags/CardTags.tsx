e45bee309b9ad3468057316d40ac9a25a2235310
Angel <atstojanov@gmail.com>
2021-09-22 15:54:14 +0300

    One tooltip for all tags

--- src/components/composite/common/CardTags/CardTags.tsx
+++ src/components/composite/common/CardTags/CardTags.tsx
@@ -12,27 +12,23 @@ export const CardTags: FC<CardTagsProps> = ({ tags, limit = 3 }) => {
   const truncatedTags = useMemo(() => tags.slice(0, limit), [tags]);
 
   return (
-    <TagContainer>
-      {truncatedTags.map((t, i) => (
-        // with maxWidth limit long tags to 2 per line. so no more than 2 lines of tags.
-        // 45% was the break point.
-        // eslint-disable-next-line react/jsx-no-undef
-        <Box key={i} overflow={'hidden'} maxWidth={'45%'}>
-          <Tooltip placement="right" title={t} arrow>
-            <div>
+    <Tooltip placement="right" title={tags.join(', ')} id="more-tags" arrow>
+      <div>
+        <TagContainer>
+          {truncatedTags.map((t, i) => (
+            // with maxWidth limit long tags to 2 per line. so no more than 2 lines of tags.
+            // 45% was the break point.
+            // eslint-disable-next-line react/jsx-no-undef
+            <Box key={i} overflow={'hidden'} maxWidth={'45%'}>
               <Tag key={i} text={t} color="neutralMedium" />
-            </div>
-          </Tooltip>
-        </Box>
-      ))}
-      {tags.length > 3 && (
-        <Tooltip placement="right" title={tags.slice(3).join(', ')} id="more-tags" arrow>
-          <span>
+            </Box>
+          ))}
+          {tags.length > 3 && (
             <Tag text={<>{`+ ${tags.length - truncatedTags.length} more`}</>} color="neutralMedium" />
-          </span>
-        </Tooltip>
-      )}
-    </TagContainer>
+          )}
+        </TagContainer>
+      </div>
+    </Tooltip>
   );
 };
 
