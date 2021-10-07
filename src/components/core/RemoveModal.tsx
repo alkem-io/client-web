675c96967d7e243933ff925c0f7e96c9cb8d99e2
Svetoslav Petkov <svetoslav@alkem.io>
2021-10-07 17:37:10 +0300

    organization groups are now deletable

--- src/components/core/RemoveModal.tsx
+++ src/components/core/RemoveModal.tsx
@@ -12,12 +12,14 @@ interface RelationRemoveModalProps {
   title?: string;
 }
 
-const RemoveModal: FC<RelationRemoveModalProps> = ({ show, text, onCancel, title = 'Confirm remove', onConfirm }) => {
+const RemoveModal: FC<RelationRemoveModalProps> = ({ show, text, onCancel, title, onConfirm }) => {
   const { t } = useTranslation();
+  const defaultTitle = t('components.remove-modal.title');
+
   return (
     <Dialog open={show} maxWidth="md" fullWidth aria-labelledby="remove-dialog-title">
       <DialogTitle id="remove-dialog-title" onClose={onCancel}>
-        {title}
+        {title || defaultTitle}
       </DialogTitle>
       <DialogContent dividers>{text}</DialogContent>
       <DialogActions>
