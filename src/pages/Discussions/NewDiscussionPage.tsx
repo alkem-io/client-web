4fd1fa2e567493b956936b80e6c01ace759c39c9
hero101 <svetoslav@alkem.io>
2021-11-17 15:40:20 +0200

    fixed bug with incorrect titles on overview and new discussion

--- src/pages/Discussions/NewDiscussionPage.tsx
+++ src/pages/Discussions/NewDiscussionPage.tsx
@@ -6,18 +6,20 @@ import { ThemeProviderV2 } from '../../context/ThemeProvider';
 import { useUpdateNavigation } from '../../hooks';
 import NewDiscussionView from '../../views/Discussions/NewDiscussionView';
 import { PageProps } from '../common';
+import { useCommunityContext } from '../../context/CommunityProvider';
 
 export interface NewDiscussionPageProps extends PageProps {}
 
 const NewDiscussionPage: FC<NewDiscussionPageProps> = ({ paths }) => {
   const { url } = useRouteMatch();
   const { handleCreateDiscussion } = useDiscussionsContext();
+  const { communityName } = useCommunityContext();
   const currentPaths = useMemo(() => [...paths, { value: url, name: 'new', real: false }], [paths]);
 
   useUpdateNavigation({ currentPaths });
 
   // TODO [ATS]:  this will be constructed depending on the community.
-  const title = 'Digital Twinning - Initiate Discussion';
+  const title = `${communityName} - Initiate Discussion`;
 
   return (
     <ThemeProviderV2>
