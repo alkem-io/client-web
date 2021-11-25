f0a07cc72cd669c58fa664865a426c534aa6b656
Svetoslav Petkov <svetoslav@alkem.io>
2021-11-25 22:17:14 +0200

    top bar search to redirect to search page

--- src/components/composite/layout/TopNavigation/SearchComponent.tsx
+++ src/components/composite/layout/TopNavigation/SearchComponent.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { useHistory } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import TextField from '@mui/material/TextField';
 import { InputAdornment } from '@mui/material';
@@ -6,12 +8,23 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const SearchComponent = () => {
   const { t } = useTranslation();
+  const history = useHistory();
+  const [value, setValue] = useState<String>('');
+
+  const keyPressHandler = ({ code }: React.KeyboardEvent<HTMLDivElement>) => {
+    if (code === 'Enter' || code === 'NumpadEnter') {
+      const terms = value.split(' ').join(',');
+      history.push(`/search?terms=${terms}`);
+    }
+  };
 
   return (
     <TextField
       aria-label="Search"
       placeholder={t('common.search')}
+      onChange={e => setValue(e.target.value)}
       margin="dense"
+      onKeyPress={keyPressHandler}
       sx={{
         padding: 0,
         margin: 0,
