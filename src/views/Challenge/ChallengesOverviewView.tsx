93696a2792df72b8a4276a5f4f44fdc5a554fb52
Svetoslav Petkov <svetoslav@alkem.io>
2021-12-06 10:23:01 +0200

    finishing touches

--- src/views/Challenge/ChallengesOverviewView.tsx
+++ src/views/Challenge/ChallengesOverviewView.tsx
@@ -1,19 +1,14 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Box, Chip } from '@mui/material';
+import { Box } from '@mui/material';
 import EcoverseChallengesContainer from '../../containers/ecoverse/EcoverseChallengesContainer';
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
 import { ChallengeCardContainer } from '../../containers/challenge/ChallengeCardContainer';
-import ChallengeCard, { ChallengeCardProps } from '../../components/composite/entities/Ecoverse/ChallengeCard';
+import ChallengeCard from '../../components/composite/entities/Ecoverse/ChallengeCard';
 import { useUserContext } from '../../hooks';
 import { buildChallengeUrl } from '../../utils/urlBuilders';
 import { Accordion } from '../../components/composite/common/Accordion/Accordion';
 import { CardContainer } from '../../components/core/CardContainer';
-import ProfileCard from '../../components/composite/common/cards/ProfileCard/ProfileCard';
-import CardFilter from '../../components/core/card-filter/CardFilter';
-import { ValueType } from '../../components/core/card-filter/filterFn';
 
 export interface HubOverview {
   ecoverseID: string;
@@ -31,58 +26,13 @@ export interface ChallengesOverviewViewProps {
   hubs: HubOverview[];
 }
 
-const tagsValueGetter = (props: ChallengeCardProps) => props.tags;
-const valueGetter = (props: ChallengeCardProps): ValueType => ({
-  id: props.id,
-  values: [
-    props?.displayName ?? '',
-    props.context?.tagline ?? '',
-  ]
-});
-
 export const ChallengesOverviewView: FC<ChallengesOverviewViewProps> = ({ myChallenges, hubs }) => {
   const { t } = useTranslation();
   const { user } = useUserContext();
 
-  const [filterTerms, setFilterTerms] = useState<string[]>([]);
-  const handleChange = (e, value: string[]) => {
-    const trimmedValues = value.map(x => x.trim().toLowerCase());
-    setFilterTerms(trimmedValues);
-  };
-
   return (
     <Box paddingY={2}>
       <Grid container rowSpacing={4}>
-        <Grid item xs={12}>
-          <ProfileCard
-            title={t('pages.challenges-overview.filter.title')}
-            subtitle={t('pages.challenges-overview.filter.subtitle')}
-          >
-            <Autocomplete
-              aria-label="Filter"
-              id="challenge-filter"
-              multiple
-              fullWidth
-              freeSolo
-              disableCloseOnSelect
-              options={[]}
-              onChange={handleChange}
-              renderTags={(value, getTagProps) =>
-                value.map((option, index) => (
-                  <Chip color="primary" variant="outlined" label={option} {...getTagProps({ index })} />
-                ))
-              }
-              renderInput={params => (
-                <TextField
-                  {...params}
-                  size={'small'}
-                  variant="outlined"
-                  placeholder={'Search'}
-                />
-              )}
-            />
-          </ProfileCard>
-        </Grid>
         <Grid item xs={12}>
           <Accordion
             title={t('pages.challenges-overview.my.title', { count: myChallenges.length })}
@@ -99,25 +49,17 @@ export const ChallengesOverviewView: FC<ChallengesOverviewViewProps> = ({ myChal
                     }
 
                     return (
-                      <CardFilter
-                        data={[]}
-                        tagsValueGetter={tagsValueGetter}
-                        valueGetter={valueGetter}
-                      >
-                        {filteredData => (
-                          <ChallengeCard
-                            key={i}
-                            id={cardProps.id}
-                            displayName={cardProps.displayName}
-                            activity={cardProps.activity}
-                            context={cardProps.context}
-                            isMember={cardProps.isMember}
-                            tags={cardProps.tags}
-                            url={cardProps.url}
-                          />
-                        )}
-                      </CardFilter>
-                    )
+                      <ChallengeCard
+                        key={i}
+                        id={cardProps.id}
+                        displayName={cardProps.displayName}
+                        activity={cardProps.activity}
+                        context={cardProps.context}
+                        isMember={cardProps.isMember}
+                        tags={cardProps.tags}
+                        url={cardProps.url}
+                      />
+                    );
                   }}
                 </ChallengeCardContainer>
               ))}
@@ -132,11 +74,14 @@ export const ChallengesOverviewView: FC<ChallengesOverviewViewProps> = ({ myChal
                 ecoverseNameId: hubNameId,
               }}
             >
-              {(cEntities) => {
+              {cEntities => {
                 return (
                   <Grid item xs={12}>
                     <Accordion
-                      title={t('pages.challenges-overview.hubs.title', { count: cEntities.challenges.length, name: hubName })}
+                      title={t('pages.challenges-overview.hubs.title', {
+                        count: cEntities.challenges.length,
+                        name: hubName,
+                      })}
                       subtitle={t('pages.challenges-overview.hubs.subtitle', { name: hubName })}
                       helpText={t('pages.challenges-overview.hubs.help-text')}
                       ariaKey={hubName}
@@ -162,7 +107,7 @@ export const ChallengesOverviewView: FC<ChallengesOverviewViewProps> = ({ myChal
                       </CardContainer>
                     </Accordion>
                   </Grid>
-                )
+                );
               }}
             </EcoverseChallengesContainer>
           );
