b6574a1060076827f776f96eed6d8ea7a57f93d6
Angel <atstojanov@gmail.com>
2021-12-09 12:49:49 +0200

    Fix: (UFT) Missing counters on dashboard Updates and Discussions cards #1472

--- src/components/composite/common/sections/DashboardUpdatesSection.tsx
+++ src/components/composite/common/sections/DashboardUpdatesSection.tsx
@@ -17,40 +17,40 @@ const DashboardUpdatesSection: FC<DashboardUpdatesSectionProps> = ({ entities })
   const { t } = useTranslation();
 
   return (
-    <DashboardGenericSection headerText={t('dashboard-updates-section.title')}>
-      <CommunityUpdatesContainer entities={entities}>
-        {entities => {
-          if (!entities.messages.length) {
-            return t('dashboard-updates-section.no-data');
-          }
+    <CommunityUpdatesContainer entities={entities}>
+      {entities => {
+        const messages = [...entities.messages];
+        const [latestMessage] = messages.sort((a, b) => b.timestamp - a.timestamp);
+        const messageSender = {
+          id: latestMessage.sender,
+        };
 
-          const messages = [...entities.messages];
-          const [latestMessage] = messages.sort((a, b) => b.timestamp - a.timestamp);
-          const messageSender = {
-            id: latestMessage.sender,
-          };
-
-          return (
-            <AvatarsProvider users={[messageSender]}>
-              {populatedUsers => (
-                <SingleUpdateView
-                  author={{
-                    id: populatedUsers[0].id,
-                    displayName: populatedUsers[0].displayName,
-                    avatarUrl: populatedUsers[0].profile?.avatar ?? '',
-                    firstName: '',
-                    lastName: '',
-                    url: buildUserProfileUrl(populatedUsers[0].nameID),
-                  }}
-                  createdDate={new Date(latestMessage.timestamp)}
-                  content={latestMessage.message}
-                />
-              )}
-            </AvatarsProvider>
-          );
-        }}
-      </CommunityUpdatesContainer>
-    </DashboardGenericSection>
+        return (
+          <DashboardGenericSection headerText={t('dashboard-updates-section.title', { count: messages.length })}>
+            {messages.length ? (
+              <AvatarsProvider users={[messageSender]}>
+                {populatedUsers => (
+                  <SingleUpdateView
+                    author={{
+                      id: populatedUsers[0].id,
+                      displayName: populatedUsers[0].displayName,
+                      avatarUrl: populatedUsers[0].profile?.avatar ?? '',
+                      firstName: '',
+                      lastName: '',
+                      url: buildUserProfileUrl(populatedUsers[0].nameID),
+                    }}
+                    createdDate={new Date(latestMessage.timestamp)}
+                    content={latestMessage.message}
+                  />
+                )}
+              </AvatarsProvider>
+            ) : (
+              t('dashboard-updates-section.no-data')
+            )}
+          </DashboardGenericSection>
+        );
+      }}
+    </CommunityUpdatesContainer>
   );
 };
 export default DashboardUpdatesSection;
