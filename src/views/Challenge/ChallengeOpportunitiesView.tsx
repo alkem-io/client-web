aec6c3d32f899ecd9c1a5328147862ca2f17f7af
Angel <atstojanov@gmail.com>
2021-12-10 17:15:57 +0200

    Add message on challenge page if there are no opportunities available

--- src/views/Challenge/ChallengeOpportunitiesView.tsx
+++ src/views/Challenge/ChallengeOpportunitiesView.tsx
@@ -1,19 +1,19 @@
+import { Box, Grid, Typography } from '@mui/material';
 import React, { FC } from 'react';
-import { ChallengeContainerEntities, ChallengeContainerState } from '../../containers/challenge/ChallengePageContainer';
-import { Opportunity } from '../../models/graphql-schema';
+import { useTranslation } from 'react-i18next';
+import OpportunityCard from '../../components/composite/entities/Challenge/OpportunityCard';
+import { Loading } from '../../components/core';
+import CardFilter from '../../components/core/card-filter/CardFilter';
 import {
   entityTagsValueGetter,
   entityValueGetter,
 } from '../../components/core/card-filter/value-getters/entity-value-getter';
 import { CardContainer } from '../../components/core/CardContainer';
-import OpportunityCard from '../../components/composite/entities/Challenge/OpportunityCard';
-import { buildOpportunityUrl } from '../../utils/urlBuilders';
-import CardFilter from '../../components/core/card-filter/CardFilter';
-import { useTranslation } from 'react-i18next';
-import { useChallenge } from '../../hooks';
-import { Loading } from '../../components/core';
-import { Grid } from '@mui/material';
 import ErrorBlock from '../../components/core/ErrorBlock';
+import { ChallengeContainerEntities, ChallengeContainerState } from '../../containers/challenge/ChallengePageContainer';
+import { useChallenge } from '../../hooks';
+import { Opportunity } from '../../models/graphql-schema';
+import { buildOpportunityUrl } from '../../utils/urlBuilders';
 
 interface ChallengeOpportunitiesViewProps {
   entities: ChallengeContainerEntities;
@@ -38,6 +38,12 @@ export const ChallengeOpportunitiesView: FC<ChallengeOpportunitiesViewProps> = (
     );
   }
 
+  if (opportunities.length <= 0)
+    return (
+      <Box paddingBottom={2} display="flex" justifyContent="center">
+        <Typography>{t('pages.challenge.sections.opportunities.body-missing')}</Typography>
+      </Box>
+    );
   return (
     <CardFilter data={opportunities} tagsValueGetter={entityTagsValueGetter} valueGetter={entityValueGetter}>
       {filteredData => (
