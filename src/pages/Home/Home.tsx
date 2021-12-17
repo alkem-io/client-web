e6956d04b3e506f1367a5121b458f350dc9ec73c
nevelichkoff <nevelichkoff@gmail.com>
2021-12-17 11:58:08 +0200

    Address home page issues

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import React, { useMemo } from 'react';
 import { SectionSpacer } from '../../components/core/Section/Section';
 import { useUpdateNavigation } from '../../hooks';
@@ -9,18 +10,32 @@ import GettingStartedSection from './GettingStartedSection';
 import ShowInterestSection from './ShowInterestSection';
 import WelcomeSection from './WelcomeSection';
 
+const useStyles = makeStyles(_ => ({
+  grow: {
+    flexGrow: 1,
+  },
+}));
+
 export const HomePage = () => {
   const currentPaths = useMemo(() => [], []);
   useUpdateNavigation({ currentPaths });
+
+  const styles = useStyles();
 
   return (
     <>
       <SectionSpacer />
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} display="flex" flexDirection="column">
           <WelcomeSection />
           <SectionSpacer />
-          <ShowInterestSection />
+          <ShowInterestSection
+            classes={{
+              section: {
+                root: styles.grow,
+              },
+            }}
+          />
         </Grid>
         <Grid item xs={12} md={6}>
           <AlkemioActivitySection />
