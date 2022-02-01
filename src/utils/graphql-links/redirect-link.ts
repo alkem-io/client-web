2ba3a6bc62a07c0643c06b137b2de1030776c682
Svetoslav Petkov <svetoslav@alkem.io>
2022-02-01 13:42:29 +0200

    changed how graphql error links are handling errors

--- src/utils/graphql-links/redirect-link.ts
+++ src/utils/graphql-links/redirect-link.ts
@@ -12,10 +12,13 @@ import { AUTH_LOGIN_PATH } from '../../models/constants';
  * the response has been redirected
  * and the response source url matches the auth login page.
  * The browser is redirected to the login page if the above criteria are met.
+ *
+ * This function is called after the GraphQL operation completes and execution is moving back up your link chain.
+ * The function should not return a value unless you want to retry the operation.
  */
-export const redirectLink = onError(({ networkError, forward, operation }) => {
+export const redirectLink = onError(({ networkError }) => {
   if (!networkError) {
-    return forward(operation);
+    return;
   }
 
   const parseError = networkError as ServerParseError;
@@ -29,7 +32,5 @@ export const redirectLink = onError(({ networkError, forward, operation }) => {
       logger.info(`[ServerParseError]: Redirecting to ${response.url}...`);
     }
   }
-
-  return forward(operation);
 });
 export default redirectLink;
