f2f04b1af5aa322d6a34f0c8e7a7d6ebaae638ac
Andrew Pazniak <andrew@yess.llc>
2022-04-07 00:24:57 +0200

    cleanup

--- src/components/composite/forms/ContextForm.tsx
+++ src/components/composite/forms/ContextForm.tsx
@@ -5,53 +5,31 @@ import { Context } from '../../../models/graphql-schema';
 import { ContextSegment, contextSegmentSchema } from '../../Admin/Common/ContextSegment';
 
 export interface ContextFormValues {
-  // name: string;
-  // nameID: string;
   background: string;
   impact: string;
-  // tagline: string;
   vision: string;
   who: string;
-  // references: Reference[];
-  // visuals: Visual2[]; todo: enable when it's time
-  // tagsets: Tagset[];
 }
 
 interface Props {
   context?: Context;
-  // name?: string;
-  // nameID?: string;
-  // tagset?: Tagset;
   onSubmit: (formData: ContextFormValues) => void;
   wireSubmit: (setter: () => void) => void;
-  // contextOnly?: boolean;
-  // isEdit: boolean;
 }
 
 const ContextForm: FC<Props> = ({ context, onSubmit, wireSubmit }) => {
   const initialValues: ContextFormValues = {
-    // name: name || '',
-    // nameID: nameID || '',
     background: context?.background || '',
     impact: context?.impact || '',
-    // tagline: context?.tagline || '',
     vision: context?.vision || '',
     who: context?.who || '',
-    // references: context?.references || [],
-    // tagsets: tagsets,
   };
 
   const validationSchema = yup.object().shape({
-    // name: contextOnly ? yup.string() : nameSegmentSchema.fields?.name || yup.string(),
-    // nameID: contextOnly ? yup.string() : nameSegmentSchema.fields?.nameID || yup.string(),
     background: contextSegmentSchema.fields?.background || yup.string(),
     impact: contextSegmentSchema.fields?.impact || yup.string(),
-    // tagline: contextSegmentSchema.fields?.tagline || yup.string(),
     vision: contextSegmentSchema.fields?.vision || yup.string(),
     who: contextSegmentSchema.fields?.who || yup.string(),
-    // references: referenceSegmentSchema,
-    // visual: visualSegmentSchema,
-    // tagsets: tagsetSegmentSchema,
   });
 
   let isSubmitWired = false;
