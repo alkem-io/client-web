2036e619f22a9329dde2e8751e287d7e4f13be4b
Andrew Pazniak <andrew@yess.llc>
2022-04-07 13:21:19 +0200

    Challenge Settings: initial redirect to /profile

--- src/routing/admin/challenge/ChallengeRoute.tsx
+++ src/routing/admin/challenge/ChallengeRoute.tsx
@@ -1,11 +1,8 @@
 import React, { FC, useMemo } from 'react';
-import { Route, Routes, useResolvedPath } from 'react-router-dom';
-import { managementData } from '../../../components/Admin/managementData';
+import { Route, Routes, useResolvedPath, Navigate } from 'react-router-dom';
 import { useChallenge, useHub } from '../../../hooks';
 import { AuthorizationCredential } from '../../../models/graphql-schema';
 import { Error404, PageProps } from '../../../pages';
-import ManagementPageTemplatePage from '../../../pages/Admin/ManagementPageTemplatePage';
-import { buildChallengeUrl } from '../../../utils/urlBuilders';
 import { CommunityRoute } from '../community';
 import { OpportunitiesRoute } from '../opportunity/OpportunitiesRoute';
 import ChallengeAuthorizationRoute from './ChallengeAuthorizationRoute';
@@ -15,10 +12,9 @@ import ChallengeCommunicationsPage from '../../../pages/Admin/Challenge/Challeng
 
 export const ChallengeRoute: FC<PageProps> = ({ paths }) => {
   const { pathname: url } = useResolvedPath('.');
-  const { communityId: parentCommunityId, loading: loadingHub } = useHub();
-  const { challenge, displayName, hubNameId, challengeId, challengeNameId, loading: loadingChallenge } = useChallenge();
+  const { communityId: parentCommunityId } = useHub();
+  const { challenge, displayName, challengeId } = useChallenge();
   const communityId = challenge?.community?.id;
-  const loading = loadingHub || loadingChallenge;
 
   const currentPaths = useMemo(
     () => [...paths, { value: url, name: displayName || '', real: true }],
@@ -28,18 +24,7 @@ export const ChallengeRoute: FC<PageProps> = ({ paths }) => {
   return (
     <Routes>
       <Route path={'/'}>
-        <Route
-          index
-          element={
-            <ManagementPageTemplatePage
-              data={managementData.challengeLvl}
-              paths={currentPaths}
-              title={displayName}
-              entityUrl={buildChallengeUrl(hubNameId, challengeNameId)}
-              loading={loading}
-            />
-          }
-        />
+        <Route index element={<Navigate to="profile" replace />} />
         <Route path="profile" element={<ChallengeProfilePage paths={currentPaths} />} />
         <Route path="context" element={<ChallengeContextPage paths={currentPaths} />} />
         <Route
