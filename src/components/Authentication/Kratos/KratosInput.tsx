ee53b1885f8bce94422fbe7c3cdb8df41f84f44d
Svetoslav Petkov <svetoslav@alkem.io>
2022-04-15 18:00:18 +0300

    removed bootstrap-icons package

--- src/components/Authentication/Kratos/KratosInput.tsx
+++ src/components/Authentication/Kratos/KratosInput.tsx
@@ -1,14 +1,13 @@
 import { Grid, InputAdornment, OutlinedInputProps, TextField } from '@mui/material';
 import { UiNodeInputAttributes } from '@ory/kratos-client';
-import { ReactComponent as EyeSlash } from 'bootstrap-icons/icons/eye-slash.svg';
-import { ReactComponent as Eye } from 'bootstrap-icons/icons/eye.svg';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import React, { FC, useContext, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import Icon from '../../core/Icon';
-import IconButton from '../../core/IconButton';
 import { KratosUIContext } from '../KratosUI';
 import { getNodeName, getNodeTitle, getNodeValue, isInvalidNode, isRequired } from './helpers';
 import { KratosInputExtraProps, KratosProps } from './KratosProps';
+import IconButton from '@mui/material/IconButton';
 
 interface KratosInputProps extends KratosProps, KratosInputExtraProps {}
 
@@ -40,8 +39,8 @@ export const KratosInput: FC<KratosInputProps> = ({ node, autoCapitalize, autoCo
       ...InputProps,
       endAdornment: (
         <InputAdornment position="end">
-          <IconButton onClick={() => setInputType(inputType === 'password' ? 'text' : 'password')} size="large">
-            <Icon component={inputType === 'password' ? Eye : EyeSlash} color="inherit" size={'xs'} />
+          <IconButton onClick={() => setInputType(inputType === 'password' ? 'text' : 'password')} size="small">
+            {inputType === 'password' ? <VisibilityIcon /> : <VisibilityOffIcon />}
           </IconButton>
         </InputAdornment>
       ),
