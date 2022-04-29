ccf0ef5ace8858bfa586e1bd32b88a66e847c211
hero101 <svetoslav@alkem.io>
2022-04-29 11:25:12 +0300

    reviewers suggestions

--- src/utils/apollo-cache/pagination-policy.ts
+++ src/utils/apollo-cache/pagination-policy.ts
@@ -1,7 +1,7 @@
 import { FieldPolicy } from '@apollo/client';
 import { unionBy } from 'lodash/array';
 
-type KeyArgs = FieldPolicy<any>['keyArgs'];
+type KeyArgs = FieldPolicy<unknown>['keyArgs'];
 const pageInfoFieldName = 'pageInfo';
 
 export const paginationFieldPolicy = (keyArgs: KeyArgs = false) => {
@@ -13,7 +13,10 @@ export const paginationFieldPolicy = (keyArgs: KeyArgs = false) => {
       }
 
       const dataKey = Object.keys(incoming).filter(x => x !== pageInfoFieldName && x !== '__typename');
-
+      // we expect 'incoming' to be { __typename: string, pageInfo: {}, items: [] };
+      // items is a placeholder field - it's calculated on the server based on what entity you have requested, e.g. 'users'
+      // if after filtering out the two other fields, we still have more fields, override the old data;
+      // throwing an exception or a warning maybe also be required
       if (dataKey.length > 1) {
         return incoming;
       }
@@ -23,7 +26,7 @@ export const paginationFieldPolicy = (keyArgs: KeyArgs = false) => {
 
       const existingData = existing[dataFieldName];
 
-      const newData = incomingData ? unionBy([...existingData, incomingData], '__ref') : existingData;
+      const newData = incomingData ? unionBy(existingData, incomingData, '__ref') : existingData;
 
       return {
         [dataFieldName]: newData,
