b5281dee43f78314c1043e29760947b8c24b5e22
Andrew Pazniak <andrew@yess.llc>
2022-05-12 13:29:27 +0400

    Normalized output of formatLocation()

--- src/domain/location/LocationUtils.ts
+++ src/domain/location/LocationUtils.ts
@@ -3,24 +3,22 @@ import { Location as LocationModel } from '../../domain/location/Location';
 import { COUNTRIES } from '../../models/constants';
 
 /**
- * getLocationModel turns a Location coming from a GraphQL query into a Location usable by the UI.
+ * formatLocation turns a Location coming from a GraphQL query into a Location usable by the UI.
  * In the database we are storing only the country code, a 2 chars string, we need to turn that into
  * a CountryType object { name: string, code: string }
  *
- * Handles well empty objects, null/undefined/(countries not found by code) will be returned as { name: '', code: '' }
- *
  * @param data Location from a GraphQL query
- * @returns A Location from ../../domain/location/Location that contains the full CountryType object,
+ * @returns A Location that contains the full CountryType object
  */
-const getLocationModel = function (data: Maybe<GraphQLLocation>): LocationModel | null {
-  if (!data) return null;
+const formatLocation = function (data: Maybe<GraphQLLocation>): Partial<LocationModel> | undefined {
+  if (!data) {
+    return undefined;
+  }
 
-  const location: LocationModel = {
-    city: data?.city || '',
-    country: COUNTRIES.find(x => x.code === data?.country) || { name: '', code: '' },
+  return {
+    city: data?.city,
+    country: COUNTRIES.find(x => x.code === data?.country),
   };
-
-  return location;
 };
 
-export { getLocationModel };
+export { formatLocation };
