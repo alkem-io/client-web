665984a08362ca54b3f850c967b3b3b94850036f
Andrew Pazniak <andrew@yess.llc>
2022-05-25 11:07:49 +0300

    jsdoc for useSubscriptionToSubEntity

--- src/domain/shared/subscriptions/useSubscriptionToSubEntity.ts
+++ src/domain/shared/subscriptions/useSubscriptionToSubEntity.ts
@@ -14,6 +14,24 @@ interface CreateUseSubscriptionToSubEntityOptions<SubEntity, SubEntitySubscripti
   updateSubEntity: (subEntity: SubEntity | undefined, subscriptionData: SubEntitySubscription) => void;
 }
 
+/**
+ * Creates and returns a hook that subscribes to the specified SubscriptionDocument by leveraging subscribeToMore()
+ * from a parent query.
+ * This hook factory is designed in the way so that the following parameters are bound at the factory level:
+ * - SubscriptionDocument (the entity to be fetched),
+ * - the getter for the subscription variables,
+ * - the callback that updates the cache on a subscription message.
+ * The factory produces a hook that is bound to some subscription document / response structure but allows subscribing
+ * to this entity from multiple parent queries.
+ * Example: by calling this factory you can create a hook that subscribes to Comments; the resulting hook you can use to
+ * subscribe for Comments on Hub Aspects, Challenge Aspects and Opportunity Aspects. To the resulting hook you'll have to
+ * pass different paths to Comments from different parent entities as well as subscribeToMore() from different the parent queries.
+ * @param options.subscriptionDocument - the subscription document to fetch.
+ * @param options.getSubscriptionVariables - the function that constructs subscription variables by reading the entity.
+ * @param options.updateSubEntity - the callback that updates cache based on the received subscription message; it should
+ * just mutate the cache data directly by leveraging property assignments, array.push(), etc.; immutability / producing new
+ * structures is ensured/handled by 'immer'.
+ */
 const createUseSubscriptionToSubEntityHook =
   <SubEntity, SubEntitySubscriptionVariables, SubEntitySubscription>(
     options: CreateUseSubscriptionToSubEntityOptions<SubEntity, SubEntitySubscriptionVariables, SubEntitySubscription>
