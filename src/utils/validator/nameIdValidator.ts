c29c7a1e8fab76cba713892cc86c86611d57cab2
Andrew P <594548+me-andre@users.noreply.github.com>
2022-06-01 16:13:01 +0300

    Validation messages are refreshed on language change (#2117)
    * Validation messages are refreshed on language change
    
    * better signature
    
    * better naming

--- src/utils/validator/nameIdValidator.ts
+++ src/utils/validator/nameIdValidator.ts
@@ -1,9 +1,9 @@
 import * as yup from 'yup';
-import i18next from 'i18next';
+import { TranslatedValidation } from '../../domain/shared/i18n/ValidationMessageTranslation';
 
 export const nameIdValidator = yup
   .string()
-  .required(i18next.t('forms.validations.required'))
-  .min(3, 'NameID should be at least 3 symbols long')
-  .max(25, 'Exceeded the limit of 25 characters')
-  .matches(/^[a-z0-9-]*$/, 'NameID can contain only lower case latin characters, numbers and hyphens');
+  .required('forms.validations.required')
+  .min(3, TranslatedValidation('forms.validations.minLength'))
+  .max(25, TranslatedValidation('forms.validations.maxLength'))
+  .matches(/^[a-z0-9-]*$/, 'forms.validations.alphaNumeric');
