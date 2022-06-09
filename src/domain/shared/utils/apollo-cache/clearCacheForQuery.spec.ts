807c73fc90742c422fc8e3d9ca2db5fa58ba985e
Andrew Pazniak <andrew@yess.llc>
2022-06-09 10:28:38 +0300

    +1 test case

--- src/domain/shared/utils/apollo-cache/clearCacheForQuery.spec.ts
+++ src/domain/shared/utils/apollo-cache/clearCacheForQuery.spec.ts
@@ -1,21 +1,35 @@
 import { getRegExpForQueryName } from './clearCacheForQuery';
 
 describe('getRegExpForQueryName', () => {
-  const queryName = 'sampleQueryName';
+  const sampleQueryName = 'sampleQueryName';
 
   it('matches the query name', () => {
-    expect(getRegExpForQueryName(queryName).test(queryName)).toBeTruthy();
+    expect(getRegExpForQueryName(sampleQueryName).test(sampleQueryName)).toBeTruthy();
   });
 
   it('matches the query followed by variables in parentheses', () => {
-    expect(getRegExpForQueryName(queryName).test('sampleQueryName({"arg": "value"})')).toBeTruthy();
+    expect(getRegExpForQueryName(sampleQueryName).test('sampleQueryName({"arg": "value"})')).toBeTruthy();
   });
 
   it('matches the query followed by a semicolon', () => {
-    expect(getRegExpForQueryName(queryName).test('sampleQueryName:{"arg": "value"}')).toBeTruthy();
+    expect(getRegExpForQueryName(sampleQueryName).test('sampleQueryName:{"arg": "value"}')).toBeTruthy();
   });
 
   it('does not match another query name that starts with the same string', () => {
-    expect(getRegExpForQueryName(queryName).test(`${queryName}Two`)).toBeFalsy();
+    expect(getRegExpForQueryName(sampleQueryName).test(`${sampleQueryName}Two`)).toBeFalsy();
+  });
+
+  it('Potential RegExp statefulness does not affect the match', () => {
+    const regExp = getRegExpForQueryName(sampleQueryName);
+    expect(
+      [
+        sampleQueryName,
+        'sampleQueryName({"arg": "value"})',
+        'sampleQueryName:{"arg": "value"}',
+        sampleQueryName,
+        'sampleQueryName({"arg": "value"})',
+        'sampleQueryName:{"arg": "value"}',
+      ].map(queryName => regExp.test(queryName))
+    ).toEqual(Array(6).fill(true));
   });
 });
