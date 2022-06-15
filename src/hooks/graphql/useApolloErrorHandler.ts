61d2432dc097e8cc200cf21a0216767669aa4318
svetoslav <svetoslav@alkem.io>
2022-06-15 17:54:17 +0300

    type check improvements

--- src/hooks/graphql/useApolloErrorHandler.ts
+++ src/hooks/graphql/useApolloErrorHandler.ts
@@ -1,4 +1,4 @@
-import { ApolloError } from '@apollo/client';
+import { ApolloError, ServerError, ServerParseError } from '@apollo/client';
 import { GraphQLError } from 'graphql';
 import { Severity } from '../../state/global/notifications/notificationMachine';
 
@@ -7,12 +7,21 @@ import { useNotification } from '../useNotification';
 export const useApolloErrorHandler = (severity: Severity = 'error') => {
   const notify = useNotification();
   return (error: ApolloError) => {
-    const networkError = error.networkError as any;
-    if (networkError && networkError.result && networkError.result.errors) {
+    const networkError = error.networkError;
+    if (!networkError) {
+      return;
+    }
+
+    if (isServerError(networkError)) {
       const graphqlError = networkError.result.errors[0] as GraphQLError;
       notify(graphqlError.message, severity);
+    } else if (isServerParseError(networkError)) {
+      notify(networkError.bodyText, severity);
     } else {
       notify(error.message, severity);
     }
   };
 };
+
+const isServerParseError = (error: unknown): error is ServerParseError => 'bodyText' in (error as ServerParseError);
+const isServerError = (error: unknown): error is ServerError => 'result' in (error as ServerError);
