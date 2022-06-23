69019ff4484dbef676e12af10fa4aceb95e0f4e0
Carlos Cano <carlos@alkem.io>
2022-06-23 11:42:51 +0200

    Change severity strings to Sentry.SeverityLevel

--- src/services/logging/sentry/log.ts
+++ src/services/logging/sentry/log.ts
@@ -1,10 +1,18 @@
 import * as Sentry from '@sentry/react';
 
+// Sentry severity levels - See @sentry/types/types/severity.d.ts
+const debugLevel: Sentry.SeverityLevel = 'debug';
+const infoLevel: Sentry.SeverityLevel = 'info';
+const logLevel: Sentry.SeverityLevel = 'log';
+const warningLevel: Sentry.SeverityLevel = 'warning';
+const errorLevel: Sentry.SeverityLevel = 'error';
+const fatalLevel: Sentry.SeverityLevel = 'fatal';
+
 export const error = (
   error: Error,
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   setup: (scope: Sentry.Scope) => void = () => {},
-  severity: 'fatal' | 'error' = 'error'
+  severity: typeof fatalLevel | typeof errorLevel = errorLevel
 ) => {
   Sentry.withScope(scope => {
     scope.setLevel(severity);
@@ -15,12 +23,12 @@ export const error = (
 
 export const warn = (warning: string) => {
   Sentry.withScope(scope => {
-    scope.setLevel('warning');
+    scope.setLevel(warningLevel);
     Sentry.captureEvent({ message: warning });
   });
 };
 
-export const info = (message: string, severity: 'info' | 'log' | 'debug' = 'info') => {
+export const info = (message: string, severity: typeof debugLevel | typeof infoLevel | typeof logLevel = infoLevel) => {
   Sentry.withScope(scope => {
     scope.setLevel(severity);
     Sentry.captureMessage(message);
