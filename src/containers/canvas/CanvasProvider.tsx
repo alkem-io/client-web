fcaac20314dcd2dbd5f2a03bad8b2545cf132571
Neil Smyth <neil@thesmyths.eu>
2022-06-25 20:43:34 +0200

    only reload templates on hub changes

--- src/containers/canvas/CanvasProvider.tsx
+++ src/containers/canvas/CanvasProvider.tsx
@@ -73,7 +73,7 @@ const CanvasProvider: FC<CanvasProviderProps> = ({ children }) => {
         result: hubData?.hub.templates?.canvasTemplates || [],
       },
     };
-  }, [hubData, challengeData, opportunityData]);
+  }, [hubData]);
 
   return (
     <>
