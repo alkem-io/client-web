f06a1b6f521f78cd53f9c0b4f3e7cb8fd78ed947
Carlos Cano <carlos@alkem.io>
2022-06-27 17:32:02 +0200

    Better BannerImage component

--- src/pages/Home/BannerImage.tsx
+++ src/pages/Home/BannerImage.tsx
@@ -1,56 +1,34 @@
-import { Box, Paper, Skeleton } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import React, { useState } from 'react';
+import { Box, styled } from '@mui/material';
 
-const useStyles = makeStyles(theme => ({
-  bannerSize: {
-    background: theme.palette.neutralMedium.light,
-    display: 'block',
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
+const BannerImageElement = styled('div')(({ theme }) => ({
+  background: theme.palette.neutralMedium.light,
+  display: 'block',
 }));
 
 const BannerImage = () => {
-  const bannerUrl = './alkemio-banner/alkemio-banner-{size}.png';
   // https://mui.com/system/display/
+  // https://mui.com/material-ui/customization/breakpoints/#custom-breakpoints
   const sizes = {
-    xs: { display: { xs: 'block', sm: 'none' }, marginTop: 8 },
-    sm: { display: { xs: 'none', sm: 'block', md: 'none' } },
-    md: { display: { xs: 'none', md: 'block', lg: 'none' }, marginTop: 2 },
-    lg: { display: { xs: 'none', lg: 'block', xl: 'none' } },
-    xl: { display: { xs: 'none', xl: 'block' } },
+    xs: { display: { xs: 'block', sm: 'none' }, marginTop: 8, height: '156px' },
+    sm: { display: { xs: 'none', sm: 'block', md: 'none' }, height: '200px' },
+    md: { display: { xs: 'none', md: 'block', lg: 'none' }, marginTop: 2, height: '200px' },
+    lg: { display: { xs: 'none', lg: 'block', xl: 'none' }, height: '200px' },
+    xl: { display: { xs: 'none', xl: 'block' }, height: '200px' },
   };
 
-  const [bannerLoading, setBannerLoading] = useState<boolean>(true);
-
-  const styles = useStyles();
-
   return (
-    <Box position="relative" className={styles.bannerSize}>
+    <>
       {Object.keys(sizes).map(size => (
         <Box sx={sizes[size]}>
-          <img
-            src={bannerUrl.replace('{size}', size)}
-            alt="banner"
-            onLoad={() => setBannerLoading(false)}
-            className={styles.bannerSize}
-            style={{ width: '100%' }}
+          <BannerImageElement
+            style={{
+              backgroundImage: `url('./alkemio-banner/alkemio-banner-${size}.png')`,
+              height: sizes[size].height,
+            }}
           />
         </Box>
       ))}
-      <div className={styles.overlay}></div>
-      {bannerLoading && (
-        <Paper square sx={{ position: 'absolute', inset: 0 }}>
-          <Skeleton variant="rectangular" animation="wave" sx={{ height: '100%' }} />
-        </Paper>
-      )}
-    </Box>
+    </>
   );
 };
 
