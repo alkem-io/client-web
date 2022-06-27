f3e605fa749c48b4aeeb2a9620eb772644902a38
Carlos Cano <carlos@alkem.io>
2022-06-27 16:36:56 +0200

    Logo changed

--- src/components/composite/layout/TopBar/LogoComponent.tsx
+++ src/components/composite/layout/TopBar/LogoComponent.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { styled } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { ReactComponent as LogoPreview } from './logo-preview.svg';
+import { ReactComponent as LogoImage } from './alkemio-logo.svg';
 
-const Logo = styled(LogoPreview)(({ theme }) => ({
-  height: theme.spacing(5),
+const Logo = styled(LogoImage)(({ theme }) => ({
+  height: theme.spacing(4),
 }));
 
 const LogoComponent = () => {
