bbefd489354db7bf51f422317b8a4871be157b1e
Carlos Cano <carlos@alkem.io>
2022-06-28 12:22:51 +0200

    Home page top section review #2171  (#2250)
    * Home page review
    
    * Removed ShowInterestSection
    
    * Remove GettingStartedSection
    
    * Logo changed
    
    * Remove unused
    
    * Clean up
    
    * Better BannerImage component
    
    * Skeleton for user loading
    
    * Wrap in grow
    
    * Grow better
    
    * Even better Grow
    
    * BannerImage is a Box
    
    * Top level properties for the H1
    
    * Small fix for screens size lg
    
    * Restore logo-preview
    
    * Fix console error
    
    * Add forwardRef to the HomePage components
    
    * cleanup
    
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>
    Co-authored-by: Andrew Pazniak <andrew@yess.llc>

---
+++ src/pages/Home/BannerImage.tsx
@@ -0,0 +1,34 @@
+import { Box, styled } from '@mui/material';
+
+const BannerImageElement = styled(Box)(({ theme }) => ({
+  background: theme.palette.neutralMedium.light,
+}));
+
+const BannerImage = () => {
+  // https://mui.com/system/display/
+  // https://mui.com/material-ui/customization/breakpoints/#custom-breakpoints
+  const sizes = {
+    xs: { display: { xs: 'block', sm: 'none' }, marginTop: 8, height: '156px' },
+    sm: { display: { xs: 'none', sm: 'block', md: 'none' }, height: '200px' },
+    md: { display: { xs: 'none', md: 'block', lg: 'none' }, marginTop: 2, height: '200px' },
+    lg: { display: { xs: 'none', lg: 'block', xl: 'none' }, height: '200px' },
+    xl: { display: { xs: 'none', xl: 'block' }, height: '200px' },
+  };
+
+  return (
+    <>
+      {Object.keys(sizes).map(size => (
+        <Box sx={sizes[size]} key={`banner-image-${size}`}>
+          <BannerImageElement
+            style={{
+              backgroundImage: `url('./alkemio-banner/alkemio-banner-${size}.png')`,
+              height: sizes[size].height,
+            }}
+          />
+        </Box>
+      ))}
+    </>
+  );
+};
+
+export default BannerImage;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,63 +1,30 @@
-import { Grid } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { Grid, Grow } from '@mui/material';
 import React from 'react';
 import { SectionSpacer } from '../../domain/shared/components/Section/Section';
 import { useUpdateNavigation, useUserContext } from '../../hooks';
-import AlkemioActivitySection from './AlkemioActivitySection';
 import ContributorsSection from './ContributorsSection';
 import HubsSection from '../../domain/hub/DashboardHubs/HubsSection';
-import GettingStartedSection from './GettingStartedSection';
-import ShowInterestSection from './ShowInterestSection';
 import WelcomeSection from './WelcomeSection';
-import MyHubsSection from '../../domain/hub/MyHubs/MyHubsSection';
 import HomePageFooter from './HomePageFooter';
-
-const useStyles = makeStyles(_ => ({
-  grow: {
-    flexGrow: 1,
-  },
-}));
+import AnonymousUserHome from './AnonymousUserHome';
+import AuthenticatedUserHome from './AuthenticatedUserHome';
 
 const currentPaths = [];
 
 export const HomePage = () => {
   useUpdateNavigation({ currentPaths });
 
-  const styles = useStyles();
-
   const user = useUserContext();
 
   return (
     <>
-      <SectionSpacer />
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6} display="flex" flexDirection="column">
+        <Grid item xs={12}>
           <WelcomeSection />
-          <SectionSpacer />
-          <AlkemioActivitySection
-            classes={{
-              section: {
-                root: styles.grow,
-              },
-            }}
-          />
         </Grid>
-        <Grid item xs={12} md={6} display="flex" flexDirection="column">
-          <GettingStartedSection />
-          <SectionSpacer />
-          <ShowInterestSection
-            classes={{
-              section: {
-                root: styles.grow,
-              },
-            }}
-          />
-        </Grid>
-        {user.isAuthenticated && (
-          <Grid item xs={12}>
-            <MyHubsSection userHubRoles={user.userHubRoles} loading={user.loading} />
-          </Grid>
-        )}
+        <Grow appear={!user.loading}>
+          {user.isAuthenticated ? <AuthenticatedUserHome user={user} /> : <AnonymousUserHome />}
+        </Grow>
         <Grid item xs={12}>
           <HubsSection />
         </Grid>
