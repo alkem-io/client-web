afbb6920780c1ca168f8fb4c6a3afa5390dcac66
Carlos Cano <carlos@alkem.io>
2022-06-28 10:11:02 +0200

    Restore logo-preview

--- src/components/composite/layout/TopBar/LogoComponent.tsx
+++ src/components/composite/layout/TopBar/LogoComponent.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { styled } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { ReactComponent as LogoImage } from './alkemio-logo.svg';
+import { ReactComponent as LogoImage } from './logo-preview.svg';
 
 const Logo = styled(LogoImage)(({ theme }) => ({
-  height: theme.spacing(4),
+  height: theme.spacing(5),
 }));
 
 const LogoComponent = () => {
