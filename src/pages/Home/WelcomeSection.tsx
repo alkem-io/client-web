7dda30567a422cd4028122243d2f111744ce3ed6
Andrew Pazniak <andrew@yess.llc>
2022-06-28 13:02:56 +0300

    cleanup

--- src/pages/Home/WelcomeSection.tsx
+++ src/pages/Home/WelcomeSection.tsx
@@ -23,7 +23,7 @@ const WelcomeSection = () => {
         lineHeight={theme.spacing(4)}
         sx={{
           marginTop: theme.spacing(2),
-          [`${theme.breakpoints.only('lg')}`]: {
+          [theme.breakpoints.only('lg')]: {
             marginLeft: '4em',
             marginRight: '4em',
           },
