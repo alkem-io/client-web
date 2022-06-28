e35a836be2f9e2201eaaa55d382524a366c16e17
Carlos Cano <carlos@alkem.io>
2022-06-28 11:06:38 +0200

    Add forwardRef to the HomePage components

--- src/pages/Home/AuthenticatedUserHome.tsx
+++ src/pages/Home/AuthenticatedUserHome.tsx
@@ -1,3 +1,4 @@
+import React, { forwardRef } from 'react';
 import MyHubsSection from '../../domain/hub/MyHubs/MyHubsSection';
 import { useTranslation } from 'react-i18next';
 import { UserContextValue } from '../../domain/user/providers/UserProvider/UserProvider';
@@ -8,17 +9,17 @@ interface AuthenticatedUserHomeProps {
   user: UserContextValue;
 }
 
-const AuthenticatedUserHome = ({ user }: AuthenticatedUserHomeProps) => {
+const AuthenticatedUserHome = forwardRef<HTMLDivElement, AuthenticatedUserHomeProps>(({ user }, ref) => {
   const { t } = useTranslation();
 
   return (
-    <Grid item xs={12}>
+    <Grid item xs={12} ref={ref}>
       <Typography variant="h3" weight="bold">
         {t('pages.home.sections.welcome.welcome-back', { username: user.user?.user.firstName })}
       </Typography>
       <MyHubsSection userHubRoles={user.userHubRoles} loading={user.loading} />
     </Grid>
   );
-};
+});
 
 export default AuthenticatedUserHome;
