2adb36b963a94ff39c27f6cbee01664013845853
Carlos Cano <carlos@alkem.io>
2022-07-07 09:00:14 +0200

    Better help icon

--- src/components/composite/layout/TopBar/HelpIcon.tsx
+++ src/components/composite/layout/TopBar/HelpIcon.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { Box } from '@mui/material';
 
 const HelpIcon = () => {
   return (
-    <Link to="/help">
-      <HelpOutlineIcon color="primary" />
-    </Link>
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Link to="/help">
+        <HelpOutlineIcon color="primary" />
+      </Link>
+    </Box>
   );
 };
 export default HelpIcon;
