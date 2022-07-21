5beec4177283b0c96c9e65c5ced101493a924f18
Carlos Cano <carlos@alkem.io>
2022-07-21 16:31:35 +0200

    Move tabs to a common component

---
+++ src/domain/shared/components/PageHeader/HeaderNavigationTabs.tsx
@@ -0,0 +1,81 @@
+import { SettingsOutlined } from '@mui/icons-material';
+import { Box, styled, Tabs, TabsProps } from '@mui/material';
+import { FC } from 'react';
+import HeaderNavigationTab from './HeaderNavigationTab';
+
+const Root = styled(Box)(({ theme }) => ({
+  position: 'relative',
+  '.settings-button': {
+    color: theme.palette.common.white,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    minHeight: theme.spacing(4),
+  },
+}));
+
+const StyledTabs = styled(Tabs)(({ theme }) => ({
+  minHeight: 0,
+  color: theme.palette.common.white,
+  backgroundColor: theme.palette.primary.main,
+  [theme.breakpoints.up('xs')]: {
+    paddingLeft: 0,
+    paddingRight: theme.spacing(5),
+  },
+  [theme.breakpoints.up('lg')]: {
+    paddingLeft: theme.spacing(8),
+    paddingRight: theme.spacing(8),
+  },
+  '& .MuiTabs-indicator': {
+    backgroundColor: theme.palette.common.white,
+    bottom: 2,
+    height: 2,
+  },
+  '& .MuiTabs-flexContainer': {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  '& .MuiTab-root': {
+    fontSize: theme.typography.button.fontSize,
+    flexGrow: 1,
+    minHeight: theme.spacing(4),
+  },
+}));
+
+export interface NavigationTabsProps extends TabsProps {
+  showSettings: boolean;
+  settingsValue: string;
+  settingsUrl: string;
+}
+
+const HeaderNavigationTabs: FC<NavigationTabsProps> = ({
+  value,
+  'aria-label': ariaLabel,
+  showSettings,
+  settingsValue,
+  settingsUrl,
+  children,
+}) => {
+  return (
+    <Root>
+      <StyledTabs
+        value={value}
+        aria-label={ariaLabel}
+        variant="scrollable"
+        scrollButtons={'auto'}
+        allowScrollButtonsMobile
+      >
+        {children}
+      </StyledTabs>
+      {showSettings && (
+        <HeaderNavigationTab
+          className="settings-button"
+          icon={<SettingsOutlined />}
+          value={settingsValue}
+          to={settingsUrl}
+        />
+      )}
+    </Root>
+  );
+};
+export default HeaderNavigationTabs;
--- src/routing/hub/HubTabs.tsx
+++ src/routing/hub/HubTabs.tsx
@@ -1,4 +1,3 @@
-import { Box, styled, Tabs } from '@mui/material';
 import React, { FC, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Outlet, resolvePath, useResolvedPath } from 'react-router-dom';
@@ -9,8 +8,8 @@ import useRouteMatch from '../../hooks/routing/useRouteMatch';
 import { FEATURE_COLLABORATION_CANVASES, FEATURE_COMMUNICATIONS_DISCUSSIONS } from '../../models/constants';
 import { buildAdminHubUrl } from '../../utils/urlBuilders';
 import PageBanner from '../../domain/shared/components/PageHeader/PageBanner';
-import { SettingsOutlined } from '@mui/icons-material';
 import { getVisualBanner } from '../../utils/visuals.utils';
+import HeaderNavigationTabs from '../../domain/shared/components/PageHeader/HeaderNavigationTabs';
 
 const routes = {
   discussions: 'discussions',
@@ -23,45 +22,6 @@ const routes = {
   context: 'context',
   contribute: 'contribute',
 } as const;
-
-const TabsContainer = styled(Box)(({ theme }) => ({
-  position: 'relative',
-  '.settings-button': {
-    color: theme.palette.common.white,
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    minHeight: theme.spacing(4),
-  },
-}));
-
-const StyledTabs = styled(Tabs)(({ theme }) => ({
-  minHeight: 0,
-  color: theme.palette.common.white,
-  backgroundColor: theme.palette.primary.main,
-  [theme.breakpoints.up('xs')]: {
-    paddingLeft: 0,
-    paddingRight: theme.spacing(5),
-  },
-  [theme.breakpoints.up('lg')]: {
-    paddingLeft: theme.spacing(8),
-    paddingRight: theme.spacing(8),
-  },
-  '& .MuiTabs-indicator': {
-    backgroundColor: theme.palette.common.white,
-    bottom: 2,
-    height: 2,
-  },
-  '& .MuiTabs-flexContainer': {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  '& .MuiTab-root': {
-    fontSize: theme.typography.button.fontSize,
-    flexGrow: 1,
-    minHeight: theme.spacing(4),
-  },
-}));
 
 type HubRoutesType = keyof typeof routes;
 
@@ -104,55 +64,45 @@ const HubTabs: FC<HubTabsProps> = ({ hubNameId, permissions }) => {
         loading={loading}
         bannerUrl={getVisualBanner(context?.visuals)}
       />
-      <TabsContainer>
-        <StyledTabs
-          value={currentTab}
-          aria-label="Hub tabs"
-          variant="scrollable"
-          scrollButtons={'auto'}
-          allowScrollButtonsMobile
-        >
-          <NavigationTab label={t('common.dashboard')} value={tabValue('dashboard')} to={routes.dashboard} />
-          <NavigationTab label={t('common.context')} value={tabValue('context')} to={'context'} />
+      <HeaderNavigationTabs
+        value={currentTab}
+        aria-label="Hub tabs"
+        showSettings={permissions.viewerCanUpdate}
+        settingsValue={tabValue('settings')}
+        settingsUrl={buildAdminHubUrl(hubNameId)}
+      >
+        <NavigationTab label={t('common.dashboard')} value={tabValue('dashboard')} to={routes.dashboard} />
+        <NavigationTab label={t('common.context')} value={tabValue('context')} to={'context'} />
+        <NavigationTab
+          disabled={!permissions.communityReadAccess}
+          label={t('common.community')}
+          value={tabValue('community')}
+          to={routes.community}
+        />
+        <NavigationTab label={t('common.contribute')} value={tabValue('contribute')} to={routes.contribute} />
+        <NavigationTab
+          disabled={!permissions.canReadChallenges}
+          label={t('common.challenges')}
+          value={tabValue('challenges')}
+          to={routes.challenges}
+        />
+        {isFeatureEnabled(FEATURE_COMMUNICATIONS_DISCUSSIONS) && (
           <NavigationTab
             disabled={!permissions.communityReadAccess}
-            label={t('common.community')}
-            value={tabValue('community')}
-            to={routes.community}
-          />
-          <NavigationTab label={t('common.contribute')} value={tabValue('contribute')} to={routes.contribute} />
-          <NavigationTab
-            disabled={!permissions.canReadChallenges}
-            label={t('common.challenges')}
-            value={tabValue('challenges')}
-            to={routes.challenges}
-          />
-          {isFeatureEnabled(FEATURE_COMMUNICATIONS_DISCUSSIONS) && (
-            <NavigationTab
-              disabled={!permissions.communityReadAccess}
-              label={t('common.discussions')}
-              value={tabValue('discussions')}
-              to={routes.discussions}
-            />
-          )}
-          {isFeatureEnabled(FEATURE_COLLABORATION_CANVASES) && (
-            <NavigationTab
-              disabled={!permissions.communityReadAccess}
-              label={t('common.canvases')}
-              value={tabValue('canvases')}
-              to={routes.canvases}
-            />
-          )}
-        </StyledTabs>
-        {permissions.viewerCanUpdate && (
-          <NavigationTab
-            className="settings-button"
-            icon={<SettingsOutlined />}
-            value={tabValue('settings')}
-            to={buildAdminHubUrl(hubNameId)}
+            label={t('common.discussions')}
+            value={tabValue('discussions')}
+            to={routes.discussions}
           />
         )}
-      </TabsContainer>
+        {isFeatureEnabled(FEATURE_COLLABORATION_CANVASES) && (
+          <NavigationTab
+            disabled={!permissions.communityReadAccess}
+            label={t('common.canvases')}
+            value={tabValue('canvases')}
+            to={routes.canvases}
+          />
+        )}
+      </HeaderNavigationTabs>
       <Outlet />
     </>
   );
