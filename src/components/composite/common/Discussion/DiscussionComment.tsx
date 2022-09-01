57730c04225e804557eacef6aea5bff97ec12890
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2022-09-01 18:00:26 +0200

    Update DiscussionComment component.

--- src/components/composite/common/Discussion/DiscussionComment.tsx
+++ src/components/composite/common/Discussion/DiscussionComment.tsx
@@ -1,20 +1,21 @@
 import React, { FC } from 'react';
 import { DeleteOutlined } from '@mui/icons-material';
-import { Avatar, Box, Paper, Typography, styled, AvatarProps, BoxProps, IconButton } from '@mui/material';
+import { Avatar, Box, Paper, Typography, styled, AvatarProps, BoxProps, IconButton, Grid } from '@mui/material';
 import { Comment } from '../../../../models/discussion/comment';
 import Markdown from '../../../core/Markdown';
 
-const AVATAR_SIZE = 5;
+const AVATAR_SIZE = 7;
 
 const CommentBox = styled(props => <Box {...props} />)<BoxProps>(({ theme }) => ({
   overflowWrap: 'break-word',
-  padding: theme.spacing(1),
+  paddingX: theme.spacing(1),
+  '& > p:first-child': { marginTop: 0 },
+  '& > p:last-child': { marginBottom: 0 },
 }));
 
 const UserAvatar = styled(props => <Avatar {...props} />)<AvatarProps>(({ theme }) => ({
   height: theme.spacing(AVATAR_SIZE),
   width: theme.spacing(AVATAR_SIZE),
-  marginRight: theme.spacing(2),
 }));
 
 interface DiscussionCommentProps {
@@ -28,40 +29,66 @@ export const DiscussionComment: FC<DiscussionCommentProps> = ({ comment, canDele
   const { author, body, id } = comment;
 
   return (
-    <Box
-      component={Paper}
-      elevation={1}
-      bgcolor={isRootComment ? 'background.paper' : 'background.default'}
-      borderRadius={1}
-    >
-      <Box
-        padding={1}
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        bgcolor={isRootComment ? 'background.paper' : 'neutralMedium.light'}
-      >
-        <Box display="flex" alignItems="center">
-          <UserAvatar src={author?.avatarUrl} variant="rounded">
-            {author?.displayName[0]}
-          </UserAvatar>
-          <Box>
-            <Typography variant="body1">{comment.author?.displayName}</Typography>
-            <Typography variant="body2" color="neutralMedium.dark">{`${
-              isRootComment ? 'started the discussion on ' : 'on '
-            }${comment.createdAt.toLocaleString()}`}</Typography>
-          </Box>
+    <Grid container spacing={1.5}>
+      <Grid item>
+        <UserAvatar src={author?.avatarUrl} variant="rounded" sx={{ borderRadius: 1.5 }}>
+          {author?.displayName[0]}
+        </UserAvatar>
+      </Grid>
+      <Grid item xs={10}>
+        <Box
+          padding={1}
+          display="flex"
+          alignItems="top"
+          justifyContent="space-between"
+          bgcolor={isRootComment ? 'background.paper' : 'neutralMedium.light'}
+          borderRadius={1.5}
+          component={Paper}
+          elevation={1}
+          marginRight={1}
+        >
+          <Grid container direction="column">
+            <Grid item>
+              <Grid container spacing={1}>
+                <Grid item>
+                  <Typography variant="h6" fontWeight={600}>
+                    {author?.displayName}
+                  </Typography>
+                </Grid>
+                <Grid item xs>
+                  <Typography variant="h6" color="neutralMedium.dark" sx={{ fontStyle: 'italic' }}>
+                    Host
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  {canDelete && onDelete && (
+                    <IconButton aria-label="Delete" onClick={() => onDelete(id)} size={'small'}>
+                      <DeleteOutlined />
+                    </IconButton>
+                  )}
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item zeroMinWidth>
+                <Box>
+                  <CommentBox>
+                    <Markdown>{body}</Markdown>
+                  </CommentBox>
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Box display="flex" justifyContent="flex-end">
+                <Typography variant="body2" color="neutralMedium.dark">
+                  {`${isRootComment ? 'started the discussion ' : ' '}${comment.createdAt.toLocaleString()}`}
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
-        {canDelete && onDelete && (
-          <IconButton aria-label="Delete" onClick={() => onDelete(id)} size={'large'}>
-            <DeleteOutlined />
-          </IconButton>
-        )}
-      </Box>
-      <CommentBox>
-        <Markdown>{body}</Markdown>
-      </CommentBox>
-    </Box>
+      </Grid>
+    </Grid>
   );
 };
 export default DiscussionComment;
