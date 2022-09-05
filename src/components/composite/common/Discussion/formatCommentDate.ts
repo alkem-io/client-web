67058c652b7fd03db2953c78f0cbc5ebea752c50
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2022-09-05 10:05:47 +0200

    Update comment time formating

--- src/components/composite/common/Discussion/formatCommentDate.ts
+++ src/components/composite/common/Discussion/formatCommentDate.ts
@@ -1,4 +1,7 @@
-const ONE_DAY = 1000 * 60 * 60 * 24;
+const ONE_SECOND = 1000;
+const ONE_MINUTE = 60 * ONE_SECOND;
+const ONE_HOUR = 60 * ONE_MINUTE;
+const ONE_DAY = 24 * ONE_HOUR;
 const ONE_MONTH = 31 * ONE_DAY;
 const ONE_YEAR = 256 * ONE_DAY;
 
@@ -8,14 +11,29 @@ export const formatCommentDate = (commentDate: Date): string => {
   const diffInTime = currentDate.getTime() - commentDate.getTime();
 
   if (diffInTime > ONE_YEAR) {
-    const yearsDiff = Math.round(diffInTime / ONE_YEAR);
-    return `${yearsDiff} month${yearsDiff === 1 ? '' : 's'} ago`;
+    const timeDiff = Math.round(diffInTime / ONE_YEAR);
+    return `${timeDiff} month${timeDiff === 1 ? '' : 's'} ago`;
   }
   if (diffInTime > ONE_MONTH) {
-    const montsDiff = Math.round(diffInTime / ONE_MONTH);
-    return `${montsDiff} month${montsDiff === 1 ? '' : 's'} ago`;
+    const timeDiff = Math.round(diffInTime / ONE_MONTH);
+    return `${timeDiff} month${timeDiff === 1 ? '' : 's'} ago`;
   }
 
-  const dayssDiff = Math.round(diffInTime / ONE_DAY);
-  return `${dayssDiff} day${dayssDiff === 1 ? '' : 's'} ago`;
+  if (diffInTime > ONE_DAY) {
+    const timeDiff = Math.round(diffInTime / ONE_DAY);
+    return `${timeDiff} day${timeDiff === 1 ? '' : 's'} ago`;
+  }
+
+  if (diffInTime > ONE_HOUR) {
+    const timeDiff = Math.round(diffInTime / ONE_HOUR);
+    return `${timeDiff} hour${timeDiff === 1 ? '' : 's'} ago`;
+  }
+
+  if (diffInTime > ONE_MINUTE) {
+    const timeDiff = Math.round(diffInTime / ONE_MINUTE);
+    return `${timeDiff} minute${timeDiff === 1 ? '' : 's'} ago`;
+  }
+
+  const timeDiff = Math.round(diffInTime / ONE_SECOND);
+  return `${timeDiff} second${timeDiff === 1 ? '' : 's'} ago`;
 };
