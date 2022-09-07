920bd5e1057929101aefa79e02ebbd7d03075f4c
Carlos Cano <carlos@alkem.io>
2022-09-07 18:09:43 +0200

    Comments 3 author role

--- src/domain/communication/useAuthorsDetails.ts
+++ src/domain/communication/useAuthorsDetails.ts
@@ -4,10 +4,56 @@ import { buildUserProfileUrl } from '../../common/utils/urlBuilders';
 import { useAuthorDetailsQuery } from '../../hooks/generated/graphql';
 import { uniq } from 'lodash';
 import { COUNTRIES_BY_CODE } from '../../models/constants';
-import { User } from '../../models/graphql-schema';
-import { useChallenge, useHub, useOpportunity, useUserCardRoleName } from '../../hooks';
+import { AuthorizationCredential } from '../../models/graphql-schema';
+import { useChallenge, useHub, useOpportunity, WithCredentials } from '../../hooks';
+import { useTranslation } from 'react-i18next';
+import {
+  AuthorRoleNameKey,
+  HOST_TRANSLATION_KEY,
+  LEAD_TRANSLATION_KEY,
+} from '../../models/constants/translation.constants';
+
+type ResourceTypeName = 'hub' | 'challenge' | 'opportunity';
+
+function findRoleNameKey(
+  user: WithCredentials,
+  reourceType: ResourceTypeName,
+  resourceId: string
+): AuthorRoleNameKey | undefined {
+  switch (reourceType) {
+    case 'hub': {
+      if (
+        user.agent?.credentials?.find(c => c.resourceID === resourceId && c.type === AuthorizationCredential.HubHost)
+      ) {
+        return HOST_TRANSLATION_KEY;
+      }
+      break;
+    }
+    case 'challenge': {
+      if (
+        user.agent?.credentials?.find(
+          c => c.resourceID === resourceId && c.type === AuthorizationCredential.ChallengeLead
+        )
+      ) {
+        return LEAD_TRANSLATION_KEY;
+      }
+      break;
+    }
+    case 'opportunity': {
+      if (
+        user.agent?.credentials?.find(
+          c => c.resourceID === resourceId && c.type === AuthorizationCredential.OpportunityLead
+        )
+      ) {
+        return LEAD_TRANSLATION_KEY;
+      }
+      break;
+    }
+  }
+}
 
 export const useAuthorsDetails = (authorIds: string[]) => {
+  const { t } = useTranslation();
   const uniqIds = uniq(authorIds).sort();
 
   const { data: authorData, loading } = useAuthorDetailsQuery({
@@ -19,24 +65,27 @@ export const useAuthorsDetails = (authorIds: string[]) => {
   const { opportunityId } = useOpportunity();
 
   const resourceId = opportunityId || challengeId || hubId || '';
-
-  const usersWithRoles = useUserCardRoleName((authorData?.usersById || []) as User[], resourceId);
+  const reourceType: ResourceTypeName = opportunityId ? 'opportunity' : challengeId ? 'challenge' : 'hub';
 
   const authors = useMemo(
     () =>
-      authorData?.usersById.map<Author>(a => ({
-        id: a.id,
-        displayName: a.displayName,
-        firstName: a.firstName,
-        lastName: a.lastName,
-        avatarUrl: a.profile?.avatar?.uri || '',
-        url: buildUserProfileUrl(a.nameID),
-        tags: a.profile?.tagsets?.flatMap(x => x.tags),
-        city: a.profile?.location?.city,
-        country: COUNTRIES_BY_CODE[a.profile?.location?.country || ''],
-        roleName: usersWithRoles.find(u => u.id === a.id)?.roleName,
-      })),
-    [authorData]
+      authorData?.usersById.map<Author>(author => {
+        const roleNameKey = findRoleNameKey(author, reourceType, resourceId);
+        const roleName = roleNameKey ? t(roleNameKey) : '';
+        return {
+          id: author.id,
+          displayName: author.displayName,
+          firstName: author.firstName,
+          lastName: author.lastName,
+          avatarUrl: author.profile?.avatar?.uri || '',
+          url: buildUserProfileUrl(author.nameID),
+          tags: author.profile?.tagsets?.flatMap(x => x.tags),
+          city: author.profile?.location?.city,
+          country: COUNTRIES_BY_CODE[author.profile?.location?.country || ''],
+          roleName: roleName,
+        };
+      }),
+    [authorData, resourceId]
   );
 
   const getAuthor = useCallback((id: string) => authors?.find(a => a.id === id), [authors]);
--- src/models/constants/translation.constants.ts
+++ src/models/constants/translation.constants.ts
@@ -1,5 +1,8 @@
 export const OWNER_TRANSLATION_KEY = 'common.owner';
 export const ADMIN_TRANSLATION_KEY = 'common.admin';
 export const MEMBER_TRANSLATION_KEY = 'common.member';
+export const LEAD_TRANSLATION_KEY = 'common.lead';
+export const HOST_TRANSLATION_KEY = 'common.host';
 
 export type RoleNameKey = typeof OWNER_TRANSLATION_KEY | typeof ADMIN_TRANSLATION_KEY | typeof MEMBER_TRANSLATION_KEY;
+export type AuthorRoleNameKey = typeof LEAD_TRANSLATION_KEY | typeof HOST_TRANSLATION_KEY;
