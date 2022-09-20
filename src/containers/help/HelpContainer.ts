cdf37e47a3fdcabd62a37ec61f8876fbc2b47800
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-09-20 17:23:50 +0300

    Help

--- src/containers/help/HelpContainer.ts
+++ src/containers/help/HelpContainer.ts
@@ -1,5 +1,5 @@
 import { FC, useEffect, useState } from 'react';
-import { HelpApi } from '../../api/HelpApi';
+import { HelpApi } from '../../core/help/api/HelpApi';
 import {
   ComponentOrChildrenFn,
   renderComponentOrChildrenFn,
