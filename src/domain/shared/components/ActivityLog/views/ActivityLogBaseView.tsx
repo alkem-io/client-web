ac0880841e4128c844407b62fd6df6c8029032da
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2022-09-28 16:05:02 +0200

    Fix server date handling.

--- src/domain/shared/components/ActivityLog/views/ActivityLogBaseView.tsx
+++ src/domain/shared/components/ActivityLog/views/ActivityLogBaseView.tsx
@@ -7,6 +7,7 @@ import { formatTimeElapsed } from '../../../utils/formatTimeElapsed';
 import AuthorAvatar from '../../AuthorAvatar/AuthorAvatar';
 import { Author } from '../../AuthorAvatar/models/author';
 import { ClampedTypography } from '../../ClampedTypography';
+import { adjustServerDateForTimezone } from '../../../utils/adjustServerDateForTimezone';
 
 export interface ActivityLogBaseViewProps {
   author: Author | undefined;
@@ -24,8 +25,8 @@ export const ActivityLogBaseView: FC<ActivityLogBaseViewProps> = ({
   loading,
 }) => {
   const { t } = useTranslation();
-
-  const formattedTime = useMemo(() => formatTimeElapsed(createdDate), [createdDate]);
+  const adjustedDate = adjustServerDateForTimezone(createdDate);
+  const formattedTime = useMemo(() => formatTimeElapsed(adjustedDate), [createdDate]);
 
   const title = useMemo(
     () => `${formattedTime} ${author?.displayName ?? t('common.user')} ${action}`,
