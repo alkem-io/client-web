5521701999257e89da3c30823f2dfc34cb9f62c4
Carlos Cano <carlos@alkem.io>
2022-09-30 11:41:13 +0200

    Squashed commit of the following:
    commit 403bc5c04faab760e10bb6545ded8c11cb4ac12c
    Author: Neil Smyth <30729240+techsmyth@users.noreply.github.com>
    Date:   Fri Sep 30 09:09:29 2022 +0200
    
        Update Crowdin configuration file
    
    commit 59ad27f4fdeab1891a994b7c922177f028158c02
    Author: Svetoslav Petkov <svetoslav@alkem.io>
    Date:   Fri Sep 30 09:41:31 2022 +0300
    
        create buttons for challenge & opportunity under the normal user pages (#2689)

--- src/domain/challenge/hub/pages/HubChallengesPage.tsx
+++ src/domain/challenge/hub/pages/HubChallengesPage.tsx
@@ -1,20 +1,35 @@
 import React, { FC, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Box, Button } from '@mui/material';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import { useHub, useUpdateNavigation } from '../../../../hooks';
 import { PageProps } from '../../../../pages/common';
 import HubChallengesView from '../views/HubChallengesView';
 import ChallengesCardContainer from '../../../../containers/hub/ChallengesCardContainer';
 import { EntityPageSection } from '../../../shared/layout/EntityPageSection';
 import HubPageLayout from '../layout/HubPageLayout';
+import { RouterLink } from '../../../../common/components/core/RouterLink';
+import { buildAdminNewChallengeUrl } from '../../../../common/utils/urlBuilders';
 
 export interface HubChallengesPageProps extends PageProps {}
 
 const HubChallengesPage: FC<HubChallengesPageProps> = ({ paths }) => {
+  const { t } = useTranslation();
   const { hubNameId, permissions } = useHub();
   const currentPaths = useMemo(() => [...paths, { value: '', name: 'challenges', real: false }], [paths]);
   useUpdateNavigation({ currentPaths });
 
+  const newChallengeUrl = useMemo(() => buildAdminNewChallengeUrl(hubNameId), [hubNameId]);
+
   return (
     <HubPageLayout currentSection={EntityPageSection.Challenges}>
+      {permissions.canCreate && (
+        <Box sx={{ display: 'flex', justifyContent: 'end', marginBottom: theme => theme.spacing(1) }}>
+          <Button startIcon={<AddOutlinedIcon />} variant="contained" component={RouterLink} to={newChallengeUrl}>
+            {t('buttons.create')}
+          </Button>
+        </Box>
+      )}
       <ChallengesCardContainer hubNameId={hubNameId}>
         {(entities, state) => (
           <HubChallengesView
