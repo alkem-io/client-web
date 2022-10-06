983036b8f87bd9b0e4e158088f455d8fb2fcf89e
Carlos Cano <carlos@alkem.io>
2022-10-06 11:12:52 +0200

    buildCalloutUrl

--- src/common/utils/urlBuilders.ts
+++ src/common/utils/urlBuilders.ts
@@ -67,6 +67,26 @@ export const buildNewOrganizationUrl = () => {
   return '/admin/organizations/new';
 };
 
+export const buildCalloutUrl = (
+  calloutNameId: string,
+  hubNameId: string | undefined,
+  challengeNameId?: string,
+  opportunityNameId?: string
+) => {
+  if (!hubNameId) return '';
+
+  const calloutUrl = `/${EntityPageSection.Explore}/callouts/${calloutNameId}`;
+  if (challengeNameId) {
+    if (opportunityNameId) {
+      return `${buildOpportunityUrl(hubNameId, challengeNameId, opportunityNameId)}${calloutUrl}`;
+    } else {
+      return `${buildChallengeUrl(hubNameId, challengeNameId)}${calloutUrl}`;
+    }
+  } else {
+    return `${buildHubUrl(hubNameId)}${calloutUrl}`;
+  }
+};
+
 interface buildAspectUrlParams {
   hubNameId: string;
   challengeNameId?: string;
