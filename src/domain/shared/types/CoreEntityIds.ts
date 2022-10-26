01978384652a8836c1f058ef07346c1226699f9e
Andrew Pazniak <andrew@yess.llc>
2022-10-26 11:30:35 +0300

    improved typings in urlBuilders

--- src/domain/shared/types/CoreEntityIds.ts
+++ src/domain/shared/types/CoreEntityIds.ts
@@ -4,19 +4,21 @@ type Required<SourceType extends {}, Props extends keyof SourceType> = {
   [P in Exclude<keyof SourceType, Props>]?: undefined;
 };
 
-interface CoreEntityIdTypes {
+export interface CoreEntityIdTypes {
   hubNameId: string;
-  challengeNameId: string;
-  opportunityNameId: string;
+  challengeNameId?: string;
+  opportunityNameId?: string;
 }
 
 export type OptionalCoreEntityIds = Partial<CoreEntityIdTypes>;
 
-type HubIdHolder = Required<OptionalCoreEntityIds, 'hubNameId'>;
-type ChallengeIdHolder = Required<OptionalCoreEntityIds, 'hubNameId' | 'challengeNameId'>;
-type OpportunityIdHolder = Required<OptionalCoreEntityIds, 'hubNameId' | 'opportunityNameId'>;
-
-export type CoreEntityIdCombinations = HubIdHolder | ChallengeIdHolder | OpportunityIdHolder;
+export type HubIdHolder = Required<CoreEntityIdTypes, 'hubNameId'>;
+export type ChallengeIdHolder = Required<CoreEntityIdTypes, 'hubNameId' | 'challengeNameId'>;
+export type OpportunityIdHolder = Required<CoreEntityIdTypes, 'hubNameId' | 'opportunityNameId'>;
+export type ChallengeOpportunityIdsHolder = Required<
+  CoreEntityIdTypes,
+  'hubNameId' | 'challengeNameId' | 'opportunityNameId'
+>;
 
 export const isHubId = (ids: OptionalCoreEntityIds): ids is HubIdHolder =>
   typeof ids.challengeNameId === 'undefined' && typeof ids.opportunityNameId === 'undefined';
@@ -24,3 +26,5 @@ export const isChallengeId = (ids: OptionalCoreEntityIds): ids is ChallengeIdHol
   typeof ids.challengeNameId === 'string' && typeof ids.opportunityNameId === 'undefined';
 export const isOpportunityId = (ids: OptionalCoreEntityIds): ids is OpportunityIdHolder =>
   typeof ids.opportunityNameId === 'string';
+export const isChallengeOpportunityIds = (ids: OptionalCoreEntityIds): ids is ChallengeOpportunityIdsHolder =>
+  typeof ids.challengeNameId === 'string' && typeof ids.opportunityNameId === 'string';
