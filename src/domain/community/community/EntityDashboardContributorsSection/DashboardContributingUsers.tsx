608b97b4107c8d0b919628703169ab4ed576a033
Andrew Pazniak <andrew@yess.llc>
2022-10-27 08:54:25 +0300

    On Challenge/Opportunity>About user cards are small

--- src/domain/community/community/EntityDashboardContributorsSection/DashboardContributingUsers.tsx
+++ src/domain/community/community/EntityDashboardContributorsSection/DashboardContributingUsers.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+import React, { useMemo } from 'react';
 import { WithId } from '../../../../types/WithId';
-import {
-  ContributorCard,
-  ContributorCardProps,
-} from '../../../../common/components/composite/common/cards/ContributorCard/ContributorCard';
+import { ContributorCardProps } from '../../../../common/components/composite/common/cards/ContributorCard/ContributorCard';
 import SectionHeader from '../../../shared/components/Section/SectionHeader';
 import withOptionalCount from '../../../shared/utils/withOptionalCount';
 import { SectionSpacer } from '../../../shared/components/Section/Section';
+import LeadUserCard, { LeadUserCardProps } from '../LeadUserCard/LeadUserCard';
+import { Identifiable } from '../../../shared/types/Identifiable';
 
 interface DashboardContributingUsersProps {
   headerText: string;
@@ -16,17 +14,22 @@ interface DashboardContributingUsersProps {
 }
 
 const DashboardContributingUsers = ({ users, usersCount, headerText }: DashboardContributingUsersProps) => {
+  const remappedUsers = useMemo<(LeadUserCardProps & Identifiable)[] | undefined>(() => {
+    return users?.map(({ displayName, avatar, url, ...user }) => ({
+      fullName: displayName,
+      avatarUrl: avatar,
+      userUrl: url,
+      ...user,
+    }));
+  }, [users]);
+
   return (
     <>
       <SectionHeader text={withOptionalCount(headerText, usersCount)} />
       <SectionSpacer />
-      <Grid container spacing={2}>
-        {users?.map(user => (
-          <Grid key={user.id} item xs={3}>
-            <ContributorCard {...user} />
-          </Grid>
-        ))}
-      </Grid>
+      {remappedUsers?.map(user => (
+        <LeadUserCard key={user.id} {...user} />
+      ))}
     </>
   );
 };
