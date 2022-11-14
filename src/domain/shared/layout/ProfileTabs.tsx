9a502d9e1c81dcec3772d0cda12b4fa41da16847
Andrew Pazniak <andrew@yess.llc>
2022-11-14 18:06:38 +0300

    Using BottomNavigation for User/Organization Profile

---
+++ src/domain/shared/layout/ProfileTabs.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { useTheme } from '@mui/styles';
+import { EntityTabsProps } from './PageLayout';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import getEntityColor from '../utils/getEntityColor';
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import { SettingsOutlined, SvgIconComponent } from '@mui/icons-material';
+import { EntityPageSection } from './EntityPageSection';
+import HeaderNavigationTab from '../components/PageHeader/HeaderNavigationTab';
+import hexToRGBA from '../../../common/utils/hexToRGBA';
+import HeaderNavigationTabs from '../components/PageHeader/HeaderNavigationTabs';
+import { FloatingActionButtons } from '../../../common/components/core';
+
+interface ProfileTabsProps extends EntityTabsProps {
+  showSettings: boolean;
+  profileIconComponent: SvgIconComponent;
+  routes: Record<EntityPageSection.Profile | EntityPageSection.Settings, string>;
+}
+
+const ProfileTabs = ({
+  currentTab,
+  showSettings,
+  routes,
+  mobile,
+  profileIconComponent: ProfileIcon,
+}: ProfileTabsProps) => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  const navigationBackgroundColor = getEntityColor(theme, 'profile');
+  const navigationForegroundColor = theme.palette.common.white;
+
+  if (mobile) {
+    return (
+      <>
+        {/*TODO z-index fixes content of Organization settings laying over the navigation tabs*/}
+        {/*TODO investigate why it happens and fix properly*/}
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 1 }} elevation={3}>
+          <BottomNavigation
+            showLabels
+            value={currentTab}
+            onChange={(event, nextValue) => {
+              navigate(routes[nextValue]);
+            }}
+            sx={{
+              backgroundColor: navigationBackgroundColor,
+              '.MuiBottomNavigationAction-root.Mui-selected': {
+                color: navigationForegroundColor,
+              },
+              '.MuiBottomNavigationAction-root:not(.Mui-selected)': {
+                color: hexToRGBA(navigationForegroundColor, 0.75),
+              },
+            }}
+          >
+            <BottomNavigationAction
+              value={EntityPageSection.Profile}
+              label={t('common.profile')}
+              icon={<ProfileIcon />}
+            />
+            {showSettings && (
+              <BottomNavigationAction
+                value={EntityPageSection.Settings}
+                label={t('common.settings')}
+                icon={<SettingsOutlined />}
+              />
+            )}
+          </BottomNavigation>
+        </Paper>
+        <FloatingActionButtons bottom={theme => theme.spacing(10)} />
+      </>
+    );
+  }
+
+  return (
+    <HeaderNavigationTabs
+      value={currentTab}
+      defaultTab={EntityPageSection.Profile}
+      showSettings={showSettings}
+      settingsUrl={routes[EntityPageSection.Settings]}
+    >
+      <HeaderNavigationTab
+        label={t('common.profile')}
+        value={EntityPageSection.Profile}
+        to={routes[EntityPageSection.Profile]}
+        className="singleCenteredTab"
+      />
+    </HeaderNavigationTabs>
+  );
+};
+
+export default ProfileTabs;
--- src/domain/shared/utils/getEntityColor.ts
+++ src/domain/shared/utils/getEntityColor.ts
@@ -1,7 +1,7 @@
 import { Theme } from '@mui/material/styles';
 import { EntityTypeName } from '../layout/PageLayout/SimplePageLayout';
 
-const getEntityColor = (theme: Theme, _entityTypeName: EntityTypeName | 'admin') => {
+const getEntityColor = (theme: Theme, _entityTypeName: EntityTypeName | 'admin' | 'profile') => {
   return theme.palette.primary.main;
 };
 
