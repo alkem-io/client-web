7b336e1e70e1b44472212d9adf6ff1e9a069c5a1
Carlos Cano <carlos@alkem.io>
2022-12-05 15:46:53 +0100

    Add EntityLayout to ApplyRoute

--- src/domain/community/application/routing/apply.route.tsx
+++ src/domain/community/application/routing/apply.route.tsx
@@ -5,20 +5,38 @@ import { PageProps } from '../../../shared/types/PageProps';
 import { Error404 } from '../../../../core/pages/Errors/Error404';
 import ApplyPage from '../pages/ApplyPage';
 import RestrictedRoute from '../../../../core/routing/RestrictedRoute';
+import { EntityPageLayoutHolder } from '../../../shared/layout/PageLayout';
+import ChallengePageLayout from '../../../challenge/challenge/layout/ChallengePageLayout';
+import HubPageLayout from '../../../challenge/hub/layout/HubPageLayout';
+import OpportunityPageLayout from '../../../challenge/opportunity/layout/OpportunityPageLayout';
+import { EntityPageSection } from '../../../shared/layout/EntityPageSection';
 
 interface Props extends PageProps {
   type: ApplicationTypeEnum;
 }
 
+const EntityLayout: React.FC<{ type: ApplicationTypeEnum }> = ({ type, children }) => {
+  switch (type) {
+    case ApplicationTypeEnum.challenge:
+      return <ChallengePageLayout currentSection={EntityPageSection.Dashboard}>{children}</ChallengePageLayout>;
+    case ApplicationTypeEnum.hub:
+      return <HubPageLayout currentSection={EntityPageSection.Dashboard}>{children}</HubPageLayout>;
+    case ApplicationTypeEnum.opportunity:
+      return <OpportunityPageLayout currentSection={EntityPageSection.Dashboard}>{children}</OpportunityPageLayout>;
+  }
+};
+
 const ApplyRoute: FC<Props> = ({ paths, type }) => {
   return (
     <Routes>
-      <Route path={'/'}>
+      <Route path={'/'} element={<EntityPageLayoutHolder />}>
         <Route
           index
           element={
             <RestrictedRoute>
-              <ApplyPage paths={paths} type={type} />
+              <EntityLayout type={type}>
+                <ApplyPage paths={paths} type={type} />
+              </EntityLayout>
             </RestrictedRoute>
           }
         />
