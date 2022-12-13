c8b282c82d523b700f0410945f19f7e48355fbef
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-12-13 11:16:02 +0200

    Version bump + lint warning disabled for a line

--- src/core/auth/authentication/components/Kratos/UiNodeInput.ts
+++ src/core/auth/authentication/components/Kratos/UiNodeInput.ts
@@ -2,6 +2,7 @@ import { UiNode } from '@ory/kratos-client';
 import { UiNodeInputAttributes } from '@ory/kratos-client/api';
 
 // any is in Kratos typings
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface UiNodeInput<Value = any> extends UiNode {
   attributes: Omit<UiNodeInputAttributes, 'value'> & { value?: Value | null };
 }
