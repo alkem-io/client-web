49ecb46fb22d237feca861f3ea1a6ab3f70326c6
Andrew Pazniak <andrew@yess.llc>
2022-12-21 09:38:13 +0300

    Using new JourneyCards when displaying memberships

--- src/domain/community/profile/views/ProfileView/ContributionsView.tsx
+++ src/domain/community/profile/views/ProfileView/ContributionsView.tsx
@@ -1,14 +1,22 @@
 import { Grid, Skeleton } from '@mui/material';
-import React, { FC } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { ContributionCardV2 } from '../../../../../common/components/composite/common/cards';
-import ProfileCard, {
-  ProfileCardProps,
-} from '../../../../../common/components/composite/common/cards/ProfileCard/ProfileCard';
 import ContributionDetailsContainer from '../../ContributionDetailsContainer/ContributionDetailsContainer';
 import { ContributionItem } from '../../../contributor/contribution';
+import JourneyCard from '../../../../challenge/common/JourneyCard/JourneyCard';
+import { OpportunityIcon } from '../../../../challenge/opportunity/icon/OpportunityIcon';
+import { ChallengeIcon } from '../../../../challenge/challenge/icon/ChallengeIcon';
+import { HubIcon } from '../../../../challenge/hub/icon/HubIcon';
+import { BlockTitle, Caption } from '../../../../../core/ui/typography';
+import webkitLineClamp from '../../../../../core/ui/utils/webkitLineClamp';
+import PageContentBlockGrid from '../../../../../core/ui/content/PageContentBlockGrid';
+import PageContentBlock from '../../../../../core/ui/content/PageContentBlock';
+import PageContentBlockHeader from '../../../../../core/ui/content/PageContentBlockHeader';
 
-export interface ContributionViewProps extends ProfileCardProps {
+export interface ContributionViewProps {
+  title: string;
+  subtitle?: string;
+  helpText?: string; // TODO it's unused: either find a way to use or remove
   contributions: ContributionItem[];
   loading?: boolean;
 }
@@ -38,11 +46,23 @@ const SkeletonItem = () => (
   </Grid>
 );
 
-export const ContributionsView: FC<ContributionViewProps> = ({ contributions, loading, ...rest }) => {
+const getIcon = ({ challengeId, opportunityId }: ContributionItem) => {
+  if (opportunityId) {
+    return OpportunityIcon;
+  }
+  if (challengeId) {
+    return ChallengeIcon;
+  }
+  return HubIcon;
+};
+
+export const ContributionsView = ({ title, subtitle, contributions, loading }: ContributionViewProps) => {
   const { t } = useTranslation();
   return (
-    <ProfileCard {...rest}>
-      <Grid container spacing={2}>
+    <PageContentBlock>
+      <PageContentBlockHeader title={title} />
+      {subtitle && <Caption>{subtitle}</Caption>}
+      <PageContentBlockGrid disablePadding>
         {loading && (
           <>
             <SkeletonItem />
@@ -51,19 +71,36 @@ export const ContributionsView: FC<ContributionViewProps> = ({ contributions, lo
         )}
         {!loading &&
           contributions.map(contributionItem => (
-            <Grid item key={getContributionItemKey(contributionItem)}>
-              <ContributionDetailsContainer entities={contributionItem}>
-                {({ details }, state) => <ContributionCardV2 details={details} loading={state.loading} />}
-              </ContributionDetailsContainer>
-            </Grid>
+            <ContributionDetailsContainer key={getContributionItemKey(contributionItem)} entities={contributionItem}>
+              {({ details }, { loading }) => {
+                const Icon = getIcon(contributionItem);
+                if (loading) {
+                  return null;
+                }
+                return (
+                  <JourneyCard
+                    bannerUri={details?.mediaUrl!}
+                    iconComponent={Icon}
+                    header={
+                      <BlockTitle component="div" sx={webkitLineClamp(2)}>
+                        {details?.headerText}
+                      </BlockTitle>
+                    }
+                    tagline={details?.descriptionText!}
+                    tags={details?.tags ?? []}
+                    journeyUri={details?.url!}
+                  />
+                );
+              }}
+            </ContributionDetailsContainer>
           ))}
         {!contributions.length && (
           <Grid item flexGrow={1} flexBasis={'50%'}>
-            {t('contributions-view.no-data', { name: rest.title })}
+            {t('contributions-view.no-data', { name: title })}
           </Grid>
         )}
-      </Grid>
-    </ProfileCard>
+      </PageContentBlockGrid>
+    </PageContentBlock>
   );
 };
 
