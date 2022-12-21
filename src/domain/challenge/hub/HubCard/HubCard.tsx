c524e203835e0da9dd57f1cdfd7bd09b4acab579
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2022-12-21 06:57:17 +0100

    Journey Cards Refresh WIP (#3149)
    * Card: made header highlight when hovering image
    
    * temp
    
    * temp
    
    * temp
    
    * JourneyCard member icon
    
    * HubCard isMember
    
    * JourneyCards join
    
    * JourneyCard banner placeholder

--- src/domain/challenge/hub/HubCard/HubCard.tsx
+++ src/domain/challenge/hub/HubCard/HubCard.tsx
@@ -4,19 +4,32 @@ import { HubOutlined } from '@mui/icons-material';
 import JourneyCard, { JourneyCardProps } from '../../common/JourneyCard/JourneyCard';
 import { BlockTitle, Caption } from '../../../../core/ui/typography';
 import JourneyCardTagline from '../../common/JourneyCard/JourneyCardTagline';
+import JourneyCardVision from '../../common/JourneyCard/JourneyCardVision';
+import JourneyCardSpacing from '../../common/JourneyCard/JourneyCardSpacing';
+import { useUserContext } from '../../../community/contributor/user';
+import CardActions from '../../../../core/ui/card/CardActions';
+import JourneyCardGoToButton from '../../common/JourneyCard/JourneyCardGoToButton';
+import JourneyCardJoinButton from '../../common/JourneyCard/JourneyCardJoinButton';
 
-interface HubCardProps extends Omit<JourneyCardProps, 'header' | 'iconComponent'> {
+interface HubCardProps extends Omit<JourneyCardProps, 'header' | 'iconComponent' | 'expansion' | 'journeyTypeName'> {
+  hubId?: string;
   displayName: string;
+  vision: string;
   membersCount: number;
 }
 
-const HubCard = ({ displayName, membersCount, tagline, ...props }: HubCardProps) => {
+const HubCard = ({ hubId, displayName, vision, membersCount, tagline, ...props }: HubCardProps) => {
   const { t } = useTranslation();
+
+  const { user } = useUserContext();
+
+  const isMember = hubId ? user?.ofHub(hubId) : undefined;
 
   return (
     <JourneyCard
       iconComponent={HubOutlined}
       tagline={tagline}
+      member={isMember}
       header={
         <>
           <BlockTitle noWrap component="dt">
@@ -26,6 +39,18 @@ const HubCard = ({ displayName, membersCount, tagline, ...props }: HubCardProps)
             {t('community.members-count', { count: membersCount })}
           </Caption>
         </>
+      }
+      expansion={
+        <>
+          <JourneyCardVision>{vision}</JourneyCardVision>
+          <JourneyCardSpacing />
+        </>
+      }
+      expansionActions={
+        <CardActions>
+          <JourneyCardGoToButton journeyUri={props.journeyUri} journeyTypeName="hub" />
+          <JourneyCardJoinButton />
+        </CardActions>
       }
       {...props}
     >
