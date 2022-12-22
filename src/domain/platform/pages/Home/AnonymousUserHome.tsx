037af4ab2201f589b782051c1ebfbcbc5822bbeb
Andrew Pazniak <andrew@yess.llc>
2022-12-22 10:28:16 +0300

    HomePage top block repaired: Grow/adaptivity

--- src/domain/platform/pages/Home/AnonymousUserHome.tsx
+++ src/domain/platform/pages/Home/AnonymousUserHome.tsx
@@ -1,16 +1,34 @@
-import React, { forwardRef } from 'react';
-import { Box, Button } from '@mui/material';
+import React, { forwardRef, PropsWithChildren } from 'react';
+import { Box, Button, Paper } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import {
   AUTH_LOGIN_PATH,
   AUTH_SIGN_UP_PATH,
 } from '../../../../core/auth/authentication/constants/authentication.constants';
 import { RouterLink } from '../../../../common/components/core/RouterLink';
-import PageContentBlock from '../../../../core/ui/content/PageContentBlock';
 import PageContentBlockHeader from '../../../../core/ui/content/PageContentBlockHeader';
 import { Caption } from '../../../../core/ui/typography';
 import { gutters } from '../../../../core/ui/grid/utils';
 import { Actions } from '../../../../core/ui/actions/Actions';
+import GridItem from '../../../../core/ui/grid/GridItem';
+
+interface AnonymousUserHomeBlockProps {
+  imageUri: string;
+  imageAlign: 'left' | 'right';
+}
+
+const AnonymousUserHomeBlock = ({ imageUri, imageAlign, children }: PropsWithChildren<AnonymousUserHomeBlockProps>) => {
+  return (
+    <GridItem columns={6}>
+      <Paper variant="outlined" sx={{ display: 'flex', flexDirection: imageAlign === 'left' ? 'row-reverse' : 'row' }}>
+        <Box display="flex" flexDirection="column" padding={gutters()} gap={gutters()} flexGrow={1}>
+          {children}
+        </Box>
+        <Box component="img" src={imageUri} alt="" sx={{ objectFit: 'cover' }} />
+      </Paper>
+    </GridItem>
+  );
+};
 
 const AnonymousUserHome = forwardRef<HTMLDivElement>((_, ref) => {
   const { t } = useTranslation();
@@ -18,56 +36,50 @@ const AnonymousUserHome = forwardRef<HTMLDivElement>((_, ref) => {
   const banner2Url = './alkemio-banner/alkemio-side-banner-right.png';
 
   return (
-    <>
-      <PageContentBlock halfWidth disablePadding ref={ref} sx={{ display: 'flex', flexDirection: 'row' }}>
-        <Box component="img" src={banner1Url} alt="" />
-        <Box display="flex" flexDirection="column" padding={gutters()} gap={gutters()}>
-          <PageContentBlockHeader title={t('pages.home.sections.welcome.existing-user.header')} />
-          <Caption>{t('pages.home.sections.welcome.existing-user.subheader')}</Caption>
-          <Actions>
-            <Button
-              color="primary"
-              variant="contained"
-              aria-label="Sign in"
-              component={RouterLink}
-              to={AUTH_LOGIN_PATH}
-              sx={{
-                marginRight: theme => theme.spacing(1),
-              }}
-            >
-              {t('authentication.sign-in')}
-            </Button>
-            <Button href={t('pages.home.sections.welcome.existing-user.more-info-url')} target="_blank">
-              {t('pages.home.sections.welcome.existing-user.more-info')}
-            </Button>
-          </Actions>
-        </Box>
-      </PageContentBlock>
-      <PageContentBlock halfWidth disablePadding sx={{ display: 'flex', flexDirection: 'row' }}>
-        <Box component="img" src={banner2Url} alt="" sx={{ order: '1' }} />
-        <Box display="flex" flexDirection="column" padding={gutters()} gap={gutters()}>
-          <PageContentBlockHeader title={t('pages.home.sections.welcome.new-to-alkemio.header')} />
-          <Caption>{t('pages.home.sections.welcome.new-to-alkemio.subheader')}</Caption>
-          <Actions>
-            <Button
-              color="primary"
-              variant="contained"
-              aria-label="Sign up"
-              component={RouterLink}
-              to={AUTH_SIGN_UP_PATH}
-              sx={{
-                marginRight: theme => theme.spacing(1),
-              }}
-            >
-              {t('authentication.sign-up')}
-            </Button>
-            <Button href={t('pages.home.sections.welcome.new-to-alkemio.more-info-url')} target="_blank">
-              {t('pages.home.sections.welcome.new-to-alkemio.more-info')}
-            </Button>
-          </Actions>
-        </Box>
-      </PageContentBlock>
-    </>
+    <Box ref={ref} display="flex" flexWrap="wrap" gap={gutters()} flexGrow={1}>
+      <AnonymousUserHomeBlock imageUri={banner1Url} imageAlign="left">
+        <PageContentBlockHeader title={t('pages.home.sections.welcome.existing-user.header')} />
+        <Caption>{t('pages.home.sections.welcome.existing-user.subheader')}</Caption>
+        <Actions>
+          <Button
+            color="primary"
+            variant="contained"
+            aria-label="Sign in"
+            component={RouterLink}
+            to={AUTH_LOGIN_PATH}
+            sx={{
+              marginRight: theme => theme.spacing(1),
+            }}
+          >
+            {t('authentication.sign-in')}
+          </Button>
+          <Button href={t('pages.home.sections.welcome.existing-user.more-info-url')} target="_blank">
+            {t('pages.home.sections.welcome.existing-user.more-info')}
+          </Button>
+        </Actions>
+      </AnonymousUserHomeBlock>
+      <AnonymousUserHomeBlock imageUri={banner2Url} imageAlign="right">
+        <PageContentBlockHeader title={t('pages.home.sections.welcome.new-to-alkemio.header')} />
+        <Caption>{t('pages.home.sections.welcome.new-to-alkemio.subheader')}</Caption>
+        <Actions>
+          <Button
+            color="primary"
+            variant="contained"
+            aria-label="Sign up"
+            component={RouterLink}
+            to={AUTH_SIGN_UP_PATH}
+            sx={{
+              marginRight: theme => theme.spacing(1),
+            }}
+          >
+            {t('authentication.sign-up')}
+          </Button>
+          <Button href={t('pages.home.sections.welcome.new-to-alkemio.more-info-url')} target="_blank">
+            {t('pages.home.sections.welcome.new-to-alkemio.more-info')}
+          </Button>
+        </Actions>
+      </AnonymousUserHomeBlock>
+    </Box>
   );
 });
 
