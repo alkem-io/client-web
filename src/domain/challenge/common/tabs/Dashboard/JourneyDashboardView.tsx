164b5d82414a2e69ddce1398e939d0ca87a31b9e
Carlos Cano <carlos@alkem.io>
2022-12-22 15:02:21 +0100

    Fix recommendations

--- src/domain/challenge/common/tabs/Dashboard/JourneyDashboardView.tsx
+++ src/domain/challenge/common/tabs/Dashboard/JourneyDashboardView.tsx
@@ -93,6 +93,9 @@ const JourneyDashboardView = <ChildEntity extends Identifiable>({
     : 'community.leading-organizations';
   const leadUsersHeader = isHub ? 'community.host' : 'community.leads';
 
+  const validRecommendations = recommendations?.filter(rec => rec.uri) || [];
+  const hasRecommendations = validRecommendations.length > 0;
+
   return (
     <PageContent>
       <PageContentColumn columns={4}>
@@ -128,11 +131,13 @@ const JourneyDashboardView = <ChildEntity extends Identifiable>({
       </PageContentColumn>
 
       <PageContentColumn columns={8}>
-        <PageContentBlock halfWidth>
-          <PageContentBlockHeader title={t('pages.generic.sections.recommendations.title')} />
-          <References references={recommendations} icon={RecommendationIcon} />
-        </PageContentBlock>
-        <PageContentBlock halfWidth>
+        {hasRecommendations && (
+          <PageContentBlock halfWidth>
+            <PageContentBlockHeader title={t('pages.generic.sections.recommendations.title')} />
+            <References references={validRecommendations} icon={RecommendationIcon} />
+          </PageContentBlock>
+        )}
+        <PageContentBlock halfWidth={hasRecommendations}>
           <PageContentBlockHeader title="Top Callouts" />
           {topCallouts?.map(callout => (
             <TopCalloutDetails
