24b1f0b5ff6c55106d214f694ed9be5f60bdd097
Andrew Pazniak <andrew@yess.llc>
2022-12-23 20:24:09 +0300

    EntityPageBanner: blurred sides

--- src/core/ui/grid/constants.ts
+++ src/core/ui/grid/constants.ts
@@ -2,7 +2,8 @@ export const GUTTER_PX = 20;
 
 export const GUTTER_MUI = 2;
 
-export const MAX_CONTENT_WIDTH_WITH_GUTTER = 1400;
+export const MAX_CONTENT_WIDTH_WITH_GUTTER_PX = 1400;
+export const MAX_CONTENT_WIDTH_GUTTERS = 70;
 
 export const GRID_COLUMNS_MOBILE = 4;
 export const GRID_COLUMNS_DESKTOP = 12;
--- src/domain/shared/layout/AuthPageContentContainer.tsx
+++ src/domain/shared/layout/AuthPageContentContainer.tsx
@@ -1,11 +1,11 @@
 import { Box, BoxProps } from '@mui/material';
 import { gutters } from '../../../core/ui/grid/utils';
-import { MAX_CONTENT_WIDTH_WITH_GUTTER } from '../../../core/ui/grid/constants';
+import { MAX_CONTENT_WIDTH_WITH_GUTTER_PX } from '../../../core/ui/grid/constants';
 
 const AuthPageContentContainer = (props: BoxProps) => {
   return (
     <Box
-      maxWidth={MAX_CONTENT_WIDTH_WITH_GUTTER / 2}
+      maxWidth={MAX_CONTENT_WIDTH_WITH_GUTTER_PX / 2}
       marginX="auto"
       display="flex"
       flexDirection="column"
