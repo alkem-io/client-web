c04b7cfdac37251cb2b27f7ac9d79ef71cdfbe02
Andrew Pazniak <andrew@yess.llc>
2022-12-23 11:42:00 +0300

    Cards grid quick fix

--- src/core/ui/grid/GridProvider.tsx
+++ src/core/ui/grid/GridProvider.tsx
@@ -3,17 +3,18 @@ import GridContext, { GridProperties } from './GridContext';
 
 interface GridProviderProps {
   columns: number;
+  force?: boolean;
 }
 
-const GridProvider = ({ columns, children }: PropsWithChildren<GridProviderProps>) => {
+const GridProvider = ({ columns, force = false, children }: PropsWithChildren<GridProviderProps>) => {
   const parentGridContext = useContext(GridContext);
 
   const gridProps: GridProperties = useMemo(() => {
     return {
-      columnsAvailable: parentGridContext ? Math.min(columns, parentGridContext.columnsAvailable) : columns,
+      columnsAvailable: parentGridContext && !force ? Math.min(columns, parentGridContext.columnsAvailable) : columns,
       columnsDeclared: columns,
     };
-  }, [columns, parentGridContext]);
+  }, [columns, force, parentGridContext]);
 
   return <GridContext.Provider value={gridProps}>{children}</GridContext.Provider>;
 };
