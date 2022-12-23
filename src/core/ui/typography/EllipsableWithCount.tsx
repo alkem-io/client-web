ab223c74ae53dc37de773bf8c86178d330239f1a
Andrew Pazniak <andrew@yess.llc>
2022-12-23 10:53:17 +0300

    LinksList consistent look on Callouts and Challenges

---
+++ src/core/ui/typography/EllipsableWithCount.tsx
@@ -0,0 +1,21 @@
+import React, { PropsWithChildren } from 'react';
+import { Box, Typography } from '@mui/material';
+
+interface EllipsableWithCountProps {
+  count: number;
+}
+
+const EllipsableWithCount = ({ count, children }: PropsWithChildren<EllipsableWithCountProps>) => {
+  return (
+    <Box component="span" display="flex" gap={0.5} flexGrow={1} minWidth={0}>
+      <Typography component="span" variant="inherit" flexShrink={1} noWrap>
+        {children}
+      </Typography>
+      <Box component="span" flexShrink={0}>
+        ({count})
+      </Box>
+    </Box>
+  );
+};
+
+export default EllipsableWithCount;
