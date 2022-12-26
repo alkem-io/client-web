09f91719b770406ae81bf6b3268e9414cc5d587c
Andrew Pazniak <andrew@yess.llc>
2022-12-26 13:34:48 +0300

    small fix

--- src/core/routing/Redirect.tsx
+++ src/core/routing/Redirect.tsx
@@ -1,14 +1,14 @@
-import { Navigate, NavigateProps, useParams } from 'react-router-dom';
 import React from 'react';
+import { Navigate, NavigateProps, useParams } from 'react-router-dom';
 
 interface RedirectProps extends NavigateProps {
   to: string;
 }
 
-const Redirect = ({ to }: RedirectProps) => {
+const Redirect = ({ to, ...props }: RedirectProps) => {
   const match = useParams();
 
-  return <Navigate to={`../${to}/${match['*']}`} replace />;
+  return <Navigate to={`../${to}/${match['*']}`} replace {...props} />;
 };
 
 export default Redirect;
