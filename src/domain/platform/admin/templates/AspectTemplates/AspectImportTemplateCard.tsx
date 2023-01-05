96e2d8552cb9c064e16fcc23e17ab2fb36eeaf84
Carlos Cano <carlos@alkem.io>
2023-01-05 08:39:23 +0100

    New cards WIP

--- src/domain/platform/admin/templates/AspectTemplates/AspectImportTemplateCard.tsx
+++ src/domain/platform/admin/templates/AspectTemplates/AspectImportTemplateCard.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import { TemplateImportCardComponentProps } from '../InnovationPacks/ImportTemplatesDialogGalleryStep';
 import { getVisualBannerNarrow } from '../../../../common/visual/utils/visuals.utils';
-import CardWithProvider, { ExtraInfoWithIcon } from '../../../../shared/components/CardWithProvider';
+import { ExtraInfoWithIcon } from '../../../../shared/components/CardWithProvider';
 import { AspectIcon } from '../../../../collaboration/aspect/icon/AspectIcon';
 import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
 import { TemplateInnovationPackMetaInfo } from '../InnovationPacks/InnovationPack';
+import ContributeCard from '../../../../../core/ui/card/ContributeCard';
+import CardHeader from '../../../../../core/ui/card/CardHeader';
+import CardFooter from '../../../../../core/ui/card/CardFooter';
+import CardDetails from '../../../../../core/ui/card/CardDetails';
+import CardDescription from '../../../../../core/ui/card/CardDescription';
+import CardTags from '../../../../../core/ui/card/CardTags';
 
 interface AspectImportTemplateCardProps extends TemplateImportCardComponentProps<TemplateInnovationPackMetaInfo> {}
 
-const AspectImportTemplateCard = ({ template, onClick, actionButtons }: AspectImportTemplateCardProps) => {
+const AspectImportTemplateCard = ({ template, onClick }: AspectImportTemplateCardProps) => {
+  //       providerLogoUrl={template.provider?.profile.avatar?.uri}
+  //            imageUrl={getVisualBannerNarrow(template.info.visual)}
+//       defaultImage={<AspectIcon />}
+
   return (
-    <CardWithProvider
-      key={template.id}
-      iconComponent={AspectIcon}
-      title={template.info.title}
-      provider={template.provider?.displayName}
-      providerLogoUrl={template.provider?.profile.avatar?.uri}
-      extraInformation={
+    <ContributeCard onClick={onClick}>
+      <CardHeader title={template.info.title} iconComponent={AspectIcon} createdBy={template.provider?.displayName} />
+      <CardDetails>
+        <CardDescription>{template.info.description}</CardDescription>
+        <CardTags tags={template.info.tagset?.tags ?? []} paddingX={1.5} marginY={1} />
+      </CardDetails>
+      <CardFooter>
         <ExtraInfoWithIcon iconComponent={Inventory2OutlinedIcon}>
           {template.innovationPackDisplayName}
         </ExtraInfoWithIcon>
-      }
-      imageUrl={getVisualBannerNarrow(template.info.visual)}
-      defaultImage={<AspectIcon />}
-      onClick={onClick}
-      actionButtons={actionButtons}
-    />
+      </CardFooter>
+    </ContributeCard>
   );
 };
 
--- src/domain/platform/admin/templates/CanvasTemplates/CanvasImportTemplateCard.tsx
+++ src/domain/platform/admin/templates/CanvasTemplates/CanvasImportTemplateCard.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import { TemplateImportCardComponentProps } from '../InnovationPacks/ImportTemplatesDialogGalleryStep';
-import CardWithProvider, { ExtraInfoWithIcon } from '../../../../shared/components/CardWithProvider';
+import { ExtraInfoWithIcon } from '../../../../shared/components/CardWithProvider';
 import { getVisualBannerNarrow } from '../../../../common/visual/utils/visuals.utils';
 import { CanvasIcon } from '../../../../collaboration/canvas/icon/CanvasIcon';
 import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
 import { TemplateInnovationPackMetaInfo } from '../InnovationPacks/InnovationPack';
+import ContributeCard from '../../../../../core/ui/card/ContributeCard';
+import CardHeader from '../../../../../core/ui/card/CardHeader';
+import CardDetails from '../../../../../core/ui/card/CardDetails';
+import CardTags from '../../../../../core/ui/card/CardTags';
+import CardFooter from '../../../../../core/ui/card/CardFooter';
+import CardDescription from '../../../../../core/ui/card/CardDescription';
 
 interface CanvasImportTemplateCardProps extends TemplateImportCardComponentProps<TemplateInnovationPackMetaInfo> {}
 
-const CanvasImportTemplateCard = ({ template, onClick, actionButtons }: CanvasImportTemplateCardProps) => {
+const CanvasImportTemplateCard = ({ template, onClick }: CanvasImportTemplateCardProps) => {
+  //       providerLogoUrl={template.provider?.profile.avatar?.uri}
+// imageUrl={getVisualBannerNarrow(template.info.visual)}
+//       defaultImage={<CanvasIcon />}
+
   return (
-    <CardWithProvider
-      key={template.id}
-      iconComponent={CanvasIcon}
-      title={template.info.title}
-      provider={template.provider?.displayName}
-      providerLogoUrl={template.provider?.profile.avatar?.uri}
-      extraInformation={
+    <ContributeCard onClick={onClick}>
+      <CardHeader title={template.info.title} iconComponent={CanvasIcon} createdBy={template.provider?.displayName} />
+      <CardDetails>
+        <CardDescription>{template.info.description}</CardDescription>
+        <CardTags tags={template.info.tagset?.tags ?? []} paddingX={1.5} marginY={1} />
+      </CardDetails>
+      <CardFooter>
         <ExtraInfoWithIcon iconComponent={Inventory2OutlinedIcon}>
           {template.innovationPackDisplayName}
         </ExtraInfoWithIcon>
-      }
-      imageUrl={getVisualBannerNarrow(template.info.visual)}
-      defaultImage={<CanvasIcon />}
-      onClick={onClick}
-      actionButtons={actionButtons}
-    />
+      </CardFooter>
+    </ContributeCard>
   );
 };
 
