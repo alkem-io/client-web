57ef7254ba6be080bdae77f4bd1bc79eec8727ad
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2023-01-06 14:18:16 +0100

    CardHeaderCaption styles refactored (#3309)

--- src/core/ui/card/CardHeaderCaption.tsx
+++ src/core/ui/card/CardHeaderCaption.tsx
@@ -9,19 +9,10 @@ interface CardHeaderCaptionProps extends TypographyProps {
 
 const CardHeaderCaption = ({ logoUrl, children, ...props }: PropsWithChildren<CardHeaderCaptionProps>) => {
   return (
-    <Caption {...props}>
-      {logoUrl && (
-        <Box
-          component="img"
-          src={logoUrl}
-          maxHeight={gutters()}
-          maxWidth={gutters()}
-          marginRight={gutters(0.5)}
-          sx={{ verticalAlign: 'middle' }}
-        />
-      )}
-      {children}
-    </Caption>
+    <Box display="flex" alignItems="center" gap={gutters(0.5)} {...props}>
+      {logoUrl && <Box component="img" src={logoUrl} maxHeight={gutters()} maxWidth={gutters()} />}
+      <Caption noWrap>{children}</Caption>
+    </Box>
   );
 };
 
