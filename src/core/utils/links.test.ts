51bd83f62918e78d77a9e9613c5e9803435a4202
Andrew Pazniak <andrew@yess.llc>
2023-01-06 13:40:58 +0300

    rewording in tests

--- src/core/utils/links.test.ts
+++ src/core/utils/links.test.ts
@@ -1,15 +1,15 @@
 import { hasDomainLike } from './links';
 
 describe('hasDomainLike', () => {
-  test('It matches top-level domains', () => {
+  it('matches top-level domains', () => {
     expect(hasDomainLike('alkem.io')).toBeTruthy();
   });
 
-  test('It matches 2nd-level domains', () => {
+  it('matches 2nd-level domains', () => {
     expect(hasDomainLike('www.alkem.io')).toBeTruthy();
   });
 
-  test('It does not match domain-like things in the middle of the string', () => {
+  it('does not match domain-like segments in the middle of the string', () => {
     expect(hasDomainLike('contribute/www.alkem.io')).toBeFalsy();
   });
 });
