a01ed3f6e49c9d5b32c0742446136c846364d719
Andrew Pazniak <andrew@yess.llc>
2023-01-06 16:08:16 +0300

    CardHeaderCaption styles refactored

--- src/core/ui/card/CardHeader.tsx
+++ src/core/ui/card/CardHeader.tsx
@@ -1,9 +1,9 @@
 import React, { PropsWithChildren } from 'react';
-import { Box } from '@mui/material';
 import { SvgIconComponent } from '@mui/icons-material';
 import { BlockTitle } from '../typography';
 import { gutters } from '../grid/utils';
 import RoundedIcon from '../icon/RoundedIcon';
+import BadgeCardView from '../list/BadgeCardView';
 
 interface CardTitleSectionProps {
   title?: string;
@@ -12,13 +12,16 @@ interface CardTitleSectionProps {
 
 const CardHeader = ({ iconComponent, title = '', children }: PropsWithChildren<CardTitleSectionProps>) => {
   return (
-    <Box display="flex" alignItems="center" height={gutters(3)} paddingX={1} gap={1}>
-      {iconComponent && <RoundedIcon flexShrink={0} marginLeft={0.5} size="small" component={iconComponent} />}
-      <Box flex={1} flexBasis={0} paddingX={0.5} overflow="hidden">
-        <BlockTitle noWrap>{title}</BlockTitle>
-        {children}
-      </Box>
-    </Box>
+    <BadgeCardView
+      visual={iconComponent && <RoundedIcon marginLeft={0.5} size="small" component={iconComponent} />}
+      height={gutters(3)}
+      paddingX={1}
+      gap={1}
+      contentProps={{ paddingLeft: 0.5 }}
+    >
+      <BlockTitle noWrap>{title}</BlockTitle>
+      {children}
+    </BadgeCardView>
   );
 };
 
