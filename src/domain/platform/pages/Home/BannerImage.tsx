ae33b2a890aa839c907d3f214daa565957ec1e5d
Carlos Cano <carlos@alkem.io>
2023-01-11 17:12:21 +0100

    Alkemio Banner in better resolution and responsive

--- src/domain/platform/pages/Home/BannerImage.tsx
+++ src/domain/platform/pages/Home/BannerImage.tsx
@@ -1,11 +1,14 @@
 import { Box } from '@mui/material';
+import useCurrentBreakpoint from '../../../../core/ui/utils/useCurrentBreakpoint';
 
 // TODO either use image for the current breakpoint or remove all except xxl
 const BannerImage = () => {
+  const size = useCurrentBreakpoint();
+
   return (
     <Box
       component="img"
-      src="/alkemio-banner/alkemio-banner-xxl.png"
+      src={`/alkemio-banner/alkemio-banner-${size}.png`}
       sx={{
         objectFit: 'cover',
         height: theme => ({
