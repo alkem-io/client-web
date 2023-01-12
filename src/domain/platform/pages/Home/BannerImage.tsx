70d18f48d5c1d7cd18bab05df8e3d8eb4bb2bcee
Carlos Cano <carlos@alkem.io>
2023-01-12 09:42:08 +0100

    Banner image SVG

--- src/domain/platform/pages/Home/BannerImage.tsx
+++ src/domain/platform/pages/Home/BannerImage.tsx
@@ -1,14 +1,10 @@
 import { Box } from '@mui/material';
-import useCurrentBreakpoint from '../../../../core/ui/utils/useCurrentBreakpoint';
 
-// TODO either use image for the current breakpoint or remove all except xxl
 const BannerImage = () => {
-  const size = useCurrentBreakpoint();
-
   return (
     <Box
       component="img"
-      src={`/alkemio-banner/alkemio-banner-${size}.png`}
+      src="/alkemio-banner/alkemio-banner.svg"
       sx={{
         objectFit: 'cover',
         height: theme => ({
