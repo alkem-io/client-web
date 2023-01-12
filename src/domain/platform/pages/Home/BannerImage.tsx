62180d8d3c95af58fdb0fa782a171fe75dd9c076
Andrew Pazniak <andrew@yess.llc>
2023-01-12 12:59:17 +0300

    svg condition fixed

--- src/domain/platform/pages/Home/BannerImage.tsx
+++ src/domain/platform/pages/Home/BannerImage.tsx
@@ -6,9 +6,11 @@ const biggestImageWidth = 2048;
 const BannerImage = () => {
   const size = useCurrentBreakpoint();
 
-  const hasImageForWidth = useMediaQuery(`@media only screen and (max-height: ${biggestImageWidth}px)`);
+  const hasImageForWidth = useMediaQuery(`@media only screen and (max-width: ${biggestImageWidth}px)`);
 
-  const bannerImageUrl = hasImageForWidth ? `/alkemio-banner/alkemio-banner-${size}.png` : '/alkemio-banner/alkemio-banner.svg';
+  const bannerImageUrl = hasImageForWidth
+    ? `/alkemio-banner/alkemio-banner-${size}.png`
+    : '/alkemio-banner/alkemio-banner.svg';
 
   return (
     <Box
