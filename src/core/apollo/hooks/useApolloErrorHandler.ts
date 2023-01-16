2e143c7e9be21c1b6976fe83437b7f37398511be
svetoslav <svetoslav@alkem.io>
2023-01-16 11:24:47 +0200

    removed old code

--- src/core/apollo/hooks/useApolloErrorHandler.ts
+++ src/core/apollo/hooks/useApolloErrorHandler.ts
@@ -6,18 +6,7 @@ import { Severity } from '../../state/global/notifications/notificationMachine';
 import { error as logError } from '../../../services/logging/sentry/log';
 import { useNotification } from '../../ui/notifications/useNotification';
 
-// those match the AlkemioErrorStatus on the server, but not all of them are used in the graphql errors
-enum GraphQLErrorsExtensionCodes {
-  BAD_USER_INPUT = 'BAD_USER_INPUT',
-  ENTITY_NOT_FOUND = 'ENTITY_NOT_FOUND',
-}
-
-const tryGetField = (errorMessage: string): string | undefined => {
-  let matches = errorMessage?.match(/property ([\w-]+) has failed/);
-  return matches ? matches[1] : undefined;
-};
-
-const getTranslationForCode = (error: GraphQLError, t: TFunction,i18n: i18n) => {
+const getTranslationForCode = (error: GraphQLError, t: TFunction, i18n: i18n) => {
   const code = error.extensions?.code as string;
   if (!code) {
     // if code missing send a generic error text
@@ -26,7 +15,7 @@ const getTranslationForCode = (error: GraphQLError, t: TFunction,i18n: i18n) => 
 
   const key = `apollo.errors.${code}`;
 
-  if(!i18n.exists(key)) {
+  if (!i18n.exists(key)) {
     // if the error text is missing for that code
     // send a generic error text with code
     return t('apollo.errors.generic-with-code', { code });
@@ -51,18 +40,6 @@ export const useApolloErrorHandler = (severity: Severity = 'error') => {
     const graphqlErrors = error.graphQLErrors;
 
     graphqlErrors.forEach((error: GraphQLError) => {
-      /*switch (error.extensions?.code) {
-        case GraphQLErrorsExtensionCodes.BAD_USER_INPUT: {
-          const field = tryGetField(error.message);
-          if (field) {
-            notify(t('apollo.errors.bad-user-input-withfield', { field }), severity);
-          } else {
-            notify(t('apollo.errors.bad-user-input'), severity);
-          }
-          break;
-        }
-      }*/
-
       const translation = getTranslationForCode(error, t, i18n);
       if (translation) {
         notify(translation, severity);
