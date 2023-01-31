df1740ac9590dc8f4ebdb33af4a40ce9c92e330d
Andrew Pazniak <andrew@yess.llc>
2023-01-31 13:44:57 +0100

    Calendar Event time inputs use native controls

--- src/core/ui/forms/DatePicker/FormikTimePicker.tsx
+++ src/core/ui/forms/DatePicker/FormikTimePicker.tsx
@@ -1,36 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useField } from 'formik';
 import { FormikInputProps } from '../FormikInputProps';
-import { LocalizationProvider, TimePicker, TimePickerProps } from '@mui/lab';
-import { TextField } from '@mui/material';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import NativeTimePicker, { NativeTimePickerProps } from './NativeTimePicker';
 
-interface FormikTimePickerProps extends FormikInputProps, Partial<TimePickerProps> {}
-
-const FormikDatePicker = ({ name, ...datePickerProps }: FormikTimePickerProps) => {
-  const [field, , helpers] = useField(name);
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleChange: TimePickerProps['onChange'] = date => {
-    helpers.setValue(date);
+type FormikTimePickerProps = FormikInputProps &
+  NativeTimePickerProps & {
+    step?: number;
   };
 
-  // TODO consider uncontrolled state (no open/onOpen/onClose)
-  return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <TimePicker
-        open={isOpen}
-        value={field.value}
-        onChange={handleChange}
-        renderInput={params => <TextField {...params} fullWidth />}
-        onOpen={() => setIsOpen(true)}
-        onClose={() => setIsOpen(false)}
-        ampm={false}
-        {...datePickerProps}
-      />
-    </LocalizationProvider>
-  );
+const FormikTimePicker = ({ name, ...datePickerProps }: FormikTimePickerProps) => {
+  const [field, , helpers] = useField<Date | string>(name);
+
+  return <NativeTimePicker value={field.value} onChange={helpers.setValue} fullWidth {...datePickerProps} />;
 };
 
-export default FormikDatePicker;
+export default FormikTimePicker;
