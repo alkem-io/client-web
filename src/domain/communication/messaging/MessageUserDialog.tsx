f5f015674379f83887cd0045097a72f3e213d79e
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2023-02-07 15:06:33 +0100

    Style updates.

--- src/domain/communication/messaging/MessageUserDialog.tsx
+++ src/domain/communication/messaging/MessageUserDialog.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from 'react';
 import { Dialog, TextField, DialogActions } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import SendIcon from '@mui/icons-material/Send';
 import DialogHeader from '../../../core/ui/dialog/DialogHeader';
 import DialogContent from '../../../common/components/core/dialog/DialogContent';
 import { PageTitle } from '../../../core/ui/typography/components';
@@ -17,23 +18,33 @@ export const DirectMessageDialog: FC<MessageUserDialogProps> = ({ open, onClose,
   const [isLoading, setIsloading] = useState(false);
   const [messageText, setMessageText] = useState('');
   const handleSendMessage = async () => {
-    setIsloading(false);
+    setIsloading(true);
     await onSendMessage(messageText);
     setIsloading(false);
   };
   const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setMessageText(event.target.value);
   };
+  const handleClose = () => {
+    setMessageText('');
+    onClose();
+  };
 
   return (
-    <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
-      <DialogHeader onClose={onClose}>
+    <Dialog open={open} fullWidth maxWidth="md">
+      <DialogHeader onClose={handleClose}>
         <PageTitle>{}</PageTitle>
       </DialogHeader>
       <DialogContent>
         <TextField value={messageText} onChange={handleTextChange} fullWidth />
         <DialogActions sx={{ p: 0, marginTop: 2 }}>
-          <LoadingButton loading={isLoading} disabled={isLoading} variant="contained" onClick={handleSendMessage}>
+          <LoadingButton
+            loading={isLoading}
+            disabled={isLoading}
+            variant="contained"
+            onClick={handleSendMessage}
+            startIcon={<SendIcon />}
+          >
             {t('buttons.send')}
           </LoadingButton>
         </DialogActions>
