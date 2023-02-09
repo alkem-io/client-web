0238672e4b56c172bcd378d1884ae09b4889b8e7
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2023-02-09 16:27:56 +0100

    Address pr feedback.

--- src/common/components/composite/common/ContactLeadsButton/ContactLeadsButton.tsx
+++ src/common/components/composite/common/ContactLeadsButton/ContactLeadsButton.tsx
@@ -11,16 +11,14 @@ interface ContactLeadsButtonProps {
 
 const ContactLeadsButton: FC<ContactLeadsButtonProps> = ({ title = undefined, tooltip = '', sx, onClick }) => {
   return (
-    <>
-      <Tooltip title={tooltip} arrow placement="top">
-        <Box>
-          <Button variant="contained" color="primary" onClick={() => onClick()} sx={{ width: '100%', ...sx }}>
-            <EmailOutlinedIcon sx={{ marginRight: theme => theme.spacing(2) }} />
-            {title}
-          </Button>
-        </Box>
-      </Tooltip>
-    </>
+    <Tooltip title={tooltip} arrow placement="top">
+      <Box>
+        <Button variant="contained" color="primary" onClick={() => onClick()} sx={{ width: '100%', ...sx }}>
+          <EmailOutlinedIcon sx={{ marginRight: theme => theme.spacing(2) }} />
+          {title}
+        </Button>
+      </Box>
+    </Tooltip>
   );
 };
 
