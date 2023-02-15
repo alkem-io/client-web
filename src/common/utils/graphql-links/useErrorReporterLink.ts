c3f9a593b7e2f95a555dcf9bc7cc6d3837bc9fc3
hero101 <svetoslav@alkem.io>
2023-02-15 16:06:58 +0200

    dont display the forbidden errors

--- src/common/utils/graphql-links/useErrorReporterLink.ts
+++ src/common/utils/graphql-links/useErrorReporterLink.ts
@@ -1,14 +1,19 @@
 import { onError } from '@apollo/client/link/error';
 import { ApolloError } from '@apollo/client';
 import { useApolloErrorHandler } from '../../../core/apollo/hooks/useApolloErrorHandler';
+import { AlkemioGraphqlErrorCode } from '../../constants';
 
 export const useErrorReporterLink = () => {
   const handleError = useApolloErrorHandler();
 
   return onError(({ graphQLErrors, networkError }) => {
+    // dont report the forbidden errors
+    const nonForbiddenGraphqlErrors = graphQLErrors?.filter(
+      x => x.extensions.code !== AlkemioGraphqlErrorCode.FORBIDDEN
+    );
     handleError(
       new ApolloError({
-        graphQLErrors,
+        graphQLErrors: nonForbiddenGraphqlErrors,
         networkError,
         extraInfo: 'Error caught from the errorReporterLink',
       })
