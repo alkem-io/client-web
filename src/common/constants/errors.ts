635fb6d12371a8dbd3c730d2ab8b499ca1c838f4
Svetoslav Petkov <svetoslav@alkem.io>
2023-02-15 17:55:24 +0200

    dont display the forbidden errors (#3514)
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

---
+++ src/common/constants/errors.ts
@@ -0,0 +1,5 @@
+// since all the errors are in this enum and not all of them are graphql related
+// this enum will grow with time
+export enum AlkemioGraphqlErrorCode {
+  FORBIDDEN = 'FORBIDDEN',
+}
