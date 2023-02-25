97bf1201b0c7d8927e5ce8f7653f4bc2ac47ac07
Carlos Cano <carlos@alkem.io>
2023-02-25 13:39:46 +0100

    CalendarEventCard now forwards ref for scrolling

--- src/domain/timeline/calendar/views/CalendarEventCard.tsx
+++ src/domain/timeline/calendar/views/CalendarEventCard.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { forwardRef, useCallback } from 'react';
 import ContributeCard from '../../../../core/ui/card/ContributeCard';
 import CardDetails from '../../../../core/ui/card/CardDetails';
 import CardDescription from '../../../../core/ui/card/CardDescription';
@@ -29,12 +29,12 @@ interface CalendarEventCardProps {
   onClick: (event: CalendarEventCardData) => void;
 }
 
-const CalendarEventCard = ({ event, onClick }: CalendarEventCardProps) => {
+const CalendarEventCard = forwardRef<HTMLDivElement, CalendarEventCardProps>(({ event, onClick }, ref) => {
   const handleClick = useCallback(() => event && onClick(event), [onClick, event]);
   const { t } = useTranslation();
 
   return (
-    <ContributeCard onClick={handleClick} columns={0}>
+    <ContributeCard onClick={handleClick} columns={0} ref={ref}>
       <EventCardHeader event={event} />
       <CardDetails transparent>
         <CardDescription marginLeft={gutters(2.5)} paddingY={0} overflow="hidden" overflowGradientColor="paper">
@@ -46,6 +46,6 @@ const CalendarEventCard = ({ event, onClick }: CalendarEventCardProps) => {
       </CardFooter>
     </ContributeCard>
   );
-};
+});
 
 export default CalendarEventCard;
