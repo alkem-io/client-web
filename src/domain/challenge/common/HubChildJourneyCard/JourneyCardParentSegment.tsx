9dbabaa72b18a61cca7550f3f5f21637678d374d
Carlos Cano <carlos@alkem.io>
2023-03-01 15:35:31 +0100

    CardSegmentCaption fixes

--- src/domain/challenge/common/HubChildJourneyCard/JourneyCardParentSegment.tsx
+++ src/domain/challenge/common/HubChildJourneyCard/JourneyCardParentSegment.tsx
@@ -3,15 +3,18 @@ import CardSegmentCaption from '../../../../core/ui/card/CardSegmentCaption';
 import { Caption } from '../../../../core/ui/typography';
 import { SvgIconProps } from '@mui/material';
 import RouterLink from '../../../../core/ui/link/RouterLink';
+import { LockOutlined } from '@mui/icons-material';
 
 interface JourneyCardParentSegmentProps {
-  iconComponent: ComponentType<SvgIconProps>;
+  iconComponent?: ComponentType<SvgIconProps>;
   parentJourneyUri: string;
+  locked?: boolean;
 }
 
 const JourneyCardParentSegment = ({
   iconComponent: Icon,
   parentJourneyUri,
+  locked,
   children,
 }: PropsWithChildren<JourneyCardParentSegmentProps>) => {
   const stopPropagation = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => event.stopPropagation();
@@ -20,9 +23,10 @@ const JourneyCardParentSegment = ({
     <CardSegmentCaption
       component={RouterLink}
       to={parentJourneyUri}
-      icon={<Icon fontSize="small" color="primary" />}
-      align="left"
+      icon={Icon ? <Icon fontSize="small" color="primary" /> : undefined}
+      secondaryIcon={locked ? <LockOutlined fontSize="small" color="primary" /> : undefined}
       onClick={stopPropagation}
+      disablePadding
     >
       <Caption noWrap>{children}</Caption>
     </CardSegmentCaption>
