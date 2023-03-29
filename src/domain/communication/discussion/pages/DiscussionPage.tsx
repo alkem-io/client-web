4dbbb9a2adf51458edc5d49e51c3608f8bad8014
Carlos Cano <carlos@alkem.io>
2023-03-29 13:00:55 +0200

    Revert "fix"
    This reverts commit 779a92c872f20fa0ac4bf3a7afd117474371e77b.

--- src/domain/communication/discussion/pages/DiscussionPage.tsx
+++ src/domain/communication/discussion/pages/DiscussionPage.tsx
@@ -50,6 +50,9 @@ const useDiscussionMessagesSubscription = UseSubscriptionToSubEntity<
     console.log('messageReceived:', subscriptionData.communicationDiscussionMessageReceived.message);
     discussion?.messages?.push(subscriptionData.communicationDiscussionMessageReceived.message);
   },
+  onUpdate: subEntity => {
+    console.log('onSubEntityUpdate', subEntity);
+  },
 });
 
 interface DiscussionPageProps {}
