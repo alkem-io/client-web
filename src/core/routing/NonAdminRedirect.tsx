1c25b52025140b6e02720cb9ccd641de4b57b8ee
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2023-03-31 10:50:16 +0300

    NoIdentityRedirect (#3763)
    Co-authored-by: Carlos Cano <carlos@alkem.io>

--- src/core/routing/NonAdminRedirect.tsx
+++ src/core/routing/NonAdminRedirect.tsx
@@ -1,22 +1,16 @@
 import React, { FC } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import Loading from '../../common/components/core/Loading/Loading';
-import { useAuthenticationContext } from '../auth/authentication/hooks/useAuthenticationContext';
 import { AuthorizationPrivilege } from '../apollo/generated/graphql-schema';
 import useHasPlatformLevelPrivilege from '../../domain/community/contributor/user/PlatformLevelAuthorization/useHasPlatformLevelPrivilege';
 
 const NonAdminRedirect: FC = ({ children }) => {
   const { pathname } = useLocation();
-  const { isAuthenticated, loading: isLoadingAuthentication } = useAuthenticationContext();
 
   const [isAdmin, { loading: isLoadingPrivileges }] = useHasPlatformLevelPrivilege(AuthorizationPrivilege.Admin);
 
-  if (isLoadingPrivileges || isLoadingAuthentication) {
+  if (isLoadingPrivileges) {
     return <Loading text="Loading user configuration" />;
-  }
-
-  if (!isAuthenticated) {
-    return <Navigate to={`/identity/required?returnUrl=${encodeURI(pathname)}`} />;
   }
 
   if (isAdmin) {
