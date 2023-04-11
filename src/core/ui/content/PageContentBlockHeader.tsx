2ac1f0919369185a4c9642a93d8deb68d2dd4934
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-04-11 14:23:53 +0200

    Added search results disclaimer  (#3825)
    * Added search results disclaimer
    
    * Changed disclaimer style
    
    * Reviewer's suggestions

--- src/core/ui/content/PageContentBlockHeader.tsx
+++ src/core/ui/content/PageContentBlockHeader.tsx
@@ -1,17 +1,19 @@
 import { Box } from '@mui/material';
-import { BlockTitle } from '../typography';
+import { BlockTitle, CaptionSmall } from '../typography';
 import { ReactNode } from 'react';
 import { GUTTER_PX } from '../grid/constants';
 
 export interface PageContentBlockHeaderProps {
   title: ReactNode;
   actions?: ReactNode;
+  disclaimer?: ReactNode;
 }
 
-const PageContentBlockHeader = ({ title, actions }: PageContentBlockHeaderProps) => {
+const PageContentBlockHeader = ({ title, actions, disclaimer }: PageContentBlockHeaderProps) => {
   return (
     <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" height={GUTTER_PX}>
       <BlockTitle>{title}</BlockTitle>
+      {disclaimer && <CaptionSmall>{disclaimer}</CaptionSmall>}
       {actions && <Box>{actions}</Box>}
     </Box>
   );
