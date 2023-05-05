f3a3b79bafbd473ee0f2acefa6f5c98837b94e92
Carlos Cano <carlos@alkem.io>
2023-05-05 10:49:02 +0200

    address comment

--- src/core/ui/forms/MarkdownInputControls/MarkdownInputControls.tsx
+++ src/core/ui/forms/MarkdownInputControls/MarkdownInputControls.tsx
@@ -78,7 +78,7 @@ const MarkdownInputControls = forwardRef<HTMLDivElement | null, MarkdownInputCon
 
     return (
       <Collapse in={isVisible} ref={ref}>
-        <Toolbar value={0} variant="scrollable" scrollButtons="auto" allowScrollButtonsMobile>
+        <Toolbar value={false} variant="scrollable" scrollButtons="auto" allowScrollButtonsMobile>
           <ControlsButton editor={editor} command={e => e.undo()}>
             <Undo />
           </ControlsButton>
