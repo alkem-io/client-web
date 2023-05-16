ae14e015dee3350018b73cbdebd97d396cb39e95
Carlos Cano <carlos@alkem.io>
2023-05-16 10:39:03 +0200

    Address comments

--- src/core/ui/forms/DatePicker/AlkemioTimePicker.tsx
+++ src/core/ui/forms/DatePicker/AlkemioTimePicker.tsx
@@ -1,7 +1,5 @@
 import React, { ReactNode, useMemo } from 'react';
 import { Box, BoxProps, MenuItem, Select, SelectProps, styled } from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs, { Dayjs } from 'dayjs';
 import ScheduleIcon from '@mui/icons-material/Schedule';
 
@@ -20,10 +18,6 @@ const Styles = styled(Box)(() => ({
   // Fix weird bug that hides the legend on select boxes:
   legend: { height: 15 },
   'legend > span': { opacity: 1 },
-  // Disable combo box icon (normally the arrow down) rotation
-  '.MuiSvgIcon-root': {
-    transform: 'none !important',
-  },
 }));
 
 const AlkemioTimePicker = ({
@@ -43,29 +37,33 @@ const AlkemioTimePicker = ({
   const djsDate = useMemo(() => dayjs(value).startOf('day'), [value]);
 
   const timeSlots = useMemo(
-    () => [...Array(24).keys()].flatMap(h => [djsDate.set('hour', h), djsDate.set('hour', h).set('minute', 30)]),
+    () => Array.from(Array(24).keys()).flatMap(h => [djsDate.set('hour', h), djsDate.set('hour', h).set('minute', 30)]),
     [djsDate]
   );
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Styles {...containerProps}>
-        <Select
-          value={dayjs(value).format()}
-          onChange={event => handleChange(event.target.value)}
-          IconComponent={ScheduleIcon}
-          fullWidth
-          notched
-          {...timePickerProps}
-        >
-          {timeSlots.map(t => (
-            <MenuItem value={t.format()} disabled={t.isBefore(minTime)}>
-              {t.format('HH:mm')}
-            </MenuItem>
-          ))}
-        </Select>
-      </Styles>
-    </LocalizationProvider>
+    <Styles {...containerProps}>
+      <Select
+        value={dayjs(value).format()}
+        onChange={event => handleChange(event.target.value)}
+        sx={{
+          // Disable the rotation of the combobox icon (normally that small arrow down)
+          '.MuiSvgIcon-root': {
+            transform: 'none',
+          },
+        }}
+        IconComponent={ScheduleIcon}
+        fullWidth
+        notched
+        {...timePickerProps}
+      >
+        {timeSlots.map(t => (
+          <MenuItem value={t.format()} disabled={t.isBefore(minTime)}>
+            {t.format('HH:mm')}
+          </MenuItem>
+        ))}
+      </Select>
+    </Styles>
   );
 };
 
