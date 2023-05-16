74e492065871f99d4233c4266d2eee7641c459b8
Andrew Pazniak <andrew@yess.llc>
2023-05-16 11:52:58 +0200

    InnovationFlowTemplateCard updated

--- src/domain/collaboration/templates/InnovationFlowTemplateCard/InnovationFlowTemplateCard.tsx
+++ src/domain/collaboration/templates/InnovationFlowTemplateCard/InnovationFlowTemplateCard.tsx
@@ -2,15 +2,17 @@ import { TemplateCardBaseProps } from '../CollaborationTemplatesLibrary/Template
 import { InnovationFlowTemplate } from './InnovationFlowTemplate';
 import ContributeCard from '../../../../core/ui/card/ContributeCard';
 import CardHeader from '../../../../core/ui/card/CardHeader';
-import { CardContent, Skeleton } from '@mui/material';
+import { Skeleton } from '@mui/material';
 import CardHeaderCaption from '../../../../core/ui/card/CardHeaderCaption';
-import CardImage from '../../../../core/ui/card/CardImage';
 import CardSegmentCaption from '../../../../core/ui/card/CardSegmentCaption';
 import InnovationPackIcon from '../../InnovationPack/InnovationPackIcon';
 import { Caption } from '../../../../core/ui/typography';
 import React from 'react';
 import { InnovationFlowIcon } from '../../../platform/admin/templates/InnovationTemplates/InnovationFlow/InnovationFlowIcon';
 import { SvgIconComponent } from '@mui/icons-material';
+import CardDetails from '../../../../core/ui/card/CardDetails';
+import CardDescription from '../../../../core/ui/card/CardDescription';
+import CardTags from '../../../../core/ui/card/CardTags';
 
 interface InnovationFlowTemplateCardProps extends TemplateCardBaseProps<InnovationFlowTemplate> {}
 
@@ -23,8 +25,10 @@ const InnovationFlowTemplateCard = ({ template, loading, onClick }: InnovationFl
           {template?.provider?.displayName}
         </CardHeaderCaption>
       </CardHeader>
-      <CardContent>{template?.description}</CardContent>
-      <CardImage alt={template?.displayName} defaultImage={<InnovationFlowIcon />} />
+      <CardDetails>
+        <CardDescription>{template?.description ?? ''}</CardDescription>
+        <CardTags tags={template?.tags ?? []} paddingX={1.5} marginY={1} />
+      </CardDetails>
       {template?.innovationPack.displayName && (
         <CardSegmentCaption icon={<InnovationPackIcon />}>
           <Caption noWrap>{template?.innovationPack.displayName}</Caption>
