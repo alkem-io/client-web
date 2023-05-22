64464557c1f042fd93e21c81199883f987b20c9b
Andrew Pazniak <andrew@yess.llc>
2023-05-22 13:38:05 +0200

    Simplified formatFileSize

--- src/core/utils/Storage.ts
+++ src/core/utils/Storage.ts
@@ -1,26 +1,11 @@
-/**
- * Return Human readable file size
- * https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
- * @param fileSize
- * @param decimalPlaces
- */
+const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+const unitSize = 1024;
 
 export const formatFileSize = (fileSize: number | undefined, decimalPlaces: number = 2): string => {
   if (!fileSize || fileSize <= 0) {
     return '';
-  } else if (Math.abs(fileSize) < 1024) {
-    return fileSize + ' B';
   }
-
-  const units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-  let u = -1;
-  const r = 10 ** decimalPlaces;
-
-  do {
-    fileSize /= 1024;
-    ++u;
-  } while (Math.round(Math.abs(fileSize) * r) / r >= 1024 && u < units.length - 1);
-
-  return fileSize.toFixed(decimalPlaces) + ' ' + units[u];
+  const u = Math.floor(Math.log(fileSize) / Math.log(unitSize));
+  return (fileSize / Math.pow(unitSize, u)).toFixed(u === 0 ? 0 : decimalPlaces) + ' ' + units[u];
 };
-
