4330d937e18e119993c016534e8aba7ff7be7611
Carlos Cano <carlos@alkem.io>
2023-06-15 16:47:59 +0200

    Fix #4184

--- src/core/auth/verification/components/EmailVerificationNotice/EmailVerificationNotice.tsx
+++ src/core/auth/verification/components/EmailVerificationNotice/EmailVerificationNotice.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 import { BlockTitle } from '../../../../ui/typography';
 import translateWithElements from '../../../../../domain/shared/i18n/TranslateWithElements/TranslateWithElements';
 import { AUTH_VERIFY_PATH } from '../../../authentication/constants/authentication.constants';
-import { Button } from '@mui/material';
+import { Button, Link } from '@mui/material';
 
 interface EmailVerificationNoticeProps {
   returnUrl: string;
@@ -12,16 +11,16 @@ interface EmailVerificationNoticeProps {
 
 const EmailVerificationNotice = ({ returnUrl }: EmailVerificationNoticeProps) => {
   const { t } = useTranslation();
-  const tLink = translateWithElements(<Link to="" />);
+  const tLink = translateWithElements(<Link />);
 
   return (
     <>
       <BlockTitle sx={{ textAlign: 'center', marginBottom: theme => theme.spacing(4) }}>
         {tLink('pages.verification-required.message', {
-          again: { to: AUTH_VERIFY_PATH },
+          again: { href: AUTH_VERIFY_PATH },
         })}
       </BlockTitle>
-      <Button component={Link} to={returnUrl} variant="contained">
+      <Button component={Link} href={returnUrl} variant="contained">
         {t('kratos.messages.verification-flow-continue')}
       </Button>
     </>
