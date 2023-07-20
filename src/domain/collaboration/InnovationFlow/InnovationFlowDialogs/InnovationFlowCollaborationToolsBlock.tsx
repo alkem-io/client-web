6f7ce938989fee33076874c344a50c7fc34912ba
Andrew Pazniak <andrew.pazniak@gmail.com>
2023-07-20 14:21:06 +0800

    Drag and Drop initial integration

--- src/domain/collaboration/InnovationFlow/InnovationFlowDialogs/InnovationFlowCollaborationToolsBlock.tsx
+++ src/domain/collaboration/InnovationFlow/InnovationFlowDialogs/InnovationFlowCollaborationToolsBlock.tsx
@@ -1,33 +1,38 @@
-import { ComponentType, FC } from 'react';
+import React, { ComponentType, FC, forwardRef } from 'react';
 import { BlockTitle, Caption } from '../../../../core/ui/typography';
 import PageContentBlock from '../../../../core/ui/content/PageContentBlock';
 import { useTranslation } from 'react-i18next';
-import { Box, SvgIconProps } from '@mui/material';
+import { Box, BoxProps, SvgIconProps } from '@mui/material';
 import { GrouppedCallout } from './useInnovationFlowSettings';
 import { groupBy } from 'lodash';
 import calloutIcons from '../../callout/utils/calloutIcons';
 import ScrollableCardsLayoutContainer from '../../../../core/ui/card/CardsLayout/ScrollableCardsLayoutContainer';
+import { DragDropContext, Droppable, Draggable, OnDragEndResponder } from 'react-beautiful-dnd';
+import Gutters from '../../../../core/ui/grid/Gutters';
 
 interface InnovationFlowCollaborationToolsBlockProps {
   callouts: GrouppedCallout[];
   flowStateAllowedValues: string[];
 }
 
-interface ListItemProps {
+interface ListItemProps extends BoxProps {
   displayName: string;
   icon?: ComponentType<SvgIconProps>;
   activity?: number;
 }
-const ListItem = ({ displayName, icon: Icon, activity = 0 }: ListItemProps) => {
-  return (
-    <Box>
-      <Caption>
-        {Icon && <Icon />}
-        {displayName} ({activity})
-      </Caption>
-    </Box>
-  );
-};
+
+const ListItem = forwardRef<HTMLDivElement, ListItemProps>(
+  ({ displayName, icon: Icon, activity = 0, ...boxProps }, ref) => {
+    return (
+      <Box ref={ref} {...boxProps}>
+        <Caption>
+          {Icon && <Icon />}
+          {displayName} ({activity})
+        </Caption>
+      </Box>
+    );
+  }
+);
 
 const InnovationFlowCollaborationToolsBlock: FC<InnovationFlowCollaborationToolsBlockProps> = ({
   callouts,
@@ -36,26 +41,41 @@ const InnovationFlowCollaborationToolsBlock: FC<InnovationFlowCollaborationTools
   const { t } = useTranslation();
   const groupedCallouts = groupBy(callouts, callout => callout.flowState);
 
+  const handleDragEnd: OnDragEndResponder = () => {};
+
   return (
-    <PageContentBlock>
-      <BlockTitle>{t('common.collaborationTools')}</BlockTitle>
-      <ScrollableCardsLayoutContainer orientation="horizontal" alignItems="stretch">
-        {flowStateAllowedValues.map(state => (
-          <PageContentBlock columns={3}>
-            <Caption>{state}</Caption>
-            {groupedCallouts[state] &&
-              groupedCallouts[state].length &&
-              groupedCallouts[state].map(callout => (
-                <ListItem
-                  key={callout.id}
-                  displayName={callout.profile.displayName}
-                  icon={calloutIcons[callout.type]}
-                />
-              ))}
-          </PageContentBlock>
-        ))}
-      </ScrollableCardsLayoutContainer>
-    </PageContentBlock>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <PageContentBlock>
+        <BlockTitle>{t('common.collaborationTools')}</BlockTitle>
+        <ScrollableCardsLayoutContainer orientation="horizontal" alignItems="stretch">
+          {flowStateAllowedValues.map(state => (
+            <PageContentBlock key={state} columns={3}>
+              <Caption>{state}</Caption>
+              <Droppable droppableId={state}>
+                {provided => (
+                  <Gutters ref={provided.innerRef} disablePadding flexGrow={1} {...provided.droppableProps}>
+                    {groupedCallouts[state]?.map((callout, index) => (
+                      <Draggable key={callout.id} draggableId={callout.id} index={index}>
+                        {provided => (
+                          <ListItem
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            displayName={callout.profile.displayName}
+                            icon={calloutIcons[callout.type]}
+                          />
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </Gutters>
+                )}
+              </Droppable>
+            </PageContentBlock>
+          ))}
+        </ScrollableCardsLayoutContainer>
+      </PageContentBlock>
+    </DragDropContext>
   );
 };
 
