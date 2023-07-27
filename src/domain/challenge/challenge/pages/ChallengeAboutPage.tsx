827050660834e50abc5ae5199d7fc4d6227b3c38
Andrew Pazniak <andrew.pazniak@gmail.com>
2023-07-27 20:21:55 +0800

    Challenge About page shows JourneyAboutDialog

--- src/domain/challenge/challenge/pages/ChallengeAboutPage.tsx
+++ src/domain/challenge/challenge/pages/ChallengeAboutPage.tsx
@@ -1,39 +1,72 @@
 import React, { FC } from 'react';
 import { useChallenge } from '../hooks/useChallenge';
 import AboutPageContainer from '../../common/AboutPageContainer/AboutPageContainer';
-import { ChallengeAboutView } from '../views/ChallengeAboutView';
 import ChallengeDashboardPage from './ChallengeDashboardPage';
-import DialogWithGrid from '../../../../core/ui/dialog/DialogWithGrid';
 import useBackToParentPage from '../../../shared/utils/useBackToParentPage';
+import JourneyAboutDialog from '../../common/JourneyAboutDialog/JourneyAboutDialog';
+import { IconButton } from '@mui/material';
+import { Close } from '@mui/icons-material';
+import useSendMessageToCommunityLeads from '../../../community/CommunityLeads/useSendMessageToCommunityLeads';
+import EntityDashboardContributorsSection from '../../../community/community/EntityDashboardContributorsSection/EntityDashboardContributorsSection';
 
 const ChallengeAboutPage: FC = () => {
   const { spaceNameId, profile, challengeNameId, communityId } = useChallenge();
 
   const [backToParentPage] = useBackToParentPage('../dashboard');
 
+  const sendMessageToCommunityLeads = useSendMessageToCommunityLeads(communityId);
+
   return (
     <>
       <ChallengeDashboardPage />
-      <DialogWithGrid open columns={12} onClose={backToParentPage}>
-        <AboutPageContainer spaceNameId={spaceNameId} challengeNameId={challengeNameId}>
-          {({ context, tagset, permissions, ...rest }, state) => (
-            <ChallengeAboutView
-              name={profile?.displayName}
-              tagline={profile?.tagline}
-              tags={tagset?.tags}
-              who={context?.who}
-              impact={context?.impact}
-              background={profile?.description}
-              vision={context?.vision}
-              communityReadAccess={permissions.communityReadAccess}
-              spaceNameId={spaceNameId}
-              communityId={communityId}
-              {...rest}
-              {...state}
-            />
-          )}
-        </AboutPageContainer>
-      </DialogWithGrid>
+      <AboutPageContainer spaceNameId={spaceNameId} challengeNameId={challengeNameId}>
+        {(
+          {
+            context,
+            references,
+            hostOrganization,
+            leadOrganizations,
+            leadUsers,
+            metrics,
+            memberUsers,
+            memberUsersCount,
+            memberOrganizations,
+            memberOrganizationsCount,
+          },
+          state
+        ) => (
+          <JourneyAboutDialog
+            open
+            journeyTypeName="challenge"
+            displayName={profile?.displayName}
+            tagline={profile?.tagline}
+            references={references}
+            sendMessageToCommunityLeads={sendMessageToCommunityLeads}
+            metrics={metrics}
+            description={context?.vision}
+            background={profile?.description}
+            who={context?.who}
+            impact={context?.impact}
+            loading={state.loading}
+            leadUsers={leadUsers}
+            hostOrganizations={hostOrganization && [hostOrganization]}
+            leadOrganizations={leadOrganizations}
+            endButton={
+              <IconButton onClick={backToParentPage}>
+                <Close />
+              </IconButton>
+            }
+            leftColumnChildren={
+              <EntityDashboardContributorsSection
+                memberUsers={memberUsers}
+                memberUsersCount={memberUsersCount}
+                memberOrganizations={memberOrganizations}
+                memberOrganizationsCount={memberOrganizationsCount}
+              />
+            }
+          />
+        )}
+      </AboutPageContainer>
     </>
   );
 };
