0043e8b59471b5211e14c022a7b67705622b91cd
Carlos Cano <carlos@alkem.io>
2023-08-14 12:16:31 +0200

    Refetch instead of cache policy

--- src/domain/collaboration/InnovationFlow/InnovationFlowDialogs/useInnovationFlowSettings.tsx
+++ src/domain/collaboration/InnovationFlow/InnovationFlowDialogs/useInnovationFlowSettings.tsx
@@ -63,7 +63,6 @@ const useInnovationFlowSettings = ({
       includeChallenge: isChallenge,
       includeOpportunity: !isChallenge,
     },
-    fetchPolicy: 'network-only',
     skip: !spaceNameId || (!challengeNameId && !opportunityNameId),
   });
 
