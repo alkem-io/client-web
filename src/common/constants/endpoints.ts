8dd14b32b5c48d0b64bb313ef8a191c99a53d183
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-08-18 17:19:22 +0300

    Merge fix

--- src/common/constants/endpoints.ts
+++
@@ -1,5 +0,0 @@
-import { env } from '../../types/env';
-
-const domain = (env && env.VITE_APP_ALKEMIO_DOMAIN) ?? '';
-export const publicGraphQLEndpoint = domain + '/api/public/graphql';
-export const privateGraphQLEndpoint = domain + '/api/private/graphql';
