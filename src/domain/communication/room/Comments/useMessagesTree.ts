14b599b024edf3c3a2f9d789f9333158341a8002
Andrew Pazniak <andrew.pazniak@gmail.com>
2023-08-23 12:06:27 +0800

    useMessagesTree: self-pointing replies become root nodes

--- src/domain/communication/room/Comments/useMessagesTree.ts
+++ src/domain/communication/room/Comments/useMessagesTree.ts
@@ -1,15 +1,27 @@
 import { useMemo } from 'react';
 import { groupBy } from 'lodash';
 import { Message } from '../models/Message';
+import { Identifiable } from '../../../../core/utils/Identifiable';
 
 const ROOT_THREAD = Symbol('root');
 
 const useMessagesTree = (messages: Message[] | undefined) => useMemo(() => buildMessagesTree(messages), [messages]);
 
-const buildMessagesTree = (messages: Message[] | undefined) => {
-  const groupedMessages = groupBy(messages, m => m.threadID ?? ROOT_THREAD) as unknown as
-    | Record<string | typeof ROOT_THREAD, Message[] | undefined>
-    | undefined;
+export interface IdentifiableReply extends Identifiable {
+  threadID?: string;
+}
+
+type Thread<Message> = Message & {
+  replies?: Message[];
+};
+
+export const buildMessagesTree = <Message extends IdentifiableReply>(
+  messages: Message[] | undefined
+): Thread<Message>[] | undefined => {
+  const groupedMessages = groupBy(messages, m =>
+    // TODO After dealing with client-4587 change to `m.threadID ?? ROOT_THREAD`
+    !m.threadID || m.threadID === m.id ? ROOT_THREAD : m.threadID
+  ) as unknown as Record<string | typeof ROOT_THREAD, Message[] | undefined> | undefined;
 
   return groupedMessages?.[ROOT_THREAD]?.map(message => ({
     ...message,
