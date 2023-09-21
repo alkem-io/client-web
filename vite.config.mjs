57d5ee32f99a6733b3c33e9dd4fd17d6edbd2266
hero101 <svetoslav@alkem.io>
2023-09-21 15:42:39 +0300

    lint error

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { version, name } from './package';
+import { version } from './package';
 
 export default defineConfig({
   server: {
@@ -11,27 +11,25 @@ export default defineConfig({
     host: 'localhost',
   },
   build: {
+    sourcemap: process.env.NODE_ENV  === 'production',
+    sourcemapFile: '/static',
+    emptyOutDir: true,
     outDir: 'build',
-    // sourcemap: process.env.NODE_ENV  === 'production',
-    sourcemap: true,
   },
   plugins: [
     react(),
     viteTsconfigPaths(),
     svgrPlugin(),
     sentryVitePlugin({
-      org: "alkemio",
-      project: "alkemio",
+      org: 'alkemio',
+      project: 'alkemio',
       debug: true,
       release: {
         name: `client-web@${version}`,
         version,
         create: process.env.NODE_ENV  === 'production',
       },
-      // authToken: import.meta.env.VITE_APP_SENTRY_AUTH_TOKEN,
-      // authToken: process.env.SENTRY_AUTH_TOKEN,
-      // authToken: process.env.VITE_APP_SENTRY_AUTH_TOKEN,
-      authToken: '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',
+      authToken: process.env.VITE_APP_SENTRY_AUTH_TOKEN,
     }),
   ],
 });
