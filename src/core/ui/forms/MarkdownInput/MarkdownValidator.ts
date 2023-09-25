e45722704102c77fc30c31f52673fccd35915bfb
Carlos Cano <carlos@alkem.io>
2023-09-25 17:21:29 +0200

    clean

--- src/core/ui/forms/MarkdownInput/MarkdownValidator.ts
+++ src/core/ui/forms/MarkdownInput/MarkdownValidator.ts
@@ -1,14 +1,13 @@
 import { string } from 'yup';
 import TranslationKey from '../../../i18n/utils/TranslationKey';
 import { ValidationMessageWithPayload } from '../../../../domain/shared/i18n/ValidationMessageTranslation/ValidationMessageWithPayload';
-// import { MessageWithPayload } from '../../../../domain/shared/i18n/ValidationMessageTranslation';
 import { MarkdownTextMaxLength } from '../field-length.constants';
 
 const translationKey: TranslationKey = 'components.wysiwyg-editor.validation.maxLength';
 
 const MarkdownValidator = (_maxLength: MarkdownTextMaxLength) => {
-  return string(); // .max(maxLength, MessageWithPayload(translationKey));
-}; //!!
+  return string();
+};
 
 export const isMarkdownMaxLengthError = (
   error: string | ValidationMessageWithPayload
