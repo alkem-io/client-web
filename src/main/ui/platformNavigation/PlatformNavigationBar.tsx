ad5e2ab6d8a51fd5a5c8276005a898121d1fa20f
Andrew Pazniak <andrew.pazniak@gmail.com>
2023-10-06 08:32:01 +0200

    misc

--- src/main/ui/platformNavigation/PlatformNavigationBar.tsx
+++ src/main/ui/platformNavigation/PlatformNavigationBar.tsx
@@ -1,12 +1,18 @@
 import NavigationBar from '../../../core/ui/navigation/NavigationBar';
 import PlatformNavigationUserAvatar from './PlatformNavigationUserAvatar';
 import JourneyBreadcrumbs from '../../../domain/journey/common/journeyBreadcrumbs/JourneyBreadcrumbs';
+import PlatformSearch from '../platformSearch/PlatformSearch';
 
 const PlatformNavigationBar = () => {
   return (
     <NavigationBar
       childrenLeft={<JourneyBreadcrumbs />}
-      childrenRight={<PlatformNavigationUserAvatar />}
+      childrenRight={
+        <>
+          <PlatformSearch />
+          <PlatformNavigationUserAvatar />
+        </>
+      }
     />
   );
 };
