908ad04e720d566efb5df5931f29b8a54646148f
Carlos Cano <carlos@alkem.io>
2023-10-13 12:27:20 +0200

    Address comment

--- src/domain/common/whiteboard/excalidraw/useWhiteboardFilesManager.ts
+++ src/domain/common/whiteboard/excalidraw/useWhiteboardFilesManager.ts
@@ -24,13 +24,13 @@ const dataUrlToFile = async (
 const blobToDataURL = (blob: Blob): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.readAsDataURL(blob);
     reader.onloadend = () => {
       resolve(reader.result as string);
     };
     reader.onerror = () => {
       reject(reader.error);
     };
+    reader.readAsDataURL(blob);
   });
 };
 
