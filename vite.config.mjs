aaf1e714496fbd74a7f1e1db00c38e38904155f6
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-11-09 10:02:17 +0200

    Updated asset names with hash

--- vite.config.mjs
+++ vite.config.mjs
@@ -15,9 +15,9 @@ export default defineConfig({
     outDir: 'build',
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
+        entryFileNames: 'assets/[name][hash].js',
+        chunkFileNames: 'assets/[name][hash].js',
+        assetFileNames: 'assets/[name][hash].[ext]',
       },
     },
   },
