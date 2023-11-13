174632fd1311d720348a152f27ec545244ffec5a
Carlos Cano <carlos@alkem.io>
2023-11-13 14:47:25 +0100

    Update FloatingActionButtons.tsx

--- src/core/ui/button/FloatingActionButtons.tsx
+++ src/core/ui/button/FloatingActionButtons.tsx
@@ -3,7 +3,7 @@ import React, { FC, ReactNode } from 'react';
 import ScrollToTop from './ScrollToTop';
 import Gutters from '../grid/Gutters';
 
-const NAVIGATION_FLOATING_ACTION_BUTTONS_Z_INDEX = 1200; // Dialogs are 1300
+const FLOATING_ACTION_BUTTONS_Z_INDEX = 1200; // Dialogs are 1300
 export interface FloatingActionButtonsProps extends BoxProps {
   visible?: boolean;
   floatingActions?: ReactNode;
@@ -18,7 +18,7 @@ const FloatingActionButtons: FC<FloatingActionButtonsProps> = ({ visible = true,
         right={0}
         justifyContent="center"
         alignItems="center"
-        zIndex={NAVIGATION_FLOATING_ACTION_BUTTONS_Z_INDEX}
+        zIndex={FLOATING_ACTION_BUTTONS_Z_INDEX}
         maxHeight="100vh"
         {...boxProps}
       >
