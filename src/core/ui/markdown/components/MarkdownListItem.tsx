dcbbf50d91f8456b4191e2e5ee1106ddb8a42978
Carlos Cano <carlos@alkem.io>
2023-11-14 11:58:45 +0100

    Fix markdown lists

---
+++ src/core/ui/markdown/components/MarkdownListItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ReactMarkdownProps } from 'react-markdown/lib/complex-types';
+import MarkdownParagraph from './MarkdownParagraph';
+
+const MarkdownListItem = ({ node, children, ...props }: ReactMarkdownProps) => {
+  if (children && children.length === 1 && typeof children[0] === 'string') {
+    return (
+      <li {...props}>
+        <MarkdownParagraph node={node}>{children}</MarkdownParagraph>
+      </li>
+    );
+  } else {
+    return <li {...props}>{children}</li>;
+  }
+};
+
+export default MarkdownListItem;
