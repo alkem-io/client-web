52fb1d997ac5b3630d9586d417f678b4dcee9b9e
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2023-11-15 19:58:39 +0800

    Essential UI elements reachable by keyboard navigation (#5192)
    * Top menus navigatable/skippable by keyboard/tabbing
    
    * typefix
    
    * Whiteboards are openable with keyboard
    
    * ContributeCard is openable with keyboard
    
    * typefix2
    
    * User profile associated orgs are navigatable with keyboard
    
    ---------
    
    Co-authored-by: Carlos Cano <carlos@alkem.io>

--- src/main/ui/platformSearch/PlatformSearch.tsx
+++ src/main/ui/platformSearch/PlatformSearch.tsx
@@ -1,4 +1,4 @@
-import { SelectOption } from '@mui/base/SelectUnstyled/useSelect.types';
+import { SelectOption } from '@mui/base';
 import { useTranslation } from 'react-i18next';
 import React, { forwardRef, PropsWithChildren, useCallback, useMemo, useState } from 'react';
 import { useSpace } from '../../../domain/journey/space/SpaceContext/useSpace';
@@ -31,7 +31,7 @@ const PlatformSearch = forwardRef<Collapsible, PropsWithChildren<PlatformSearchP
 
     const { spaceNameId, profile } = useSpace();
 
-    const searchOptions = useMemo<SelectOption<SearchScope>[] | undefined>(() => {
+    const searchOptions = useMemo<Partial<SelectOption<SearchScope>>[] | undefined>(() => {
       if (!spaceNameId) {
         return undefined;
       }
