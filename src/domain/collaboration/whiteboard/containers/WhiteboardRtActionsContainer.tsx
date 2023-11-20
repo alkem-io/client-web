78bc785aa0299118f32edfd6cbd714094a15030b
Svetoslav Petkov <svetoslav@alkem.io>
2023-11-20 15:12:22 +0200

    improved visibility on whiteboard auto save (#5222)
    Co-authored-by: Carlos Cano <carlos@alkem.io>
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/domain/collaboration/whiteboard/containers/WhiteboardRtActionsContainer.tsx
+++ src/domain/collaboration/whiteboard/containers/WhiteboardRtActionsContainer.tsx
@@ -11,7 +11,7 @@ export interface IWhiteboardRtActions {
   onUpdate: (
     whiteboard: WhiteboardRtContentFragment & WhiteboardRtDetailsFragment,
     previewImages?: WhiteboardPreviewImage[]
-  ) => Promise<boolean>;
+  ) => Promise<{ success: boolean; errors?: string[] }>;
 }
 
 export interface WhiteboardRtActionsContainerState {
@@ -49,7 +49,10 @@ const WhiteboardRtActionsContainer: FC<WhiteboardRtActionsContainerProps> = ({ c
           },
         },
       });
-      return !result.errors || result.errors.length === 0;
+      return {
+        success: !result.errors || result.errors.length === 0,
+        errors: result.errors?.map(({ message }) => message),
+      };
     },
     [updateWhiteboardContent]
   );
