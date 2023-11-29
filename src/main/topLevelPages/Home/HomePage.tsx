a5daab51e6c2918f4046a53eb484a91c98bd8e49
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2023-11-29 20:32:30 +0800

    Home page Create Account banner (#5253)
    Co-authored-by: Carlos Cano <carlos@alkem.io>
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/main/topLevelPages/Home/HomePage.tsx
+++ src/main/topLevelPages/Home/HomePage.tsx
@@ -15,6 +15,7 @@ import InnovationHubHomePage from '../../../domain/innovationHub/InnovationHubHo
 import Loading from '../../../core/ui/loading/Loading';
 import { useAuthenticationContext } from '../../../core/auth/authentication/hooks/useAuthenticationContext';
 import useInnovationHub from '../../../domain/innovationHub/useInnovationHub/useInnovationHub';
+import CreateAccountBanner from './CreateAccountBanner';
 
 export const HomePage = () => {
   const { isAuthenticated, loading: isLoadingAuthentication } = useAuthenticationContext();
@@ -41,6 +42,7 @@ export const HomePage = () => {
       <ReleaseUpdatesDialog />
       <PageContent>
         <PageContentColumn columns={12}>
+          {!isLoadingAuthentication && !isAuthenticated && <CreateAccountBanner />}
           {!isFromLanding && (
             <Grow in={!isLoadingAuthentication} appear>
               <Box display="flex" flexDirection="column" gap={gutters()} flexGrow={1} maxWidth="100%">
