1d3678b79c9d39f6c22c66106f328e7f8d6cc9f5
Carlos Cano <carlos@alkem.io>
2023-12-06 12:04:13 +0100

    Recent Forum messages in My Dashboard (#5282)
    * Recent Forum messages in My Dashboard
    
    * fix typo

--- src/main/topLevelPages/Home/AuthenticatedUserHome.tsx
+++ src/main/topLevelPages/Home/AuthenticatedUserHome.tsx
@@ -2,14 +2,11 @@ import React, { forwardRef } from 'react';
 import { Box } from '@mui/material';
 import MySpacesSection from '../../../domain/journey/space/MySpaces/MySpacesSection';
 import { gutters } from '../../../core/ui/grid/utils';
-import MoreAboutAlkemio from '../myDashboard/moreAboutAlkemio/MoreAboutAlkemio';
 
 const AuthenticatedUserHome = forwardRef<HTMLDivElement>((props, ref) => {
   return (
     <Box ref={ref} display="flex" flexDirection="column" gap={gutters()} flexGrow={1}>
       <MySpacesSection />
-      {/* TODO: This is not the final location for this block */}
-      <MoreAboutAlkemio />
     </Box>
   );
 });
