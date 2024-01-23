440b7e719d80e158d60f7834cbc86d465c30c421
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2024-01-23 23:56:44 +0800

    /landing redirects to welcome. (#5431)

---
+++ src/domain/platform/routes/RedirectToWelcomeSite.tsx
@@ -0,0 +1,16 @@
+import { useLayoutEffect } from 'react';
+import useLandingUrl from '../../../main/landing/useLandingUrl';
+
+const RedirectToWelcomeSite = () => {
+  const landingUrl = useLandingUrl();
+
+  useLayoutEffect(() => {
+    if (landingUrl) {
+      window.location.replace(landingUrl);
+    }
+  }, [landingUrl]);
+
+  return null;
+};
+
+export default RedirectToWelcomeSite;
