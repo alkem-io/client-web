de8790139a349d8c04d5d2e75725cda0835fd90f
Andrew Pazniak <andrew.pazniak@gmail.com>
2024-01-25 14:42:44 +0200

    Loading spinner appears in the right place when Callout is loading

--- src/domain/collaboration/CalloutPage/CalloutPage.tsx
+++ src/domain/collaboration/CalloutPage/CalloutPage.tsx
@@ -16,6 +16,8 @@ import Loading from '../../../core/ui/loading/Loading';
 import { isApolloNotFoundError } from '../../../core/apollo/hooks/useApolloErrorHandler';
 import { NotFoundPageLayout } from '../../journey/common/EntityPageLayout';
 import { Error404 } from '../../../core/pages/Errors/Error404';
+import usePageLayoutByEntity from '../../shared/utils/usePageLayoutByEntity';
+import { EntityPageSection } from '../../shared/layout/EntityPageSection';
 
 interface CalloutLocation {
   journeyTypeName: JourneyTypeName;
@@ -106,8 +108,14 @@ const CalloutPage = ({ journeyTypeName, parentRoute, renderPage, children }: Cal
 
   const isSmallScreen = useMediaQuery<Theme>(theme => theme.breakpoints.down('sm'));
 
+  const PageLayout = usePageLayoutByEntity(journeyTypeName);
+
   if (isCalloutLoading) {
-    return <Loading />;
+    return (
+      <PageLayout currentSection={EntityPageSection.Contribute}>
+        <Loading />
+      </PageLayout>
+    );
   }
 
   if (isApolloNotFoundError(error)) {
