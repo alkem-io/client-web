7e9a0e057ec5e9f009f55870d7e77c194d32389c
hero101 <svetoslav@alkem.io>
2024-01-29 16:13:45 +0200

    proper link to login page

--- src/core/auth/components/IdentityRouteLink.tsx
+++ src/core/auth/components/IdentityRouteLink.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Button, ButtonProps, Link } from '@mui/material';
-import { useConfig } from '../../../domain/platform/config/useConfig';
-import { ButtonTypeMap } from '@mui/material/Button/Button';
-import { buildReturnUrlParam } from '../../../main/routing/urlBuilders';
 import { useLocation } from 'react-router-dom';
+import { ButtonTypeMap } from '@mui/material/Button/Button';
+import { buildLoginUrl } from '../../../main/routing/urlBuilders';
 
 const IdentityRouteLink = <D extends React.ElementType = ButtonTypeMap['defaultComponent'], P = {}>({
   children,
   to,
   ...props
 }: ButtonProps<D, P> & { to: string }) => {
-  const config = useConfig();
-
-  const rootUrl = config.authentication?.providers[0].config.issuer;
-
   const { pathname } = useLocation();
 
   return (
-    <Button component={Link} href={`${rootUrl}${to}${buildReturnUrlParam(pathname)}`} {...props}>
+    <Button component={Link} href={buildLoginUrl(pathname)} {...props}>
       {children}
     </Button>
   );
