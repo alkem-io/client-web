610be53a45d5144abf1404363d0f807151a42168
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2024-01-30 17:26:38 +0800

    Closing a Callout takes the user to Callout Location (#5461)
    * Closing a Callout takes the user to Callout Location
    
    * rename
    
    ---------
    
    Co-authored-by: Carlos Cano <carlos@alkem.io>

---
+++ src/core/routing/useBackToPath.ts
@@ -0,0 +1,28 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useCanGoBack from './useCanGoBack';
+
+/**
+ * Goes back only if the previous history item has the specified URL.
+ * This is based on the native browser history API, so it may not work with all flavors of react-router.
+ */
+const useBackToPath = () => {
+  const navigate = useNavigate();
+  const canGoBack = useCanGoBack();
+
+  return useCallback((parentPagePath: string) => {
+    if (!canGoBack) {
+      navigate(parentPagePath);
+    }
+    const handlePopState = () => {
+      window.removeEventListener('popstate', handlePopState);
+      if (window.location.pathname !== parentPagePath) {
+        navigate(parentPagePath);
+      }
+    };
+    window.addEventListener('popstate', handlePopState);
+    navigate(-1);
+  }, []);
+};
+
+export default useBackToPath;
