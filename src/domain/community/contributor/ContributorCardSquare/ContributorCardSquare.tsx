38a9baee396a62dee2dbbeb1b130fece5434d4e0
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2024-01-31 18:38:14 +0100

    Fix show displayName when sending message. (#5468)

--- src/domain/community/contributor/ContributorCardSquare/ContributorCardSquare.tsx
+++ src/domain/community/contributor/ContributorCardSquare/ContributorCardSquare.tsx
@@ -70,9 +70,7 @@ export const ContributorCardSquare: FC<ContributorCardSquareProps> = props => {
   const [sendMessageToOrganization] = useSendMessageToOrganizationMutation();
   const [isMessageUserDialogOpen, setIsMessageUserDialogOpen] = useState(false);
 
-  const messageReceivers = [
-    { id, title: displayName, avatarUri: avatar, city: tooltip?.city, country: tooltip?.country },
-  ];
+  const messageReceivers = [{ id, displayName, avatarUri: avatar, city: tooltip?.city, country: tooltip?.country }];
 
   const handleSendMessage = useCallback(
     async (messageText: string) => {
