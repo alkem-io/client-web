16e02a03c1a596202e25336a4b8ba307f29511f4
Andrew Pazniak <andrew.pazniak@gmail.com>
2024-02-05 18:51:41 +0100

    Edit Callout

--- src/domain/collaboration/callout/edit/editDialog/CalloutEditDialog.tsx
+++ src/domain/collaboration/callout/edit/editDialog/CalloutEditDialog.tsx
@@ -1,7 +1,5 @@
 import React, { FC, useCallback, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Box } from '@mui/material';
-import Dialog from '@mui/material/Dialog';
 import { LoadingButton } from '@mui/lab';
 import calloutIcons from '../../utils/calloutIcons';
 import { DialogActions, DialogContent } from '../../../../../core/ui/dialog/deprecated';
@@ -23,6 +21,7 @@ import { getCalloutDisplayLocationValue } from '../../utils/getCalloutDisplayLoc
 import { JourneyTypeName } from '../../../../journey/JourneyTypeName';
 import { StorageConfigContextProvider } from '../../../../storage/StorageBucket/StorageConfigContext';
 import { DEFAULT_TAGSET } from '../../../../common/tags/tagset.constants';
+import DialogWithGrid from '../../../../../core/ui/dialog/DialogWithGrid';
 
 export interface CalloutEditDialogProps {
   open: boolean;
@@ -140,16 +139,15 @@ const CalloutEditDialog: FC<CalloutEditDialogProps> = ({
 
   return (
     <>
-      <Dialog open={open} maxWidth="md" fullWidth aria-labelledby="callout-visibility-dialog-title" onClose={onClose}>
-        <DialogHeader onClose={onClose}>
-          <Box display="flex" alignItems="center" gap={1}>
-            {CalloutIcon && <CalloutIcon />}
-            {t('components.calloutEdit.titleWithType', {
-              type: t(`components.calloutTypeSelect.label.${calloutType}` as const),
-            })}
-          </Box>
-        </DialogHeader>
-        <DialogContent dividers>
+      <DialogWithGrid open={open} columns={8} aria-labelledby="callout-visibility-dialog-title" onClose={onClose}>
+        <DialogHeader
+          icon={CalloutIcon && <CalloutIcon />}
+          title={t('components.calloutEdit.titleWithType', {
+            type: t(`components.calloutTypeSelect.label.${calloutType}` as const),
+          })}
+          onClose={onClose}
+        />
+        <DialogContent>
           <StorageConfigContextProvider
             locationType="callout"
             journeyTypeName={journeyTypeName}
@@ -188,7 +186,7 @@ const CalloutEditDialog: FC<CalloutEditDialogProps> = ({
             {t('buttons.save')}
           </LoadingButton>
         </DialogActions>
-      </Dialog>
+      </DialogWithGrid>
       <ConfirmationDialog {...confirmationDialogProps} />
     </>
   );
