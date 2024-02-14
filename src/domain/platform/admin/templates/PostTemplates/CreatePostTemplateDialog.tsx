9b84735b0ce6542f437b12010f72703d61577c76
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2024-02-14 15:32:20 +0800

    TemplateDialogs structure fixed (#5545)

--- src/domain/platform/admin/templates/PostTemplates/CreatePostTemplateDialog.tsx
+++ src/domain/platform/admin/templates/PostTemplates/CreatePostTemplateDialog.tsx
@@ -1,9 +1,10 @@
 import PostTemplateForm, { PostTemplateFormSubmittedValues, PostTemplateFormValues } from './PostTemplateForm';
 import { useTranslation } from 'react-i18next';
-import DialogWithGrid from '../../../../../core/ui/dialog/DialogWithGrid';
+import DialogWithGrid, { DialogFooter } from '../../../../../core/ui/dialog/DialogWithGrid';
 import DialogHeader, { DialogHeaderProps } from '../../../../../core/ui/dialog/DialogHeader';
 import React from 'react';
-import FormikSubmitButton from '../../../../shared/components/forms/FormikSubmitButton';
+import { FormikSubmitButtonPure } from '../../../../shared/components/forms/FormikSubmitButton';
+import { DialogActions, DialogContent } from '@mui/material';
 
 interface CreatePostTemplateDialogProps {
   open: boolean;
@@ -17,20 +18,26 @@ const CreatePostTemplateDialog = ({ open, onClose, onSubmit }: CreatePostTemplat
   const values: Partial<PostTemplateFormValues> = {};
 
   return (
-    <DialogWithGrid
-      open={open}
-      onClose={onClose}
-      PaperProps={{ sx: { backgroundColor: 'background.default', width: theme => theme.spacing(128) } }}
-      maxWidth={false}
-    >
-      <DialogHeader onClose={onClose}>
-        {t('common.create-new-entity', { entity: t('templateLibrary.postTemplates.name') })}
-      </DialogHeader>
-      <PostTemplateForm
-        initialValues={values}
-        onSubmit={onSubmit}
-        actions={<FormikSubmitButton variant="contained">{t('common.create')}</FormikSubmitButton>}
+    <DialogWithGrid columns={12} open={open} onClose={onClose}>
+      <DialogHeader
+        title={t('common.create-new-entity', { entity: t('templateLibrary.postTemplates.name') })}
+        onClose={onClose}
       />
+      <DialogContent>
+        <PostTemplateForm
+          initialValues={values}
+          onSubmit={onSubmit}
+          actions={formik => (
+            <DialogFooter>
+              <DialogActions>
+                <FormikSubmitButtonPure variant="contained" formik={formik} onClick={() => formik.handleSubmit()}>
+                  {t('common.create')}
+                </FormikSubmitButtonPure>
+              </DialogActions>
+            </DialogFooter>
+          )}
+        />
+      </DialogContent>
     </DialogWithGrid>
   );
 };
