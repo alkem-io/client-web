1b44b4f7b243f3e4841d5e8d5696d5dfd00da6e7
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2024-02-16 14:23:37 +0100

    Change post description markdown text limit. (#5575)
    Co-authored-by: Carlos Cano <carlos@alkem.io>

--- src/core/ui/forms/field-length.constants.ts
+++ src/core/ui/forms/field-length.constants.ts
@@ -4,6 +4,7 @@ export const SMALL_TEXT_LENGTH = 128;
 export const MID_TEXT_LENGTH = 512;
 export const LONG_TEXT_LENGTH = 2048;
 export const MARKDOWN_TEXT_LENGTH = 8000;
+export const LONG_MARKDOWN_TEXT_LENGTH = 16000;
 export const COMMENTS_TEXT_LENGTH = 8000;
 
-export type MarkdownTextMaxLength = typeof MARKDOWN_TEXT_LENGTH;
+export type MarkdownTextMaxLength = typeof MARKDOWN_TEXT_LENGTH | typeof LONG_MARKDOWN_TEXT_LENGTH;
