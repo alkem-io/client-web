9f4c98090d50cc7b4742cae71ce798618d17c2d3
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2024-02-16 11:43:22 +0100

    Change post description markdown text limit.

--- src/core/ui/forms/MarkdownInput/MarkdownValidator.ts
+++ src/core/ui/forms/MarkdownInput/MarkdownValidator.ts
@@ -2,11 +2,12 @@ import { string } from 'yup';
 import TranslationKey from '../../../i18n/utils/TranslationKey';
 import { ValidationMessageWithPayload } from '../../../../domain/shared/i18n/ValidationMessageTranslation/ValidationMessageWithPayload';
 import { MarkdownTextMaxLength } from '../field-length.constants';
+import { MessageWithPayload } from '../../../../domain/shared/i18n/ValidationMessageTranslation';
 
 const translationKey: TranslationKey = 'components.wysiwyg-editor.validation.maxLength';
 
-const MarkdownValidator = (_maxLength: MarkdownTextMaxLength) => {
-  return string();
+const MarkdownValidator = (maxLength: MarkdownTextMaxLength) => {
+  return string().max(maxLength, MessageWithPayload(translationKey));
 };
 
 export const isMarkdownMaxLengthError = (
