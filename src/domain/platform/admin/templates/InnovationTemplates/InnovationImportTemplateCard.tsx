8b24c7d775ed5a177e9ae25991e8de3ac34a3608
Carlos Cano <carlos@alkem.io>
2024-02-20 12:34:05 +0100

    Fix build

--- src/domain/platform/admin/templates/InnovationTemplates/InnovationImportTemplateCard.tsx
+++ src/domain/platform/admin/templates/InnovationTemplates/InnovationImportTemplateCard.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { TemplateImportCardComponentProps } from '../InnovationPacks/ImportTemplatesDialogGalleryStep';
 import InnovationFlowTemplateCard from '../../../../collaboration/InnovationFlow/InnovationFlowTemplateCard/InnovationFlowTemplateCard';
+import { Template } from '../AdminTemplatesSection';
 
-interface InnovationImportTemplateCardProps extends TemplateImportCardComponentProps {}
+interface InnovationFlowTemplate extends Template {
+  definition: string;
+}
+
+interface InnovationImportTemplateCardProps extends TemplateImportCardComponentProps<InnovationFlowTemplate> {}
 
 const InnovationImportTemplateCard = ({ template, ...props }: InnovationImportTemplateCardProps) => {
   return <InnovationFlowTemplateCard template={template} innovationPack={template.innovationPack} {...props} />;
