29b2040b9d9e4787ddffc0c1067e0a9dcded0c6a
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2024-02-29 20:37:48 +0100

    Add expand button to Callouts list.

--- src/core/ui/list/LinksList.tsx
+++ src/core/ui/list/LinksList.tsx
@@ -1,10 +1,17 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useEffect, useState } from 'react';
 import { styled } from '@mui/styles';
-import { List as MuiList, ListItem as MuiListItem, ListItemIcon as MuiListItemIcon, Skeleton } from '@mui/material';
+import {
+  Box,
+  List as MuiList,
+  ListItem as MuiListItem,
+  ListItemIcon as MuiListItemIcon,
+  Skeleton,
+} from '@mui/material';
 import { BlockSectionTitle, CaptionSmall } from '../typography';
 import RouterLink from '../link/RouterLink';
 import { gutters } from '../grid/utils';
 import { times } from 'lodash';
+import CardExpandButton from '../card/CardExpandButton';
 
 const List = styled(MuiList)(() => ({ padding: 0 }));
 
@@ -33,14 +40,35 @@ export interface LinksListProps {
   loading?: boolean;
 }
 
+const COLLAPSED_LIST_ITEM_LIMIT = 5;
+
 const LinksList: FC<LinksListProps> = ({ items = [], emptyListCaption, loading = false }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [itemsToShow, setItemsToShow] = useState<Item[]>([]);
+
+  const handleExpand = () => {
+    if (!isExpanded) {
+      setItemsToShow(items);
+    } else {
+      setItemsToShow(items.slice(0, COLLAPSED_LIST_ITEM_LIMIT));
+    }
+    setIsExpanded(!isExpanded);
+  };
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    setItemsToShow(items.slice(0, COLLAPSED_LIST_ITEM_LIMIT));
+  }, [items, loading]);
+
   return (
     <List>
       {loading && times(3, i => <ListItem key={i} component={Skeleton} />)}
       {!loading && items.length === 0 && emptyListCaption && <CaptionSmall>{emptyListCaption}</CaptionSmall>}
       {!loading &&
-        items.length > 0 &&
-        items.map(item => (
+        itemsToShow.length > 0 &&
+        itemsToShow.map(item => (
           <ListItem key={item.id} component={RouterLink} to={item.uri}>
             <ListItemIcon>{item.icon}</ListItemIcon>
             <BlockSectionTitle minWidth={0} noWrap>
@@ -48,6 +76,9 @@ const LinksList: FC<LinksListProps> = ({ items = [], emptyListCaption, loading =
             </BlockSectionTitle>
           </ListItem>
         ))}
+      <Box flexGrow={1} display="flex" justifyContent="end" paddingX={1.5} onClick={handleExpand}>
+        {!loading && items.length > COLLAPSED_LIST_ITEM_LIMIT && <CardExpandButton expanded={isExpanded} />}
+      </Box>
     </List>
   );
 };
