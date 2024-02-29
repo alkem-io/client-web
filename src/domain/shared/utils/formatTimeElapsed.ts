48c9d2ea485ff3cdebbc65f2e119d2d874adf1c7
Carlos Cano <carlos@alkem.io>
2024-02-29 08:42:47 +0100

    fix build

--- src/domain/shared/utils/formatTimeElapsed.ts
+++ src/domain/shared/utils/formatTimeElapsed.ts
@@ -1,7 +1,5 @@
 import { TFunction } from 'react-i18next';
 
-type TimeFormat = 'short' | 'long';
-
 const ONE_SECOND = 1000;
 const ONE_MINUTE = 60 * ONE_SECOND;
 const ONE_HOUR = 60 * ONE_MINUTE;
@@ -12,7 +10,7 @@ const ONE_YEAR = 256 * ONE_DAY;
 export const formatTimeElapsed = (
   date: Date | string,
   t: TFunction<'translation', undefined>,
-  format: TimeFormat = 'short'
+  format: 'short' | 'long' = 'short'
 ): string => {
   const currentDate = new Date();
   const formatCandidate = new Date(date);
@@ -21,35 +19,35 @@ export const formatTimeElapsed = (
 
   if (diffInTime > ONE_YEAR) {
     const timeDiff = Math.round(diffInTime / ONE_YEAR);
-    return t(`common.time.${format}.timeAgo`, {
-      time: t(`common.time.${format}.year`, { count: timeDiff }),
+    return t(`common.time.${format}.timeAgo` as const, {
+      time: t(`common.time.${format}.year` as const, { count: timeDiff }),
     });
   }
   if (diffInTime > ONE_MONTH) {
     const timeDiff = Math.round(diffInTime / ONE_MONTH);
-    return t(`common.time.${format}.timeAgo`, {
-      time: t(`common.time.${format}.month`, { count: timeDiff }),
+    return t(`common.time.${format}.timeAgo` as const, {
+      time: t(`common.time.${format}.month` as const, { count: timeDiff }),
     });
   }
 
   if (diffInTime > ONE_DAY) {
     const timeDiff = Math.round(diffInTime / ONE_DAY);
-    return t(`common.time.${format}.timeAgo`, {
-      time: t(`common.time.${format}.day`, { count: timeDiff }),
+    return t(`common.time.${format}.timeAgo` as const, {
+      time: t(`common.time.${format}.day` as const, { count: timeDiff }),
     });
   }
 
   if (diffInTime > ONE_HOUR) {
     const timeDiff = Math.round(diffInTime / ONE_HOUR);
-    return t(`common.time.${format}.timeAgo`, {
-      time: t(`common.time.${format}.hour`, { count: timeDiff }),
+    return t(`common.time.${format}.timeAgo` as const, {
+      time: t(`common.time.${format}.hour` as const, { count: timeDiff }),
     });
   }
 
   if (diffInTime > ONE_MINUTE) {
     const timeDiff = Math.round(diffInTime / ONE_MINUTE);
-    return t(`common.time.${format}.timeAgo`, {
-      time: t(`common.time.${format}.minute`, { count: timeDiff }),
+    return t(`common.time.${format}.timeAgo` as const, {
+      time: t(`common.time.${format}.minute` as const, { count: timeDiff }),
     });
   }
 
@@ -59,7 +57,7 @@ export const formatTimeElapsed = (
     return 'just now';
   }
 
-  return t(`common.time.${format}.timeAgo`, {
-    time: t(`common.time.${format}.second`, { count: timeDiff }),
+  return t(`common.time.${format}.timeAgo` as const, {
+    time: t(`common.time.${format}.second` as const, { count: timeDiff }),
   });
 };
