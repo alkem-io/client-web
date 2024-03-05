0bcec23c884ac0184fea315b8515cc870d015d7b
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2024-03-05 15:09:18 +0100

    Fix link create/edit form validation + refactor namings (#5682)
    * Add proper form validation when creating links.
    
    * Fix link dialogs namings.
    
    * Translate "New link" and "New reference"
    
    ---------
    
    Co-authored-by: Carlos Cano <carlos@alkem.io>
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/domain/common/reference/newReferenceName.ts
+++ src/domain/common/reference/newReferenceName.ts
@@ -1,3 +1,5 @@
-export const newReferenceName = (count: number) => {
-  return count === 0 ? 'New reference' : `New reference (${count + 1})`;
+import { TFunction } from 'react-i18next';
+
+export const newReferenceName = (t: TFunction<'translation', undefined>, count: number) => {
+  return t('components.referenceSegment.newReference', { count: count + 1 });
 };
