25e0c79c5af885daa59363d97568625695d1ba80
Carlos Cano <carlos@alkem.io>
2024-03-20 13:39:48 +0100

    Fix that console error about MUI Fragments in menu

--- src/core/ui/content/PageContentBlockContextualMenu.tsx
+++ src/core/ui/content/PageContentBlockContextualMenu.tsx
@@ -1,15 +1,25 @@
-import React, { ReactNode, useState } from 'react';
+import React, { useState } from 'react';
 import Menu from '@mui/material/Menu';
 import { IconButton } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 interface PageContentBlockContextualMenuProps {
-  children: (provided: { closeMenu: () => void }) => ReactNode;
+  children: (provided: { closeMenu: () => void }) => JSX.Element;
 }
 
 export default function PageContentBlockContextualMenu({ children }: PageContentBlockContextualMenuProps) {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const closeMenu = () => setAnchorEl(null);
+
+  // This avoids a browser console warning about MUI Menu not accepting React.Fragment inside.
+  // If there is only one child it will be unwrapped.
+  const unwrapChildren = (elements: JSX.Element) => {
+    if (!Array.isArray(elements)) {
+      return elements.props.children;
+    } else {
+      return elements;
+    }
+  };
 
   return (
     <>
@@ -22,7 +32,7 @@ export default function PageContentBlockContextualMenu({ children }: PageContent
         <MoreVertIcon fontSize="small" sx={{ padding: 0 }} />
       </IconButton>
       <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={closeMenu}>
-        {children({ closeMenu })}
+        {unwrapChildren(children({ closeMenu }))}
       </Menu>
     </>
   );
