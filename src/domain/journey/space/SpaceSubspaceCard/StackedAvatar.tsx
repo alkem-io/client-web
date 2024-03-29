3e813dcc848057b170bbdde6b1a4b099af1cb3f7
Emilia Pavlova <emilia.pavlova@gmail.com>
2024-03-29 17:57:13 +0200

    Remove alt text for avatar

--- src/domain/journey/space/SpaceSubspaceCard/StackedAvatar.tsx
+++ src/domain/journey/space/SpaceSubspaceCard/StackedAvatar.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next';
 import { Box } from '@mui/material';
 import { gutters } from '../../../../core/ui/grid/utils';
 import JourneyAvatar from '../../common/JourneyAvatar/JourneyAvatar';
@@ -8,7 +7,6 @@ interface StackedAvatarProps {
 }
 
 const StackedAvatar = ({ avatarUris }: StackedAvatarProps) => {
-  const { t } = useTranslation();
   return (
     <Box sx={{ width: gutters(2), height: gutters(2), position: 'relative', flex: '0 0 15%' }}>
       {avatarUris.map((avatarUri, index) => {
@@ -26,7 +24,6 @@ const StackedAvatar = ({ avatarUris }: StackedAvatarProps) => {
               borderRadius: theme => theme.spacing(0.6),
             }}
             src={avatarUri}
-            alt={t('visuals-alt-text.avatar.name')}
           />
         );
       })}
