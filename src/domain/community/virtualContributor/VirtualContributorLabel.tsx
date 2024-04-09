47afaf4acbc70bd78695c7cced68eaee41899c86
Andrew Pazniak <andrew.pazniak@gmail.com>
2024-04-09 15:50:19 +0200

    Custom VC icon

--- src/domain/community/virtualContributor/VirtualContributorLabel.tsx
+++ src/domain/community/virtualContributor/VirtualContributorLabel.tsx
@@ -1,13 +1,18 @@
-import { SmartToyOutlined } from '@mui/icons-material';
 import { CaptionSmall } from '../../../core/ui/typography';
 import { useTranslation } from 'react-i18next';
+import { ReactComponent as VirtualContributorIcon } from './VirtualContributor.svg';
+import { SvgIcon } from '@mui/material';
+import { gutters } from '../../../core/ui/grid/utils';
 
 const VirtualContributorLabel = () => {
   const { t } = useTranslation();
 
   return (
     <CaptionSmall sx={{ display: 'flex', gap: 0.5, alignItems: 'center' }}>
-      <SmartToyOutlined fontSize="inherit" />
+      <SvgIcon
+        component={VirtualContributorIcon}
+        sx={{ width: gutters(0.75), height: gutters(0.75), marginBottom: 0.2 }}
+      />
       {t('community.virtualContributor')}
     </CaptionSmall>
   );
