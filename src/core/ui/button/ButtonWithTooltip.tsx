35224461de01cb5eea0b11992c09e3de58c67927
Carlos Cano <carlos@alkem.io>
2024-04-11 17:08:27 +0200

    Add ButtonWithTooltip component

---
+++ src/core/ui/button/ButtonWithTooltip.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Button, ButtonProps, Tooltip, TooltipProps, useTheme } from '@mui/material';
+import { gutters } from '../grid/utils';
+
+interface ButtonWithTooltipProps extends ButtonProps {
+  tooltip: string;
+  iconButton?: boolean;
+  tooltipProps?: TooltipProps;
+}
+
+const ButtonWithTooltip: React.FC<ButtonWithTooltipProps> = ({ tooltip, iconButton, tooltipProps, ...props }) => {
+  const theme = useTheme();
+
+  const isContained = props.variant === 'contained';
+  const isOutlined = props.variant === 'outlined';
+
+  const tooltipStyle = {
+    padding: gutters(0.5)(theme),
+    backgroundColor: isContained ? theme.palette.primary.main : theme.palette.background.paper,
+    color: isContained ? theme.palette.primary.contrastText : theme.palette.primary.main,
+    border: isContained ? undefined : `1px solid ${theme.palette.divider}`,
+    '.MuiTooltip-arrow': {
+      color: isContained ? theme.palette.primary.main : theme.palette.background.paper,
+      '&::before': {
+        border: isContained ? undefined : `1px solid  ${theme.palette.divider}`,
+      },
+    },
+  };
+
+  const buttonStyle = {
+    backgroundColor: isOutlined ? theme.palette.background.paper : undefined,
+    borderColor: isOutlined ? theme.palette.divider : undefined,
+    ...props.sx,
+    minWidth: iconButton ? 0 : undefined,
+  };
+
+  return (
+    <Tooltip arrow title={tooltip} componentsProps={{ tooltip: { sx: tooltipStyle } }} {...tooltipProps}>
+      <Button aria-label={tooltip} {...props} sx={buttonStyle} />
+    </Tooltip>
+  );
+};
+
+export default ButtonWithTooltip;
