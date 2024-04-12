4c58a38bcb1ef5967371a4164cf9290409208607
Emily <emilia.pavlova@gmail.com>
2024-04-12 13:23:30 +0300

    Move time stamp on the title line (#5900)
    * Move time stamp on the title line
    
    * Fix error
    
    ---------
    
    Co-authored-by: Carlos Cano <carlos@alkem.io>

--- src/core/ui/markdown/WrapperMarkdown.tsx
+++ src/core/ui/markdown/WrapperMarkdown.tsx
@@ -27,7 +27,7 @@ export const WrapperMarkdown = ({
       disableParagraphPadding={disableParagraphPadding}
       caption={caption}
     >
-      <Box sx={{ li: { marginY: caption ? 0 : 1 } }}>
+      <Box sx={{ li: { marginY: caption ? 0 : 1 }, display: plain ? 'inline' : undefined }}>
         <ReactMarkdown
           components={components}
           remarkPlugins={[gfm, [PlainText, { enabled: plain }]]}
--- src/core/ui/markdown/components/MarkdownParagraph.tsx
+++ src/core/ui/markdown/components/MarkdownParagraph.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { ReactMarkdownProps } from 'react-markdown/lib/complex-types';
+import { useMarkdownOptions } from '../MarkdownOptionsContext';
 import createMarkdownComponent from './MarkdownComponent';
 
 const Base = createMarkdownComponent('p');
 
 const MarkdownParagraph = (props: ReactMarkdownProps) => {
-  return <Base {...props} />;
+  const { plain } = useMarkdownOptions();
+
+  return <Base {...props} sx={{ display: plain ? 'inline' : undefined }} />;
 };
 
 export default MarkdownParagraph;
