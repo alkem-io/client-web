06b48caa7f2d0684d6c2e515d24d0a7a616c965e
bobbykolev <bobbykolev@abv.bg>
2024-04-15 13:50:24 +0300

    PR fixes - renaming and siplifying

--- src/core/ui/content/ContentColumn.tsx
+++ src/core/ui/content/ContentColumn.tsx
@@ -1,20 +1,14 @@
 import React, { forwardRef } from 'react';
-import { useColumns } from '../grid/GridContext';
 import PageContentColumn from './PageContentColumn';
-import { GRID_COLUMNS_MOBILE } from '../grid/constants';
 import { BoxProps } from '@mui/material';
 
+// GRID COLUMNS - INFO_COLUMNS => 12 - 3
 const CONTENT_COLUMNS = 9;
 
-const ContentColumn = forwardRef<HTMLDivElement, BoxProps>(({ children, ...props }, ref) => {
-  const fullWidthColumns = useColumns();
-  const columnsToUse = fullWidthColumns === GRID_COLUMNS_MOBILE ? GRID_COLUMNS_MOBILE : CONTENT_COLUMNS;
-
-  return (
-    <PageContentColumn columns={columnsToUse} {...props} ref={ref}>
-      {children}
-    </PageContentColumn>
-  );
-});
+const ContentColumn = forwardRef<HTMLDivElement, BoxProps>(({ children, ...props }, ref) => (
+  <PageContentColumn columns={CONTENT_COLUMNS} {...props} ref={ref}>
+    {children}
+  </PageContentColumn>
+));
 
 export default ContentColumn;
--- src/core/ui/content/InfoColumn.tsx
+++ src/core/ui/content/InfoColumn.tsx
@@ -4,11 +4,12 @@ import { BoxProps } from '@mui/material';
 import PageContentColumn from './PageContentColumn';
 import { GRID_COLUMNS_MOBILE } from '../grid/constants';
 
+// GRID COLUMNS - CONTENT_COLUMNS => 12 - 9
 const INFO_COLUMNS = 3;
 
 const InfoColumn = forwardRef<HTMLDivElement, BoxProps>(({ children, ...props }, ref) => {
-  const fullWidthColumns = useColumns();
-  const columnsToUse = fullWidthColumns === GRID_COLUMNS_MOBILE ? GRID_COLUMNS_MOBILE : INFO_COLUMNS;
+  const availableColumns = useColumns();
+  const columnsToUse = availableColumns === GRID_COLUMNS_MOBILE ? GRID_COLUMNS_MOBILE : INFO_COLUMNS;
 
   return (
     <PageContentColumn columns={columnsToUse} {...props} ref={ref}>
