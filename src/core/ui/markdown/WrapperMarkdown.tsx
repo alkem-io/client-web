e497e52a773796b2e4c5849ac7b1b56bbf38b928
Emilia Pavlova <emilia.pavlova@gmail.com>
2024-04-16 16:06:59 +0300

    Add border radius

--- src/core/ui/markdown/WrapperMarkdown.tsx
+++ src/core/ui/markdown/WrapperMarkdown.tsx
@@ -27,7 +27,13 @@ export const WrapperMarkdown = ({
       disableParagraphPadding={disableParagraphPadding}
       caption={caption}
     >
-      <Box sx={{ li: { marginY: caption ? 0 : 1 }, display: plain ? 'inline' : undefined, img: { maxWidth: '100%' } }}>
+      <Box
+        sx={{
+          li: { marginY: caption ? 0 : 1 },
+          display: plain ? 'inline' : undefined,
+          img: { maxWidth: '100%', borderRadius: theme => `${theme.spacing(0.6)}` },
+        }}
+      >
         <ReactMarkdown
           components={components}
           remarkPlugins={[gfm, [PlainText, { enabled: plain }]]}
