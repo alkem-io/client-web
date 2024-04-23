cae056b944ded0aee7e01c52f88948ad6dcc7164
bobbykolev <bobbykolev@abv.bg>
2024-04-23 15:53:06 +0300

    mobile full-width for activity dialog

--- src/domain/journey/common/Activity/ActivityDialog.tsx
+++ src/domain/journey/common/Activity/ActivityDialog.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { DialogContent, useMediaQuery } from '@mui/material';
+import { Theme } from '@mui/material/styles';
 import DialogWithGrid from '../../../../core/ui/dialog/DialogWithGrid';
 import DialogHeader from '../../../../core/ui/dialog/DialogHeader';
-import { DialogContent } from '@mui/material';
 import SubspacePageContainer from '../../subspace/containers/SubspacePageContainer';
 import { useRouteResolver } from '../../../../main/routing/resolvers/RouteResolver';
 import DashboardAllContributionsBlock from '../dashboardRecentContributionsBlock/DashboardAllContributionsBlock';
@@ -16,9 +17,10 @@ export interface SubspacesListDialogProps {
 const SubspacesListDialog = ({ open = false, journeyId, onClose }: SubspacesListDialogProps) => {
   const { t } = useTranslation();
   const { journeyTypeName } = useRouteResolver();
+  const isMobile = useMediaQuery<Theme>(theme => theme.breakpoints.down('md'));
 
   return (
-    <DialogWithGrid open={open} columns={8}>
+    <DialogWithGrid open={open} fullScreen={isMobile} columns={8} aria-labelledby="activity-dialog">
       <DialogHeader onClose={onClose} title={t('common.contributions')} />
       <DialogContent>
         <SubspacePageContainer challengeId={journeyId}>
