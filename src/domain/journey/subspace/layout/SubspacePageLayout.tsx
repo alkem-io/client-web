0519d94c4ab165eb25ae25ee6262f30c1ef7d475
Emilia Pavlova <emilia.pavlova@gmail.com>
2024-04-26 19:21:19 +0300

    Add Join/Apply button for subspaces

--- src/domain/journey/subspace/layout/SubspacePageLayout.tsx
+++ src/domain/journey/subspace/layout/SubspacePageLayout.tsx
@@ -46,6 +46,9 @@ import WelcomeBlock from './WelcomeBlock';
 import { UrlBaseProvider } from '../../../../core/ui/link/UrlBase';
 import ButtonWithTooltip from '../../../../core/ui/button/ButtonWithTooltip';
 import { theme } from '../../../../core/ui/themes/default/Theme';
+import ApplicationButton from '../../../community/application/applicationButton/ApplicationButton';
+import ApplicationButtonContainer from '../../../community/application/containers/ApplicationButtonContainer';
+import PageContentColumn from '../../../../core/ui/content/PageContentColumn';
 
 export interface SubspacePageLayoutProps {
   journeyId: string | undefined;
@@ -182,6 +185,8 @@ const SubspacePageLayout = ({
     return !isDialogDef(action) || !consumedActions[action.props.dialogType];
   });
 
+  const hasExtendedApplicationButton = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'));
+
   return (
     <NotFoundErrorBoundary
       errorComponent={
@@ -239,6 +244,25 @@ const SubspacePageLayout = ({
                   flexShrink={1}
                   minWidth={0}
                 >
+                  <ApplicationButtonContainer>
+                    {({ applicationButtonProps }, { loading }) => {
+                      if (loading || applicationButtonProps.isMember) {
+                        return null;
+                      }
+                      return (
+                        <PageContentColumn columns={9}>
+                          <ApplicationButton
+                            {...applicationButtonProps}
+                            loading={loading}
+                            component={FullWidthButton}
+                            extended={hasExtendedApplicationButton}
+                            journeyTypeName="subspace"
+                          />
+                        </PageContentColumn>
+                      );
+                    }}
+                  </ApplicationButtonContainer>
+
                   {!isMobile && <InnovationFlowRenderPoint />}
                   {children}
                 </PageContentColumnBase>
