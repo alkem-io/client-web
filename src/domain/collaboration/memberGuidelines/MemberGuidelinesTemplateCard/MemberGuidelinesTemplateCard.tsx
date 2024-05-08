f3c6ed07322753fc1c8d7a5c5f35fbc9bb4cfe39
Emilia Pavlova <emilia.pavlova@gmail.com>
2024-05-08 18:13:49 +0300

    Create Member Guidelines Template card

---
+++ src/domain/collaboration/memberGuidelines/MemberGuidelinesTemplateCard/MemberGuidelinesTemplateCard.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react';
+import { Skeleton } from '@mui/material';
+import CardHeader from '../../../../core/ui/card/CardHeader';
+import CardHeaderCaption from '../../../../core/ui/card/CardHeaderCaption';
+import CardSegmentCaption from '../../../../core/ui/card/CardSegmentCaption';
+import ContributeCard from '../../../../core/ui/card/ContributeCard';
+import { Caption } from '../../../../core/ui/typography/components';
+import CardDescriptionWithTags from '../../../../core/ui/card/CardDescriptionWithTags';
+import CardDetails from '../../../../core/ui/card/CardDetails';
+import {
+  InnovationPackTemplate,
+  TemplateCardBaseProps,
+} from '../../templates/CollaborationTemplatesLibrary/TemplateBase';
+import InnovationPackIcon from '../../InnovationPack/InnovationPackIcon';
+import { MemberGuidelinesIcon } from '../icon/MemberGuidelinesIcon';
+
+interface MemberGuidelinesTemplateCardProps extends TemplateCardBaseProps<InnovationPackTemplate> {}
+
+const MemberGuidelinesTemplateCard: FC<MemberGuidelinesTemplateCardProps> = ({ template, loading, onClick }) => {
+  return (
+    <ContributeCard onClick={onClick}>
+      <CardHeader title={template?.profile.displayName} iconComponent={MemberGuidelinesIcon}>
+        {loading && <Skeleton />}
+        <CardHeaderCaption logoUrl={template?.innovationPack?.provider?.profile.avatar?.uri}>
+          {template?.innovationPack?.provider?.profile.displayName}
+        </CardHeaderCaption>
+      </CardHeader>
+      <CardDetails>
+        <CardDescriptionWithTags tags={template?.profile.tagset?.tags}>
+          {template?.profile.description}
+        </CardDescriptionWithTags>
+      </CardDetails>
+      <CardSegmentCaption icon={<InnovationPackIcon />}>
+        <Caption noWrap>{template?.innovationPack?.profile.displayName}</Caption>
+      </CardSegmentCaption>
+      {loading && <Skeleton />}
+    </ContributeCard>
+  );
+};
+
+export default MemberGuidelinesTemplateCard;
