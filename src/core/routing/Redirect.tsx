b8ebcfd32536602c2cbd3fcd9c77c8a147f8c4e1
Andrew Pazniak <594548+me-andre@users.noreply.github.com>
2024-05-17 11:05:45 +0300

    Catch-all route for /verify to handle Kratos misconfiguration (#6206)
    * Catch-all route for /verify

--- src/core/routing/Redirect.tsx
+++ src/core/routing/Redirect.tsx
@@ -5,6 +5,13 @@ interface RedirectProps extends NavigateProps {
   to: string;
 }
 
+/**
+ * Redirects to a different root path while keeping all path segments that * has matched.
+ * E.g. under a route /registration/* <Redirect to="/login"> takes from /registration/success to /login/success
+ * @param to
+ * @param props
+ * @constructor
+ */
 const Redirect = ({ to, ...props }: RedirectProps) => {
   const match = useParams();
 
