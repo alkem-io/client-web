1db01553dd4b70937eb76215166145b9b01e496d
Andrew Pazniak <andrew.pazniak@gmail.com>
2024-05-20 16:41:10 +0300

    Translations and terms

--- src/domain/journey/space/createSpace/CreateSpaceDialog.tsx
+++ src/domain/journey/space/createSpace/CreateSpaceDialog.tsx
@@ -2,10 +2,10 @@ import DialogWithGrid, { DialogFooter } from '../../../../core/ui/dialog/DialogW
 import DialogHeader from '../../../../core/ui/dialog/DialogHeader';
 import { useBackToStaticPath } from '../../../../core/routing/useBackToPath';
 import { ROUTE_HOME } from '../../../platform/routes/constants';
-import { Checkbox, DialogContent, FormControlLabel, TextField } from '@mui/material';
+import { Checkbox, DialogContent, FormControlLabel, Link, TextField } from '@mui/material';
 import { Caption } from '../../../../core/ui/typography';
 import { Formik } from 'formik';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import React, { useMemo, useState } from 'react';
 import { DEFAULT_TAGSET } from '../../../common/tags/tagset.constants';
 import { Tagset, TagsetType } from '../../../../core/apollo/generated/graphql-schema';
@@ -25,6 +25,9 @@ import { useUserContext } from '../../../community/user';
 import { useAuthenticationContext } from '../../../../core/auth/authentication/hooks/useAuthenticationContext';
 import { Navigate } from 'react-router-dom';
 import NameIdField from '../../../../core/utils/nameId/NameIdField';
+import WrapperMarkdown from '../../../../core/ui/markdown/WrapperMarkdown';
+import RouterLink from '../../../../core/ui/link/RouterLink';
+import { useConfig } from '../../../platform/config/useConfig';
 
 const CreateSpaceDialog = () => {
   const handleClose = useBackToStaticPath(ROUTE_HOME);
@@ -66,63 +69,89 @@ const CreateSpaceDialog = () => {
 
   const [hasAcceptedTerms, setHasAcceptedTerms] = useState(false);
 
+  const [isTermsDialogOpen, setIsTermsDialogOpen] = useState(false);
+
+  const config = useConfig();
+
   if (!isAuthenticated) {
     return <Navigate to={ROUTE_HOME} replace />;
   }
 
   return (
-    <DialogWithGrid open columns={12} onClose={handleClose}>
-      <DialogHeader title="Create a Space" onClose={handleClose} />
-      <DialogContent sx={{ paddingTop: 0, marginTop: -1 }}>
-        <PageContentBlockSeamless disablePadding>
-          <Caption>
-            To create a safe environment to train your Virtual Contributor, you first need to create a Space. Enter the
-            details for this Space below.
-          </Caption>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            enableReinitialize
-            onSubmit={async values => {
-              onSubmit(values);
-            }}
-          >
-            {({ handleSubmit }) => {
-              return (
-                <>
-                  <FormikInputField name="name" title={t('components.nameSegment.name')} required />
-                  <NameIdField name="nameID" title={t('common.url')} required />
-                  <TextField value={user?.user.profile.displayName} disabled />
-                  <FormikInputField
-                    name="tagline"
-                    title={t('context.space.tagline.title')}
-                    rows={3}
-                    maxLength={SMALL_TEXT_LENGTH}
-                  />
-                  <TagsetSegment title={t('common.tags')} tagsets={tagsets} />
-                  <FormControlLabel
-                    value={hasAcceptedTerms}
-                    onChange={(event, isChecked) => setHasAcceptedTerms(isChecked)}
-                    required
-                    control={<Checkbox />}
-                    label={
-                      <Caption>
-                        I have read and accept the terms and agreements (click here to open them) for creating a Space.
-                      </Caption>
-                    }
-                  />
-                  <DialogFooter>
-                    <Actions justifyContent="end" padding={gutters()}>
-                      <SaveButton onClick={handleSubmit} loading={loading} />
-                    </Actions>
-                  </DialogFooter>
-                </>
-              );
-            }}
-          </Formik>
-        </PageContentBlockSeamless>
-      </DialogContent>
-    </DialogWithGrid>
+    <>
+      <DialogWithGrid open columns={12} onClose={handleClose}>
+        <DialogHeader title={t('createSpace.title')} onClose={handleClose} />
+        <DialogContent sx={{ paddingTop: 0, marginTop: -1 }}>
+          <PageContentBlockSeamless disablePadding>
+            <Caption>{t('createSpace.subtitle')}</Caption>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              enableReinitialize
+              onSubmit={onSubmit}
+            >
+              {({ handleSubmit }) => {
+                return (
+                  <>
+                    <FormikInputField name="name" title={t('components.nameSegment.name')} required />
+                    <NameIdField name="nameID" title={t('common.url')} required />
+                    <TextField value={user?.user.profile.displayName} disabled />
+                    <FormikInputField
+                      name="tagline"
+                      title={t('context.space.tagline.title')}
+                      rows={3}
+                      maxLength={SMALL_TEXT_LENGTH}
+                    />
+                    <TagsetSegment title={t('common.tags')} tagsets={tagsets} />
+                    <FormControlLabel
+                      value={hasAcceptedTerms}
+                      onChange={(event, isChecked) => setHasAcceptedTerms(isChecked)}
+                      required
+                      control={<Checkbox />}
+                      label={
+                        <Caption>
+                          <Trans
+                            i18nKey="createSpace.terms.checkboxLabel"
+                            components={{
+                              terms: (
+                                <Link
+                                  underline="always"
+                                  onClick={event => {
+                                    event.stopPropagation();
+                                    event.preventDefault();
+                                    setIsTermsDialogOpen(true);
+                                  }}
+                                />
+                              ),
+                            }}
+                          />
+                        </Caption>
+                      }
+                    />
+                    <DialogFooter>
+                      <Actions justifyContent="end" padding={gutters()}>
+                        <SaveButton onClick={handleSubmit} loading={loading} />
+                      </Actions>
+                    </DialogFooter>
+                  </>
+                );
+              }}
+            </Formik>
+          </PageContentBlockSeamless>
+        </DialogContent>
+      </DialogWithGrid>
+      <DialogWithGrid columns={8} open={isTermsDialogOpen} onClose={() => setIsTermsDialogOpen(false)}>
+        <DialogHeader title={t('createSpace.terms.dialogTitle')} onClose={() => setIsTermsDialogOpen(false)} />
+        <DialogContent sx={{ paddingTop: 0 }}>
+          <WrapperMarkdown caption>{t('createSpace.terms.dialogContent')}</WrapperMarkdown>
+          {config.locations?.terms && (
+            <RouterLink to={config.locations?.terms ?? ''} blank underline="always">
+              <Caption>{t('createSpace.terms.fullTermsLink')}</Caption>
+            </RouterLink>
+          )}
+        </DialogContent>
+      </DialogWithGrid>
+    </>
   );
 };
 
