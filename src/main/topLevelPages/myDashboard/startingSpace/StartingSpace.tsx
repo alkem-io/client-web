d8727b58b44319567d1ed829a7401b71cace1a9f
Bobby Kolev <bobbykolev@abv.bg>
2024-05-21 15:17:24 +0300

    Create a Space Button (#6180)
    client-6173 Create a Space Button placement logic

--- src/main/topLevelPages/myDashboard/startingSpace/StartingSpace.tsx
+++ src/main/topLevelPages/myDashboard/startingSpace/StartingSpace.tsx
@@ -2,30 +2,28 @@ import React, { useState } from 'react';
 import { BlockTitle, Caption } from '../../../../core/ui/typography';
 import { Trans } from 'react-i18next';
 import DashboardBanner from '../../../../core/ui/content/DashboardBanner';
-import PlansTableDialog from '../../../../domain/licence/planstable/PlansTableDialog';
-import { Plan } from '../../../../domain/licence/planstable/Plan';
+import { useUserContext } from '../../../../domain/community/user/hooks/useUserContext';
+import { AuthorizationPrivilege } from '../../../../core/apollo/generated/graphql-schema';
 
 const StartingSpace = () => {
-  const [plansDialogOpen, setPlansDialogOpen] = useState(false);
-  const onSelectPlan = (plan: Plan) => {
-    // TODO: Handle plan selection
-    console.log(`SELECTED PLAN ${plan}`);
-    setPlansDialogOpen(false);
-  };
+  const { t } = useTranslation();
+  const { user } = useUserContext();
+  let createLink = t('pages.home.sections.startingSpace.url');
+
+  if (user && user.hasPlatformPrivilege(AuthorizationPrivilege.CreateSpace)) {
+    createLink = 'create-space';
+  }
 
   return (
-    <>
-      <DashboardBanner onClick={() => setPlansDialogOpen(true)} to="">
-        <Trans
-          i18nKey="pages.home.sections.startingSpace.title"
-          components={{
-            big: <BlockTitle />,
-            small: <Caption />,
-          }}
-        />
-      </DashboardBanner>
-      <PlansTableDialog open={plansDialogOpen} onClose={() => setPlansDialogOpen(false)} onSelectPlan={onSelectPlan} />
-    </>
+    <DashboardBanner to={createLink}>
+      <Trans
+        i18nKey="pages.home.sections.startingSpace.title"
+        components={{
+          big: <BlockTitle />,
+          small: <Caption />,
+        }}
+      />
+    </DashboardBanner>
   );
 };
 
