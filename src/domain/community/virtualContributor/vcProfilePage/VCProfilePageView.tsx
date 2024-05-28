2599604d32aaf1807f4e8918371869666cfd49cd
bobbykolev <bobbykolev@abv.bg>
2024-05-28 18:27:29 +0300

    VC Profile - VC Default space display + style improvements

--- src/domain/community/virtualContributor/vcProfilePage/VCProfilePageView.tsx
+++ src/domain/community/virtualContributor/vcProfilePage/VCProfilePageView.tsx
@@ -7,21 +7,43 @@ import PageContent from '../../../../core/ui/content/PageContent';
 import PageContentColumn from '../../../../core/ui/content/PageContentColumn';
 import { VirtualContributorQuery } from '../../../../core/apollo/generated/graphql-schema';
 import VCProfileView from '../views/VCProfileView';
-import { BlockTitle, Text } from '../../../../core/ui/typography';
+import { BlockSectionTitle, BlockTitle, Text } from '../../../../core/ui/typography';
 import PageContentBlock from '../../../../core/ui/content/PageContentBlock';
 import HostCardClean from './HostCardClean';
 import useTheme from '@mui/material/styles/useTheme';
 import { Trans, useTranslation } from 'react-i18next';
+import BadgeCardView from '../../../../core/ui/list/BadgeCardView';
+import Avatar from '../../../../core/ui/avatar/Avatar';
+import RouterLink from '../../../../core/ui/link/RouterLink';
 
 interface Props {
   virtualContributor: VirtualContributorQuery['virtualContributor'] | undefined;
 }
+
+const DEFULT_PROFILE = {
+  avatar: {
+    uri: 'https://alkem.io/api/private/rest/storage/document/1057e0c1-2d47-4821-8848-20ec19cb2a0d',
+  },
+  displayName: 'Welcome @ Alkemio!',
+  tagline: 'Take 5 minutes to get started',
+  url: 'https://alkem.io/welcome-space',
+};
 
 export const VCProfilePageView: FC<PropsWithChildren<Props>> = ({ virtualContributor }) => {
   const theme = useTheme();
   const { t } = useTranslation();
 
   const name = virtualContributor?.profile.displayName || t('pages.virtual-contributor-profile.default-name');
+  const profile = DEFULT_PROFILE;
+
+  const SectionTitle = ({ children }) => (
+    <BlockTitle display={'flex'} alignItems={'center'} gap={theme.spacing(1)}>
+      {children}
+    </BlockTitle>
+  );
+  const SectionContent = ({ children, withBottomOffset = false }) => (
+    <Text marginBottom={withBottomOffset ? theme.spacing(2) : 0}>{children}</Text>
+  );
 
   return (
     <PageContent>
@@ -31,44 +53,61 @@ export const VCProfilePageView: FC<PropsWithChildren<Props>> = ({ virtualContrib
       </PageContentColumn>
       <PageContentColumn columns={8}>
         <PageContentBlock>
-          <BlockTitle display={'flex'} alignItems={'center'}>
+          <SectionTitle>
             <BookIcon htmlColor={theme.palette.icons.dark} sx={{ fontSize: '18px' }} />
-            &nbsp;{t('pages.virtual-contributor-profile.sections.knowledge.title')}
-          </BlockTitle>
-          <Text>
+            {t('pages.virtual-contributor-profile.sections.knowledge.title')}
+          </SectionTitle>
+          <SectionContent withBottomOffset>
             <Trans i18nKey="pages.virtual-contributor-profile.sections.knowledge.description" values={{ name }} />
-          </Text>
-          <BlockTitle display={'flex'} alignItems={'center'}>
+            <BadgeCardView
+              marginTop={theme.spacing(2)}
+              visual={
+                <Avatar
+                  src={profile.avatar?.uri}
+                  aria-label="User avatar"
+                  alt={t('common.avatar-of', { user: profile.displayName })}
+                >
+                  {profile.displayName}
+                </Avatar>
+              }
+              component={RouterLink}
+              to={profile.url}
+            >
+              <BlockSectionTitle>{profile.displayName}</BlockSectionTitle>
+              <BlockSectionTitle>{profile.tagline}</BlockSectionTitle>
+            </BadgeCardView>
+          </SectionContent>
+          <SectionTitle>
             <RecordVoiceOverIcon htmlColor={theme.palette.icons.dark} sx={{ fontSize: '18px' }} />
-            &nbsp;{t('pages.virtual-contributor-profile.sections.personality.title')}
-          </BlockTitle>
-          <Text>
+            {t('pages.virtual-contributor-profile.sections.personality.title')}
+          </SectionTitle>
+          <SectionContent withBottomOffset>
             <Trans i18nKey="pages.virtual-contributor-profile.sections.personality.description" values={{ name }} />
-          </Text>
-          <BlockTitle display={'flex'} alignItems={'center'}>
+          </SectionContent>
+          <SectionTitle>
             <CloudDownloadIcon htmlColor={theme.palette.icons.dark} sx={{ fontSize: '18px' }} />
-            &nbsp;{t('pages.virtual-contributor-profile.sections.context.title')}
-          </BlockTitle>
-          <Text>
+            {t('pages.virtual-contributor-profile.sections.context.title')}
+          </SectionTitle>
+          <SectionContent>
             <Trans i18nKey="pages.virtual-contributor-profile.sections.context.description" values={{ name }} />
             <Trans
               i18nKey="pages.virtual-contributor-profile.sections.context.bullets"
               components={{ ul: <ul />, li: <li /> }}
             />
-          </Text>
+          </SectionContent>
         </PageContentBlock>
         <PageContentBlock>
-          <BlockTitle display={'flex'} alignItems={'center'}>
+          <SectionTitle>
             <ShieldIcon htmlColor={theme.palette.icons.dark} sx={{ fontSize: '18px' }} />
-            &nbsp;{t('pages.virtual-contributor-profile.sections.privacy.title')}
-          </BlockTitle>
-          <Text>
+            {t('pages.virtual-contributor-profile.sections.privacy.title')}
+          </SectionTitle>
+          <SectionContent>
             <Trans i18nKey="pages.virtual-contributor-profile.sections.privacy.description" values={{ name }} />
             <Trans
               i18nKey="pages.virtual-contributor-profile.sections.privacy.bullets"
               components={{ ul: <ul />, li: <li /> }}
             />
-          </Text>
+          </SectionContent>
         </PageContentBlock>
       </PageContentColumn>
     </PageContent>
