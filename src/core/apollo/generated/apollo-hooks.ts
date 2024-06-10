484858a8cba76cca263a0aea95210187973f96b7
Carlos Cano <carlos@alkem.io>
2024-06-10 17:14:43 +0200

    Search: Callout results (#6361)
    * callout search result
    
    * wip
    
    * wip
    
    * Fixes and wrap up
    
    * Removed Callouts filter for now
    
    * Cards are links
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Svetoslav <svetoslav@alkem.io>

--- src/core/apollo/generated/apollo-hooks.ts
+++ src/core/apollo/generated/apollo-hooks.ts
@@ -3272,17 +3272,64 @@ export const SearchResultUserFragmentDoc = gql`
   }
   ${SearchResultProfileFragmentDoc}
 `;
+export const CalloutParentFragmentDoc = gql`
+  fragment CalloutParent on SearchResultCallout {
+    space {
+      id
+      profile {
+        id
+        displayName
+        url
+      }
+      level
+    }
+  }
+`;
 export const SearchResultCalloutFragmentDoc = gql`
   fragment SearchResultCallout on SearchResultCallout {
+    id
     callout {
       id
       nameID
       type
       framing {
         id
+        profile {
+          id
+          displayName
+          description
+          url
+          tagset {
+            ...TagsetDetails
+          }
+        }
+      }
+      contributionPolicy {
+        id
+        state
+        allowedContributionTypes
+      }
+      contributions {
+        id
+        post {
+          id
+        }
+        whiteboard {
+          id
+        }
+        link {
+          id
+        }
+      }
+      comments {
+        id
+        messagesCount
       }
     }
+    ...CalloutParent
   }
+  ${TagsetDetailsFragmentDoc}
+  ${CalloutParentFragmentDoc}
 `;
 export const SearchResultOrganizationFragmentDoc = gql`
   fragment SearchResultOrganization on SearchResultOrganization {
@@ -21158,6 +21205,7 @@ export const SearchDocument = gql`
           ...SearchResultCallout
         }
       }
+      calloutResultsCount
       contributorResults {
         id
         score
