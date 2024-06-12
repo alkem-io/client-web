10256c8b596defa0ebb8637bb548e3fdec6ec3b6
Andrew Pazniak <andrew.pazniak@gmail.com>
2024-06-12 11:31:08 +0300

    Prefilling Callout Template from when created from Callout

--- src/domain/platform/admin/templates/CalloutTemplates/CreateCalloutTemplateDialog.tsx
+++ src/domain/platform/admin/templates/CalloutTemplates/CreateCalloutTemplateDialog.tsx
@@ -6,15 +6,79 @@ import CalloutTemplateForm, {
   CalloutTemplateFormValues,
 } from './CalloutTemplateForm';
 import TemplateDialogBase from '../../../../collaboration/templates/templateDialog/TemplateDialogBase';
+import { Reference, Tagset } from '../../../../common/profile/Profile';
+import { WhiteboardFragmentWithCallout } from '../../../../collaboration/callout/useCallouts/useCallouts';
+import {
+  AuthorizationPrivilege,
+  CalloutGroupName,
+  CalloutState,
+  CalloutType,
+  ContributeTabPostFragment,
+  MessageDetailsFragment,
+  WhiteboardDetailsFragment,
+} from '../../../../../core/apollo/generated/graphql-schema';
+import { LinkDetails } from '../../../../collaboration/callout/links/LinkCollectionCallout';
+import { useWhiteboardWithContentQuery } from '../../../../../core/apollo/generated/apollo-hooks';
 
 export interface CreateCalloutTemplateDialogProps {
   open: boolean;
   onClose: DialogHeaderProps['onClose'];
   onSubmit: (values: CalloutTemplateFormSubmittedValues) => void;
+  callout?: {
+    id: string;
+    framing: {
+      profile: {
+        id: string;
+        url: string;
+        displayName: string;
+        description?: string;
+        references?: Reference[];
+        tagset?: Tagset;
+        storageBucket: {
+          id: string;
+        };
+      };
+      whiteboard?: WhiteboardFragmentWithCallout;
+    };
+    comments?: {
+      messages: MessageDetailsFragment[] | undefined;
+    };
+    type: CalloutType;
+    groupName: CalloutGroupName;
+    contributionPolicy: {
+      state: CalloutState;
+    };
+    contributionDefaults: {
+      postDescription?: string;
+      whiteboardContent?: string;
+    };
+    contributions?: {
+      link?: LinkDetails;
+      post?: ContributeTabPostFragment;
+      whiteboard?: WhiteboardDetailsFragment;
+    }[];
+    draft: boolean;
+    editable?: boolean;
+    movable?: boolean;
+    canSaveAsTemplate?: boolean;
+    authorization?: {
+      myPrivileges?: AuthorizationPrivilege[];
+    };
+    authorName?: string;
+    authorAvatarUri?: string;
+    publishedAt?: string;
+  };
 }
 
-const CreateCalloutTemplateDialog = ({ open, onClose, onSubmit }: CreateCalloutTemplateDialogProps) => {
+const CreateCalloutTemplateDialog = ({ open, onClose, onSubmit, callout }: CreateCalloutTemplateDialogProps) => {
   const { t } = useTranslation();
+
+  const { data: whiteboardContent } = useWhiteboardWithContentQuery({
+    variables: {
+      whiteboardId: callout?.framing.whiteboard?.id!,
+    },
+    skip: !callout?.framing.whiteboard?.id,
+  });
 
   const initialValues: Partial<CalloutTemplateFormValues> = {
     displayName: '',
@@ -22,22 +86,29 @@ const CreateCalloutTemplateDialog = ({ open, onClose, onSubmit }: CreateCalloutT
     tags: [],
     framing: {
       profile: {
-        displayName: '',
-        description: '',
+        displayName: callout?.framing.profile.displayName ?? '',
+        description: callout?.framing.profile.description ?? '',
+        referencesData:
+          callout?.framing.profile.references?.map(reference => ({
+            name: reference.name,
+            description: reference.description,
+            uri: reference.uri,
+          })) ?? [],
       },
-      tags: [],
+      tags: callout?.framing.profile.tagset?.tags ?? [],
       whiteboard: {
-        content: '',
+        content: whiteboardContent?.lookup.whiteboard?.content ?? '',
         profileData: {
-          displayName: '',
-          description: '',
+          displayName: callout?.framing.whiteboard?.profile.displayName ?? '',
+          description: callout?.framing.whiteboard?.profile.description ?? '',
         },
       },
     },
     contributionDefaults: {
-      postDescription: '',
-      whiteboardContent: '',
+      postDescription: callout?.contributionDefaults.postDescription ?? '',
+      whiteboardContent: callout?.contributionDefaults.whiteboardContent ?? '',
     },
+    type: callout?.type,
   };
 
   return (
