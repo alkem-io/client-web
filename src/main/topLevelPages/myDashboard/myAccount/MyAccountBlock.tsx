13980ca39bd38aedfe81b681f8762dbd31821f1a
Emily <emilia.pavlova@gmail.com>
2024-07-01 11:43:31 +0300

    Reuse StartingSpace component (#6509)

--- src/main/topLevelPages/myDashboard/myAccount/MyAccountBlock.tsx
+++ src/main/topLevelPages/myDashboard/myAccount/MyAccountBlock.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@mui/material';
 import { useMyAccountQuery } from '../../../../core/apollo/generated/apollo-hooks';
@@ -10,7 +9,6 @@ import { gutters } from '../../../../core/ui/grid/utils';
 import { BlockSectionTitle, Caption } from '../../../../core/ui/typography';
 import Loading from '../../../../core/ui/loading/Loading';
 import { useUserContext } from '../../../../domain/community/user';
-import { ROUTE_CREATE_SPACE } from '../../../../domain/platform/routes/constants';
 import useNewVirtualContributorWizard from '../newVirtualContributorWizard/useNewVirtualContributorWizard';
 import BadgeCardView from '../../../../core/ui/list/BadgeCardView';
 import Avatar from '../../../../core/ui/avatar/Avatar';
@@ -18,10 +16,10 @@ import defaultJourneyAvatar from '../../../../domain/journey/defaultVisuals/Avat
 import GridItem from '../../../../core/ui/grid/GridItem';
 import RouterLink from '../../../../core/ui/link/RouterLink';
 import Gutters from '../../../../core/ui/grid/Gutters';
+import StartingSpace from '../startingSpace/StartingSpace';
 
 const MyAccountBlock = () => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
   const { data, loading } = useMyAccountQuery({ fetchPolicy: 'cache-and-network' });
   const { startWizard, NewVirtualContributorWizard } = useNewVirtualContributorWizard();
 
@@ -36,10 +34,6 @@ const MyAccountBlock = () => {
     .filter(vc => vc.virtualContributors.length > 0)[0]?.virtualContributors[0];
 
   const { user } = useUserContext();
-  let createLink = t('pages.home.sections.startingSpace.url');
-  if (user && user.hasPlatformPrivilege(AuthorizationPrivilege.CreateSpace)) {
-    createLink = `/${ROUTE_CREATE_SPACE}`;
-  }
 
   return (
     <PageContentBlock columns={4}>
@@ -74,14 +68,7 @@ const MyAccountBlock = () => {
           ) : (
             <>
               <Caption>{t('pages.home.sections.myAccount.hostedSpaces')}</Caption>
-              <Button
-                onClick={() => navigate(createLink)}
-                aria-label={t('pages.home.sections.myAccount.createSpaceButton')}
-                variant="contained"
-                sx={{ textTransform: 'none', paddingTop: gutters(0.5), paddingBottom: gutters(0.5) }}
-              >
-                {t('pages.home.sections.myAccount.createSpaceButton')}
-              </Button>
+              <StartingSpace />
             </>
           )}
 
---
+++ src/main/topLevelPages/myDashboard/startingSpace/StartingSpace.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BlockTitle, Caption } from '../../../../core/ui/typography';
+import { Trans, useTranslation } from 'react-i18next';
+import DashboardBanner from '../../../../core/ui/content/DashboardBanner';
+import { useUserContext } from '../../../../domain/community/user/hooks/useUserContext';
+import { AuthorizationPrivilege } from '../../../../core/apollo/generated/graphql-schema';
+import { ROUTE_CREATE_SPACE } from '../../../../domain/platform/routes/constants';
+
+const StartingSpace = () => {
+  const { t } = useTranslation();
+  const { user } = useUserContext();
+  let createLink = t('pages.home.sections.startingSpace.url');
+
+  if (user && user.hasPlatformPrivilege(AuthorizationPrivilege.CreateSpace)) {
+    createLink = `/${ROUTE_CREATE_SPACE}`;
+  }
+
+  return (
+    <DashboardBanner to={createLink}>
+      <Trans
+        i18nKey="pages.home.sections.myAccount.createSpaceButton"
+        components={{
+          big: <BlockTitle />,
+          small: <Caption />,
+        }}
+      />
+    </DashboardBanner>
+  );
+};
+
+export default StartingSpace;
