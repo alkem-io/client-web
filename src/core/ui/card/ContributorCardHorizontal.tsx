5890b7a47b6ccd7598b7f54c6723b7e7dcfe7d2f
Emily <emilia.pavlova@gmail.com>
2024-07-23 17:42:30 +0300

    Clean up Organization cards (#6615)
    * Removed LeadUserCard, LeadContributorCard and LeadOrganizationCard
    
    * Remove OrganizationCard
    
    * Remove OrganizationCardHorizontal
    
    * Fix interface
    
    * Add sendMessage functionality
    
    * Extract component
    
    * small improvement
    
    * no inline component definition
    
    * Package fix
    
    ---------
    
    Co-authored-by: Andrew Pazniak <594548+me-andre@users.noreply.github.com>
    Co-authored-by: Andrew Pazniak <andrew.pazniak@gmail.com>
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/core/ui/card/ContributorCardHorizontal.tsx
+++ src/core/ui/card/ContributorCardHorizontal.tsx
@@ -1,26 +1,31 @@
+import React, { ReactNode, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Box } from '@mui/material';
 import Avatar from '../avatar/Avatar';
-import { BlockSectionTitle } from '../typography';
 import getLocationString, { Location } from '../location/getLocationString';
 import BadgeCardView from '../list/BadgeCardView';
-import React, { useMemo } from 'react';
 import GridItem from '../grid/GridItem';
 import SwapColors from '../palette/SwapColors';
-import { useTranslation } from 'react-i18next';
-import ContributorTooltip from './ContributorTooltip';
 import RouterLink from '../link/RouterLink';
+import { BlockSectionTitle } from '../typography';
+import ContributorTooltip from './ContributorTooltip';
 
 export interface SpaceWelcomeSectionContributorProps {
-  profile: {
-    displayName: string;
-    avatar?: {
-      uri: string;
-    };
-    location?: Location;
-    tagsets?: { tags: string[] }[];
-  };
-  url: string;
+  profile:
+    | {
+        displayName: string;
+        avatar?: {
+          uri: string;
+        };
+        location?: Location;
+        tagsets?: { tags: string[] }[];
+      }
+    | undefined;
+  url?: string;
   onContact?: () => void;
   seamless?: boolean;
+  actions?: ReactNode;
+  titleEndAmendment?: ReactNode;
 }
 
 const ContributorCardHorizontal = ({
@@ -28,12 +33,14 @@ const ContributorCardHorizontal = ({
   profile,
   onContact,
   seamless = false,
+  actions,
+  titleEndAmendment,
 }: SpaceWelcomeSectionContributorProps) => {
   const { t } = useTranslation();
 
   const tags = useMemo(
-    () => (profile.tagsets ? profile.tagsets.flatMap(tagset => tagset.tags) : []),
-    [profile.tagsets]
+    () => (profile?.tagsets ? profile.tagsets.flatMap(tagset => tagset.tags) : []),
+    [profile?.tagsets]
   );
 
   if (!seamless) {
@@ -45,34 +52,42 @@ const ContributorCardHorizontal = ({
   }
 
   return (
-    <GridItem columns={2}>
-      <ContributorTooltip
-        displayName={profile.displayName}
-        avatarSrc={profile.avatar?.uri}
-        tags={tags}
-        city={profile.location?.city}
-        country={profile.location?.country}
-        isContactable={!!onContact}
-        onContact={onContact}
-      >
-        <BadgeCardView
-          visual={
-            <Avatar
-              src={profile.avatar?.uri}
-              aria-label="User avatar"
-              alt={t('common.avatar-of', { user: profile.displayName })}
-            >
-              {profile.displayName[0]}
-            </Avatar>
-          }
-          component={RouterLink}
-          to={url}
+    <>
+      <GridItem columns={2}>
+        <ContributorTooltip
+          displayName={profile?.displayName}
+          avatarSrc={profile?.avatar?.uri}
+          tags={tags}
+          city={profile?.location?.city}
+          country={profile?.location?.country}
+          isContactable={!!onContact}
+          onContact={onContact}
         >
-          <BlockSectionTitle>{profile.displayName}</BlockSectionTitle>
-          <BlockSectionTitle>{profile.location && getLocationString(profile.location)}</BlockSectionTitle>
-        </BadgeCardView>
-      </ContributorTooltip>
-    </GridItem>
+          <BadgeCardView
+            visual={
+              <Avatar
+                src={profile?.avatar?.uri}
+                aria-label="User avatar"
+                alt={t('common.avatar-of', { user: profile?.displayName })}
+              >
+                {profile?.displayName?.[0]}
+              </Avatar>
+            }
+            component={RouterLink}
+            to={url ?? ''}
+          >
+            <Box display="flex" flexDirection="row" justifyContent="space-between">
+              <Box display="flex" flexDirection="column">
+                <BlockSectionTitle>{profile?.displayName}</BlockSectionTitle>
+                <BlockSectionTitle>{profile?.location && getLocationString(profile.location)}</BlockSectionTitle>
+              </Box>
+              {titleEndAmendment}
+            </Box>
+          </BadgeCardView>
+        </ContributorTooltip>
+      </GridItem>
+      {actions}
+    </>
   );
 };
 
---
+++ src/core/ui/content/LabeledCount.tsx
@@ -0,0 +1,37 @@
+import { Box, Skeleton } from '@mui/material';
+import OrganizationVerifiedStatus from '../../../domain/community/organization/organizationVerifiedStatus/OrganizationVerifiedStatus';
+import CircleTag from '../tags/CircleTag';
+import { Caption } from '../typography';
+
+interface LabeledCountProps {
+  label: string;
+  count: number;
+  loading?: boolean;
+  verified?: boolean;
+}
+
+const LabeledCount = ({ label, count, loading, verified }: LabeledCountProps) => {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="flex-end">
+      <Box display="flex">
+        <Caption sx={{ marginRight: 1, flexGrow: 1 }}>{loading ? <Skeleton /> : label}</Caption>
+        {loading ? (
+          <Skeleton variant="circular">
+            <CircleTag text={`${count}`} color="primary" size="small" />
+          </Skeleton>
+        ) : (
+          <CircleTag text={`${count}`} color="primary" size="small" />
+        )}
+      </Box>
+      {loading ? (
+        <Skeleton>
+          <OrganizationVerifiedStatus verified={Boolean(verified)} />
+        </Skeleton>
+      ) : (
+        <OrganizationVerifiedStatus verified={Boolean(verified)} />
+      )}
+    </Box>
+  );
+};
+
+export default LabeledCount;
