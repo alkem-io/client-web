71a89b75487acde809e275f0f8da44d64db6c5b3
Bobby Kolev <bobbykolev@abv.bg>
2024-09-09 09:09:29 +0300

    Try your VC dialog now awaiting Ready state (#6850)

--- src/main/topLevelPages/myDashboard/newVirtualContributorWizard/useNewVirtualContributorWizard.tsx
+++ src/main/topLevelPages/myDashboard/newVirtualContributorWizard/useNewVirtualContributorWizard.tsx
@@ -406,7 +406,7 @@ const useNewVirtualContributorWizard = (): useNewVirtualContributorWizardProvide
         bokCommunityId,
         boKParentCommunityId
       );
-      addVCCreationCache(virtualContributorInput.name);
+
       const { data } = await getNewSpaceUrl();
       navigate(data?.space.profile.url ?? '');
     }
@@ -469,6 +469,11 @@ const useNewVirtualContributorWizard = (): useNewVirtualContributorWizardProvide
         },
       });
 
+      // add vc's nameId to the cache for the TryVC dialog
+      if (data?.createVirtualContributor.nameID) {
+        addVCCreationCache(data?.createVirtualContributor.nameID);
+      }
+
       notify(
         t('createVirtualContributorWizard.createdVirtualContributor.successMessage', { name: values.name }),
         'success'
@@ -485,7 +490,7 @@ const useNewVirtualContributorWizard = (): useNewVirtualContributorWizardProvide
         selectedKnowledge.communityId,
         selectedKnowledge.parentCommunityId
       );
-      addVCCreationCache(virtualContributorInput.name);
+
       navigate(selectedKnowledge.url ?? '');
     }
   };
