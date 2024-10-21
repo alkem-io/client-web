ca9b087ede2a914a6574511ac92bb92e842b8412
Bobby Kolev <bobbykolev@abv.bg>
2024-10-21 13:13:14 +0300

    fix memberships pages wehere the spaceLevel was still expected as number (#7071)

---
+++ src/domain/shared/utils/spaceLevel.ts
@@ -0,0 +1,14 @@
+import { SpaceLevel } from '../../../core/apollo/generated/graphql-schema';
+import { JourneyTypeName } from '../../journey/JourneyTypeName';
+
+export const getChildJourneyTypeName = ({ level }: { level: SpaceLevel }): JourneyTypeName => {
+  switch (level) {
+    case SpaceLevel.Challenge:
+      return 'subspace' as JourneyTypeName;
+    case SpaceLevel.Opportunity:
+      return 'subsubspace' as JourneyTypeName;
+    case SpaceLevel.Space:
+    default:
+      return 'space' as JourneyTypeName;
+  }
+};
