21b650bdb97b65f4d732ff0b0906c00d363788d0
bobbykolev <bobbykolev@abv.bg>
2024-10-29 11:25:27 +0200

    TipsAndTricks dialog

--- src/main/topLevelPages/myDashboard/DashboardDialogs/DashboardDialogs.tsx
+++ src/main/topLevelPages/myDashboard/DashboardDialogs/DashboardDialogs.tsx
@@ -3,15 +3,22 @@ import DialogHeader from '../../../../core/ui/dialog/DialogHeader';
 import DialogWithGrid from '../../../../core/ui/dialog/DialogWithGrid';
 import { useDashboardContext } from '../DashboardContext';
 import { DashboardDialog } from './DashboardDialogsProps';
+import { TipsAndTricks } from '../tipsAndTricks/TipsAndTricks';
+import { useTranslation } from 'react-i18next';
 
 export const DashboardDialogs = () => {
+  const { t } = useTranslation();
   const { openedDialog, setOpenedDialog } = useDashboardContext();
 
   return (
     <>
       <DialogWithGrid open={openedDialog === DashboardDialog.TipsAndTricks}>
-        <DialogHeader onClose={() => setOpenedDialog(undefined)}>Tips and Tricks</DialogHeader>
-        <DialogContent>Dialog content</DialogContent>
+        <DialogHeader onClose={() => setOpenedDialog(undefined)}>
+          {t('pages.home.sections.tipsAndTricks.title')}
+        </DialogHeader>
+        <DialogContent>
+          <TipsAndTricks />
+        </DialogContent>
       </DialogWithGrid>
     </>
   );
