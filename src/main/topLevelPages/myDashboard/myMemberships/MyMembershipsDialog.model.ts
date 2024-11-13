7cb4ed42ff26d3a74f01d3b643157acd300bbbd2
reactoholic <petar.georgiev.kolev@gmail.com>
2024-11-13 15:54:54 +0200

    resolve pr comment
    Signed-off-by: reactoholic <petar.georgiev.kolev@gmail.com>

--- src/main/topLevelPages/myDashboard/myMemberships/MyMembershipsDialog.model.ts
+++ src/main/topLevelPages/myDashboard/myMemberships/MyMembershipsDialog.model.ts
@@ -31,7 +31,6 @@ export type MembershipProps = {
         myMembershipStatus?: CommunityMembershipStatus;
       };
     };
-    childMemberships?: MembershipProps[];
   };
   childMemberships?: MembershipProps[];
 };
