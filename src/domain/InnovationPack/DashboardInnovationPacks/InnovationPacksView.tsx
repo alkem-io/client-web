3ae9be6245b393472b9ce2b10cd7e18830959df6
reactoholic <petar.georgiev.kolev@gmail.com>
2024-11-14 11:53:46 +0200

    add skeleton loading indicators in the innovation library page
    Signed-off-by: reactoholic <petar.georgiev.kolev@gmail.com>

--- src/domain/InnovationPack/DashboardInnovationPacks/InnovationPacksView.tsx
+++ src/domain/InnovationPack/DashboardInnovationPacks/InnovationPacksView.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, ReactNode } from 'react';
+import { Dispatch, ReactNode } from 'react';
 import PageContentBlockHeaderWithDialogAction from '../../../core/ui/content/PageContentBlockHeaderWithDialogAction';
 import MultipleSelect from '../../../core/ui/search/MultipleSelect';
 import InnovationPackCard, { InnovationPackCardProps } from '../InnovationPackCard/InnovationPackCard';
@@ -10,8 +10,9 @@ import ScrollableCardsLayoutContainer from '../../../core/ui/card/cardsLayout/Sc
 import { Box, Button, Theme, useMediaQuery } from '@mui/material';
 import { CONTRIBUTE_CARD_COLUMNS } from '../../../core/ui/card/ContributeCard';
 import GridItem from '../../../core/ui/grid/GridItem';
+import { Skeleton } from '@mui/material';
 
-interface InnovationPacksViewProps {
+interface InnovationPacksViewProps extends PageContentBlockProps {
   filter: string[];
   headerTitle: ReactNode;
   innovationPacks: (Identifiable & InnovationPackCardProps)[] | undefined;
@@ -20,6 +21,7 @@ interface InnovationPacksViewProps {
   onDialogOpen?: () => void;
   onDialogClose?: () => void;
   hasMore?: boolean;
+  loading?: boolean;
 }
 
 const InnovationPacksView = ({
@@ -31,8 +33,9 @@ const InnovationPacksView = ({
   onDialogOpen,
   onDialogClose,
   hasMore = false,
+  loading,
   ...props
-}: InnovationPacksViewProps & PageContentBlockProps) => {
+}: InnovationPacksViewProps) => {
   const { t } = useTranslation();
   const isMobile = useMediaQuery<Theme>(theme => theme.breakpoints.down('sm'));
 
@@ -45,29 +48,39 @@ const InnovationPacksView = ({
         expanded={expanded}
         actions={<MultipleSelect onChange={onFilterChange} value={filter} minLength={2} size="xsmall" inlineTerms />}
       />
-      {innovationPacks && (
-        <ScrollableCardsLayoutContainer minHeight={0} orientation={expanded ? 'vertical' : undefined} sameHeight>
-          {innovationPacks.map(({ id, ...cardProps }) => (
-            <InnovationPackCard key={id} {...cardProps} />
-          ))}
-          {isMobile && hasMore && (
-            <GridItem columns={CONTRIBUTE_CARD_COLUMNS}>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirections: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Button variant="contained" onClick={onDialogOpen}>
-                  {t('common.show-all')}
-                </Button>
-              </Box>
-            </GridItem>
-          )}
-        </ScrollableCardsLayoutContainer>
-      )}
+
+      <ScrollableCardsLayoutContainer minHeight={0} orientation={expanded ? 'vertical' : undefined} sameHeight>
+        {loading
+          ? Array.from({ length: 5 }).map((_, idx) => (
+              <Skeleton
+                key={idx}
+                width={240}
+                height={300}
+                animation="pulse"
+                variant="rectangular"
+                sx={{ borderRadius: 1 }}
+              />
+            ))
+          : innovationPacks?.map(({ id, ...cardProps }) => <InnovationPackCard key={id} {...cardProps} />)}
+
+        {isMobile && hasMore && (
+          <GridItem columns={CONTRIBUTE_CARD_COLUMNS}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirections: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Button variant="contained" onClick={onDialogOpen}>
+                {t('common.show-all')}
+              </Button>
+            </Box>
+          </GridItem>
+        )}
+      </ScrollableCardsLayoutContainer>
+
       {!isMobile && hasMore && <SeeMore subject={t('common.innovation-packs')} onClick={onDialogOpen} />}
     </PageContentBlock>
   );
