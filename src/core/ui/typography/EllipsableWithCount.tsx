4058fd578c486fc3c2876b1f3c7b757b7dae4d50
reactoholic <petar.georgiev.kolev@gmail.com>
2024-11-18 09:54:45 +0200

    hide all comments and contributors zero counts
    Signed-off-by: reactoholic <petar.georgiev.kolev@gmail.com>

--- src/core/ui/typography/EllipsableWithCount.tsx
+++ src/core/ui/typography/EllipsableWithCount.tsx
@@ -1,13 +1,16 @@
-import { PropsWithChildren, ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
+
+import { isNumber } from 'lodash';
 import { Box, Typography } from '@mui/material';
 
-const EllipsableWithCount = ({ count, children }: PropsWithChildren<{ count: ReactNode }>) => (
+const EllipsableWithCount = ({ count, children }: PropsWithChildren<{ count: number }>) => (
   <Box component="span" display="flex" gap={0.5} flexGrow={1} minWidth={0}>
     <Typography component="span" variant="inherit" flexShrink={1} noWrap>
       {children}
     </Typography>
+
     <Box component="span" flexShrink={0}>
-      ({count})
+      {isNumber(count) && count > 0 && `(${count})`}
     </Box>
   </Box>
 );
