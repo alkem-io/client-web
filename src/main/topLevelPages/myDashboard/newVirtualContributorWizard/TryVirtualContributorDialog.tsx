d251fe9256a238237a78f2842edbea5069bfb994
Carlos Cano <carlos@alkem.io>
2024-11-18 09:14:53 +0100

    Fix build

--- src/main/topLevelPages/myDashboard/newVirtualContributorWizard/TryVirtualContributorDialog.tsx
+++ src/main/topLevelPages/myDashboard/newVirtualContributorWizard/TryVirtualContributorDialog.tsx
@@ -33,7 +33,7 @@ import { useSubscribeOnVirtualContributorEvents } from '../../../../domain/commu
 
 interface TryVirtualContributorDialogProps {
   spaceId: string;
-  collaborationId: string;
+  collaborationId: string | undefined;
   vcNameId: string;
   open: boolean;
   onClose: () => void;
