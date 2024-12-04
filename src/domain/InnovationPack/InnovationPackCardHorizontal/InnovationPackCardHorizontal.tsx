727d8500ca97666380ffa1a0d3e3bd46b4afdb37
Bobby Kolev <bobbykolev@abv.bg>
2024-12-04 11:45:29 +0200

    [Subspace Templates] Update the innovation pack card template icons + clean up (#7277)
    * update the innocation pack card template icons; cleanup
    
    * small tweak
    
    ---------
    
    Co-authored-by: Carlos Cano <carlos@alkem.io>

--- src/domain/InnovationPack/InnovationPackCardHorizontal/InnovationPackCardHorizontal.tsx
+++ src/domain/InnovationPack/InnovationPackCardHorizontal/InnovationPackCardHorizontal.tsx
@@ -1,20 +1,12 @@
-import DesignServicesIcon from '@mui/icons-material/DesignServices';
 import { Box, Skeleton, useTheme } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import CardFooterCountWithBadge from '@/core/ui/card/CardFooterCountWithBadge';
 import { gutters } from '@/core/ui/grid/utils';
 import RouterLink from '@/core/ui/link/RouterLink';
 import BadgeCardView from '@/core/ui/list/BadgeCardView';
-import { Caption, BlockTitle } from '@/core/ui/typography';
-import { CommunityGuidelinesIcon } from '@/domain/community/communityGuidelines/icon/CommunityGuidelinesIcon';
-import { PostIcon } from '@/domain/collaboration/post/icon/PostIcon';
-import { WhiteboardIcon } from '@/domain/collaboration/whiteboard/icon/WhiteboardIcon';
+import { BlockTitle } from '@/core/ui/typography';
 import InnovationPackIcon from '../InnovationPackIcon';
 import RoundedIcon from '@/core/ui/icon/RoundedIcon';
 import ActionsMenu from '@/core/ui/card/ActionsMenu';
 import OneLineMarkdown from '@/core/ui/markdown/OneLineMarkdown';
-import { TemplateType } from '@/core/apollo/generated/graphql-schema';
-import { SpaceIcon } from '@/domain/journey/space/icon/SpaceIcon';
 import { RoundedBadgeSize } from '@/core/ui/icon/RoundedBadge';
 
 export interface InnovationPackCardHorizontalProps {
@@ -46,96 +38,31 @@ export const InnovationPackCardHorizontalSkeleton = () => {
 
 const InnovationPackCardHorizontal = ({
   profile: { displayName, description, url },
-  templates,
   actions = undefined,
   size = 'medium',
-}: InnovationPackCardHorizontalProps) => {
-  const { t } = useTranslation();
-
-  const {
-    calloutTemplatesCount,
-    collaborationTemplatesCount,
-    communityGuidelinesTemplatesCount,
-    postTemplatesCount,
-    whiteboardTemplatesCount,
-  } = templates ?? {};
-
-  const totalTemplatesCount =
-    (calloutTemplatesCount ?? 0) +
-    (collaborationTemplatesCount ?? 0) +
-    (communityGuidelinesTemplatesCount ?? 0) +
-    (postTemplatesCount ?? 0) +
-    (whiteboardTemplatesCount ?? 0);
-
-  return (
-    <BadgeCardView
-      visual={
-        <RoundedIcon
-          size={size}
-          component={InnovationPackIcon}
-          sx={{
-            color: theme => theme.palette.neutral.light,
-            background: theme => theme.palette.background.paper,
-          }}
-        />
-      }
-      component={RouterLink}
-      to={url ?? ''}
-      actions={actions && <ActionsMenu>{actions}</ActionsMenu>}
-    >
-      <Box display="flex" flexDirection="row" justifyContent="space-between">
-        <Box display="flex" flexDirection="column">
-          <BlockTitle>{displayName}</BlockTitle>
-          <OneLineMarkdown>{description ?? ''}</OneLineMarkdown>
-        </Box>
+}: InnovationPackCardHorizontalProps) => (
+  <BadgeCardView
+    visual={
+      <RoundedIcon
+        size={size}
+        component={InnovationPackIcon}
+        sx={{
+          color: theme => theme.palette.neutral.light,
+          background: theme => theme.palette.background.paper,
+        }}
+      />
+    }
+    component={RouterLink}
+    to={url ?? ''}
+    actions={actions && <ActionsMenu>{actions}</ActionsMenu>}
+  >
+    <Box display="flex" flexDirection="row" justifyContent="space-between">
+      <Box display="flex" flexDirection="column">
+        <BlockTitle>{displayName}</BlockTitle>
+        <OneLineMarkdown>{description ?? ''}</OneLineMarkdown>
       </Box>
-      {totalTemplatesCount > 0 && (
-        <Box display="flex" gap={gutters(0.5)} height={gutters(2)} alignItems="center" justifyContent="start">
-          {!!calloutTemplatesCount && (
-            <CardFooterCountWithBadge
-              iconComponent={DesignServicesIcon}
-              tooltip={t(`common.enums.templateType.${TemplateType.Callout}_plural`)}
-            >
-              <Caption>{calloutTemplatesCount}</Caption>
-            </CardFooterCountWithBadge>
-          )}
-          {!!whiteboardTemplatesCount && (
-            <CardFooterCountWithBadge
-              iconComponent={WhiteboardIcon}
-              tooltip={t(`common.enums.templateType.${TemplateType.Whiteboard}_plural`)}
-            >
-              <Caption>{whiteboardTemplatesCount}</Caption>
-            </CardFooterCountWithBadge>
-          )}
-          {!!communityGuidelinesTemplatesCount && (
-            <CardFooterCountWithBadge
-              iconComponent={CommunityGuidelinesIcon}
-              tooltip={t(`common.enums.templateType.${TemplateType.CommunityGuidelines}_plural`)}
-            >
-              <Caption>{communityGuidelinesTemplatesCount}</Caption>
-            </CardFooterCountWithBadge>
-          )}
-          {!!postTemplatesCount && (
-            <CardFooterCountWithBadge
-              iconComponent={PostIcon}
-              tooltip={t(`common.enums.templateType.${TemplateType.Post}_plural`)}
-            >
-              <Caption>{postTemplatesCount}</Caption>
-            </CardFooterCountWithBadge>
-          )}
-          {!!collaborationTemplatesCount && (
-            <CardFooterCountWithBadge
-              tooltip={t(`common.enums.templateType.${TemplateType.Collaboration}_plural`)}
-              iconComponent={SpaceIcon}
-            >
-              <Caption>{collaborationTemplatesCount}</Caption>
-            </CardFooterCountWithBadge>
-          )}
-          {/* {totalTemplatesCount === 0 && <Caption>{t('pages.admin.generic.sections.account.noTemplates')}</Caption>} */}
-        </Box>
-      )}
-    </BadgeCardView>
-  );
-};
+    </Box>
+  </BadgeCardView>
+);
 
 export default InnovationPackCardHorizontal;
