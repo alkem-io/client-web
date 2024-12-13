ec69d5ce3ef40cfe0f1b3a24ffbf7d168e868a4d
Carlos Cano <carlos@alkem.io>
2024-12-13 11:16:02 +0100

    Fix tutorials iframes

--- src/core/ui/markdown/embed/remarkVerifyIframe.ts
+++ src/core/ui/markdown/embed/remarkVerifyIframe.ts
@@ -23,27 +23,48 @@ const isAllowedUrl = (url: string) => {
 
 export const remarkVerifyIframe: Pluggable = () => {
   return tree => {
-    visit(tree, 'html', (node: Node) => {
-      // @ts-ignore
-      if (typeof node.value === 'string') {
-        // @ts-ignore
+    visit(tree, 'html', (node: { value: string }) => {
+      if (node && typeof node.value === 'string') {
         const nodeValue: string = node.value;
         if (nodeValue.toLowerCase().includes('<iframe')) {
-          const srcMatch = nodeValue.match(/src="([^"]*)"/i);
-          if (!srcMatch) {
-            // Iframe without src at all? just remove it
-            // @ts-ignore
-            node.value = '';
-          } else if (!isAllowedUrl(srcMatch[1])) {
-            // @ts-ignore
-            node.value = node.value.replace(/src="[^"]*"/i, 'src="about:blank"');
-          }
-        }
-        const sandboxMatch = nodeValue.match(/sandbox="([^"]*)"/i);
-        if (sandboxMatch) {
-          // We are not using sandbox attribute for now, so this may be comming from someone trying to tamper with our cookies. Remove the entire thing for now:
-          // @ts-ignore
-          node.value = '';
+          const filters = [
+            {
+              regex: /src="([^"]*)"/i,
+              action: match => {
+                if (!nodeValue.includes('src=')) {
+                  node.value = ''; // Iframe without src at all? just remove it
+                  return;
+                }
+                if (match && !isAllowedUrl(match[1])) {
+                  node.value = nodeValue.replace(/src="[^"]*"/i, 'src="about:blank"');
+                }
+              },
+            },
+            {
+              // Same as above, but for single quotes
+              regex: /src='([^']*)'/i,
+              action: match => {
+                if (!nodeValue.includes('src=')) {
+                  node.value = '';
+                  return;
+                }
+                if (match && !isAllowedUrl(match[1])) {
+                  node.value = nodeValue.replace(/src='[^']*'/i, "src='about:blank'");
+                }
+              },
+            },
+            {
+              // We are not using sandbox attribute for now,
+              // so this may be comming from someone trying to tamper with our cookies. Remove the entire thing for now:
+              regex: /sandbox/i,
+              action: match => {
+                if (match) {
+                  node.value = '';
+                }
+              },
+            },
+          ];
+          filters.forEach(filter => filter.action(nodeValue.match(filter.regex)));
         }
       }
     });
