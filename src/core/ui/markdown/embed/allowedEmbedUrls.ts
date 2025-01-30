73dff41f25489ec6d7331bcf5bc73b85287346eb
Neil Smyth <30729240+techsmyth@users.noreply.github.com>
2025-01-30 14:14:12 +0100

    Retrieve the iframeAllowedUrls from the server (#7531)

--- src/core/ui/markdown/embed/allowedEmbedUrls.ts
+++
@@ -1,7 +0,0 @@
-export const ALLOWED_EMBED_URLS = [
-  'https://issuu.com',
-  'https://www.youtube.com',
-  'https://player.vimeo.com',
-  'https://www.canva.com',
-  'https://demo.arcade.software',
-] as const;
