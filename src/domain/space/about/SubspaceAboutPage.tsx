5d0708bd8944034dab9d7eede95337a7b0dba63c
Bobby Kolev <bobbykolev@abv.bg>
2025-03-07 18:09:42 +0200

    Fix the join behaviour on L2 level About join (#7802)

--- src/domain/space/about/SubspaceAboutPage.tsx
+++ src/domain/space/about/SubspaceAboutPage.tsx
@@ -4,20 +4,19 @@ import { useBackToStaticPath } from '@/core/routing/useBackToPath';
 import SpaceAboutDialog from '@/domain/space/about/SpaceAboutDialog';
 import ContributorsDialog from '@/domain/community/community/ContributorsDialog/ContributorsDialog';
 import SubspaceContributorsDialogContent from '@/domain/community/community/entities/SubspaceContributorsDialogContent';
-import useUrlResolver from '@/main/routing/urlResolver/useUrlResolver';
 import { SpaceDashboardSpaceDetails } from '../layout/TabbedSpaceL0/Tabs/SpaceDashboard/SpaceDashboardView';
 import { AuthorizationPrivilege } from '@/core/apollo/generated/graphql-schema';
 
 const SubspaceAboutPage = () => {
-  const { spaceId } = useUrlResolver();
   const { subspace, loading } = useSubSpace();
   const { about } = subspace;
+
   const backToParentPage = useBackToStaticPath(about.profile.url);
 
   const [isContributorsDialogOpen, setIsContributorsDialogOpen] = useState(false);
 
   const space: SpaceDashboardSpaceDetails = {
-    id: spaceId,
+    id: subspace.id,
     about: about,
     level: subspace.level,
   };
