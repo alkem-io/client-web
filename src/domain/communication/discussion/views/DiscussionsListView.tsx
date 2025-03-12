145ee3e29d5f212636d06a82e606c471bc49ab42
Neil Smyth <neil@thesmyths.eu>
2025-03-12 20:13:37 +0100

    removed usage of ViewProps

--- src/domain/communication/discussion/views/DiscussionsListView.tsx
+++ src/domain/communication/discussion/views/DiscussionsListView.tsx
@@ -3,36 +3,20 @@ import { Skeleton } from '@mui/material';
 import { Filter } from '@/domain/platform/admin/components/Common/Filter';
 import DiscussionOverview from './DiscussionOverview';
 import { Discussion } from '../models/Discussion';
-import { ViewProps } from '@/core/container/view';
 
-interface DiscussionListViewEntities {
+interface DiscussionListViewProps {
+  filterEnabled: boolean;
+  onClickDiscussion: (discussion: Discussion) => void;
+  loading: boolean;
   discussions: Discussion[];
 }
 
-interface DiscussionListViewState {
-  loading: boolean;
-}
-
-interface DiscussionListViewActions {
-  onClickDiscussion: (discussion: Discussion) => void;
-}
-
-interface DiscussionListViewOptions {
-  filterEnabled: boolean;
-}
-
-interface DiscussionListViewProps
-  extends ViewProps<
-    DiscussionListViewEntities,
-    DiscussionListViewActions,
-    DiscussionListViewState,
-    DiscussionListViewOptions
-  > {}
-
-export const DiscussionListView = ({ entities, state, actions, options }: DiscussionListViewProps) => {
-  const { discussions } = entities;
-  const { loading } = state;
-
+export const DiscussionListView = ({
+  filterEnabled,
+  onClickDiscussion,
+  loading,
+  discussions,
+}: DiscussionListViewProps) => {
   return (
     <>
       {loading && (
@@ -48,7 +32,7 @@ export const DiscussionListView = ({ entities, state, actions, options }: Discus
           />
         </List>
       )}
-      {!loading && options.filterEnabled && (
+      {!loading && filterEnabled && (
         <Filter
           data={discussions}
           limitKeys={['title']}
@@ -60,16 +44,16 @@ export const DiscussionListView = ({ entities, state, actions, options }: Discus
           {filteredData => (
             <List>
               {filteredData.map((item, index) => (
-                <DiscussionOverview key={index} discussion={item} onClick={actions.onClickDiscussion} />
+                <DiscussionOverview key={index} discussion={item} onClick={onClickDiscussion} />
               ))}
             </List>
           )}
         </Filter>
       )}
-      {!loading && !options.filterEnabled && (
+      {!loading && !filterEnabled && (
         <List>
           {discussions.map((item, index) => (
-            <DiscussionOverview key={index} discussion={item} onClick={actions.onClickDiscussion} />
+            <DiscussionOverview key={index} discussion={item} onClick={onClickDiscussion} />
           ))}
         </List>
       )}
