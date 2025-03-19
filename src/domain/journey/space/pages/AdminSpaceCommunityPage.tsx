d5a323576018b3e45da12e21f839c6b8a296da40
Carlos Cano <carlos@alkem.io>
2025-03-19 13:47:42 +0100

    Fix wrong Ids

--- src/domain/journey/space/pages/AdminSpaceCommunityPage.tsx
+++ src/domain/journey/space/pages/AdminSpaceCommunityPage.tsx
@@ -40,7 +40,6 @@ const AdminSpaceCommunityPage = ({ routePrefix = '../' }: SettingsPageProps) => 
   const spaceId = space.id;
   const { about } = space;
   const { membership } = about;
-  const communityId = membership?.communityID!;
   const roleSetId = membership?.roleSetID!;
 
   const {
@@ -53,6 +52,7 @@ const AdminSpaceCommunityPage = ({ routePrefix = '../' }: SettingsPageProps) => 
     memberRoleDefinition,
     leadRoleDefinition,
     permissions,
+    communityGuidelinesId,
     spaceEntitlements,
     onApplicationStateChange,
     onInvitationStateChange,
@@ -156,7 +156,7 @@ const AdminSpaceCommunityPage = ({ routePrefix = '../' }: SettingsPageProps) => 
           <CommunityApplicationForm roleSetId={roleSetId} />
         </PageContentBlockCollapsible>
 
-        <CommunityGuidelinesContainer communityGuidelinesId={communityId}>
+        <CommunityGuidelinesContainer communityGuidelinesId={communityGuidelinesId}>
           {({
             communityGuidelines,
             profileId,
