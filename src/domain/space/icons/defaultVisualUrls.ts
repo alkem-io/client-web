97add3ccb7672cd29142549dfbb04e6e8e471e81
Neil Smyth <neil@thesmyths.eu>
2025-03-28 15:56:16 +0100

    move default visuals out of src tree

--- src/domain/space/icons/defaultVisualUrls.ts
+++ src/domain/space/icons/defaultVisualUrls.ts
@@ -1,12 +1,9 @@
 import { VisualType } from '@/core/apollo/generated/graphql-schema';
-import defaultJourneyAvatar from './defaultVisuals/Avatar.jpg';
-import defaultJourneyBanner from './defaultVisuals/Banner.jpg';
-import defaultJourneyCard from './defaultVisuals/Card.jpg';
 
 export const defaultVisualUrls = {
-  [VisualType.Avatar]: defaultJourneyAvatar,
-  [VisualType.Banner]: defaultJourneyBanner,
-  [VisualType.Card]: defaultJourneyCard,
+  [VisualType.Avatar]: '/default-visuals/space/avatar.jpg',
+  [VisualType.Banner]: '/default-visuals/space/banner.jpg',
+  [VisualType.Card]: '/default-visuals/space/card.jpg',
   // It's never shown as an uploadable, only replaced when saving a whiteboard, so it doesn't really need a default, but it is useful for typescript validation to define it
-  [VisualType.BannerWide]: defaultJourneyBanner,
+  [VisualType.BannerWide]: '/default-visuals/space/banner.jpg',
 } as const;
