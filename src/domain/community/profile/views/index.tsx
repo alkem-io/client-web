57c508c95c3120033c6b63f49c5894168dad1301
Neil Smyth <neil@thesmyths.eu>
2025-04-05 14:20:19 +0200

    fixed import

--- src/domain/community/profile/views/index.tsx
+++ src/domain/community/profile/views/index.tsx
@@ -1,4 +1,4 @@
 export * from './AssociatesView';
-export * from '../../../../_deprecated/ContributionsView';
+export * from '../../../../_deprecated/ContributionsView2';
 export * from './OrganizationProfileView';
 export * from './CredentialsView';
