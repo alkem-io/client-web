d12793f5f05573635a25d1f17a0b0846e8b4b999
bobbykolev <bobbykolev@abv.bg>
2025-04-10 16:56:55 +0300

    remove redundant RouterLink component

--- src/main/ui/platformFooter/FooterLogo.tsx
+++ src/main/ui/platformFooter/FooterLogo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Box, BoxProps } from '@mui/material';
-import { RouterLink } from '@/_deprecated/routing/RouterLink';
 import ImageFadeIn from '@/core/ui/image/ImageFadeIn';
 import { gutters } from '@/core/ui/grid/utils';
 
 const FooterLogo = (props: BoxProps) => {
   return (
-    <Box component={RouterLink} to="/about" {...props}>
+    <Box component={Link} to="/about" {...props}>
       <ImageFadeIn src="/logo.png" alt="Alkemio" height={gutters()} display="block" />
     </Box>
   );
