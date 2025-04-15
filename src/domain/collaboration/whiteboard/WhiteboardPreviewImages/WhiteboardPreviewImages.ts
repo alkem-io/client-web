0b28d24a7f53e20664d5aa5572641deff470d1dd
Svetoslav Petkov <svetoslav@alkem.io>
2025-04-15 17:34:10 +0300

    Upgrade to Excalidraw 0.18.0 (#7986)
    * upgraded to latest excalidraw & fixed imports
    * removed usage of the deprecated getSceneVersion
    * Fix Mermaid dialog position and remove Library button
    
    ---------
    
    Co-authored-by: Carlos Cano <carlos@alkem.io>

--- src/domain/collaboration/whiteboard/WhiteboardPreviewImages/WhiteboardPreviewImages.ts
+++ src/domain/collaboration/whiteboard/WhiteboardPreviewImages/WhiteboardPreviewImages.ts
@@ -1,4 +1,4 @@
-import type { ExportedDataState } from '@alkemio/excalidraw/dist/excalidraw/data/types';
+import type { ExportedDataState } from '@alkemio/excalidraw/dist/types/excalidraw/data/types';
 import getWhiteboardPreviewDimensions from './getWhiteboardPreviewDimensions';
 import { VisualType } from '@/core/apollo/generated/graphql-schema';
 import { useUploadVisualMutation } from '@/core/apollo/generated/apollo-hooks';
