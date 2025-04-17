fbf2b430946238be2f5f2ca2d13c759ddaad4332
bobbykolev <bobbykolev@abv.bg>
2025-04-17 18:01:14 +0300

    Invitations url for presenting the pending invitations dialog.

--- src/main/topLevelPages/myDashboard/MyDashboard.tsx
+++ src/main/topLevelPages/myDashboard/MyDashboard.tsx
@@ -1,28 +1,44 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { useLatestContributionsSpacesFlatQuery } from '@/core/apollo/generated/apollo-hooks';
 import Loading from '@/core/ui/loading/Loading';
 import { DashboardProvider } from './DashboardContext';
 import { lazyWithGlobalErrorHandler } from '@/core/lazyLoading/lazyWithGlobalErrorHandler';
 import { useUserContext } from '@/domain/community/user';
+import { useLocation, Navigate } from 'react-router-dom';
+import { DIALOG_PARAM_VALUES, useMyDashboardDialogs } from '@/main/topLevelPages/myDashboard/useMyDashboardDialogs';
+import { buildSignUpUrl } from '@/main/routing/urlBuilders';
 
 const MyDashboardUnauthenticated = lazyWithGlobalErrorHandler(() => import('./MyDashboardUnauthenticated'));
 const MyDashboardWithMemberships = lazyWithGlobalErrorHandler(() => import('./MyDashboardWithMemberships'));
 const MyDashboardWithoutMemberships = lazyWithGlobalErrorHandler(() => import('./MyDashboardWithoutMemberships'));
 
 export const MyDashboard = () => {
+  const { pathname, search } = useLocation();
   const { user, isAuthenticated, loading: isLoadingAuthentication } = useUserContext();
   const hasNotAuthorized = !isAuthenticated || !user;
+  const [navigateToMemberships, setNavigateToMemberships] = useState(false);
 
   const { data: spacesData, loading: areSpacesLoading } = useLatestContributionsSpacesFlatQuery({
     skip: hasNotAuthorized,
   });
   const hasSpaceMemberships = !!spacesData?.me.spaceMembershipsFlat.length;
 
+  useMyDashboardDialogs({
+    paramValue: DIALOG_PARAM_VALUES.INVITATIONS,
+    onDialogOpen: () => setNavigateToMemberships(true),
+  });
+
   if (areSpacesLoading || (isLoadingAuthentication && hasNotAuthorized)) {
     return <Loading />;
   }
 
   if (!isLoadingAuthentication && hasNotAuthorized) {
+    // if navigateToMemberships and user is not authenticated, we need to redirect to the sign up page
+    // with the returnUrl as the current location
+    if (navigateToMemberships) {
+      return <Navigate to={buildSignUpUrl(pathname, search)} />;
+    }
+
     return (
       <Suspense fallback={<Loading />}>
         <MyDashboardUnauthenticated />
