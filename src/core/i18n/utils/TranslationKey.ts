89b230062519dd7236492a9527419856cdca7c3a
Carlos Cano <carlos@alkem.io>
2025-05-05 13:12:43 +0200

    done TranslationKey properly

--- src/core/i18n/utils/TranslationKey.ts
+++ src/core/i18n/utils/TranslationKey.ts
@@ -1,3 +1,11 @@
-type TranslationKey = string;
+import { resources } from '../config';
+
+// Utility type to flatten nested objects into dot-separated keys. Converts the translation JSON structure into a flat key typescript type.
+type Flatten<T, Prefix extends string = ''> = {
+  [K in keyof T & string]: T[K] extends object ? Flatten<T[K], `${Prefix}${K}.`> : `${Prefix}${K}`;
+}[keyof T & string];
+
+// Define TranslationKey as the flattened keys of the translation resources
+type TranslationKey = Flatten<typeof resources.en.translation>;
 
 export default TranslationKey;
