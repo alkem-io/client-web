cc719970bfe9f9c8dda652541f4546971fbf2941
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2025-05-13 15:42:05 +0300

    more fixes: - skip unneded space tabs requests - close invitations modal on accept - show the right about page for private subspaces - cache URL resolver result in order to save rerenders

--- src/domain/space/layout/useSectionIndex.tsx
+++ src/domain/space/layout/useSectionIndex.tsx
@@ -2,11 +2,17 @@ import { useSearchParams } from 'react-router-dom';
 import useSpaceTabs from './tabbedLayout/layout/useSpaceTabs';
 import { SpaceLevel } from '@/core/apollo/generated/graphql-schema';
 import { TabbedLayoutParams } from '@/main/routing/urlBuilders';
+import { useSpace } from '../context/useSpace';
 
 export const useSectionIndex = ({ spaceId, spaceLevel }: { spaceId?: string; spaceLevel?: SpaceLevel }) => {
   const [searchParams] = useSearchParams();
 
-  const { defaultTabIndex } = useSpaceTabs({ spaceId: spaceId, skip: !spaceId || spaceLevel !== SpaceLevel.L0 });
+  const { permissions } = useSpace();
+
+  const { defaultTabIndex } = useSpaceTabs({
+    spaceId: spaceId,
+    skip: !spaceId || spaceLevel !== SpaceLevel.L0 || !permissions.canRead,
+  });
 
   let sectionIndex = searchParams.get(TabbedLayoutParams.Section);
   if (!sectionIndex) {
