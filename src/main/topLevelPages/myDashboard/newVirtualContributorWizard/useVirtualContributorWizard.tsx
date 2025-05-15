8f577552f205ad1d92ca9855338aa227e34c84cc
Bobby Kolev <bobbykolev@abv.bg>
2025-05-15 16:38:49 +0300

    show only communities with read, not readAbout (#8200)

--- src/main/topLevelPages/myDashboard/newVirtualContributorWizard/useVirtualContributorWizard.tsx
+++ src/main/topLevelPages/myDashboard/newVirtualContributorWizard/useVirtualContributorWizard.tsx
@@ -130,8 +130,8 @@ const useVirtualContributorWizard = (): useVirtualContributorWizardProvided => {
     fetchPolicy: 'cache-and-network',
   });
 
-  const hasReadAboutPrivilege = (space: SelectableSpace) => {
-    return space.authorization?.myPrivileges?.includes(AuthorizationPrivilege.ReadAbout);
+  const hasReadPrivilege = (space: SelectableSpace) => {
+    return space.authorization?.myPrivileges?.includes(AuthorizationPrivilege.Read);
   };
 
   const { myAccountId, allAccountSpaces, availableSpaces } = useMemo(() => {
@@ -141,7 +141,7 @@ const useVirtualContributorWizard = (): useVirtualContributorWizardProvided => {
     return {
       myAccountId: account?.id,
       allAccountSpaces: accountSpaces,
-      availableSpaces: accountSpaces.filter(hasReadAboutPrivilege),
+      availableSpaces: accountSpaces.filter(hasReadPrivilege),
     };
   }, [data, userModel, targetAccount]);
 
@@ -158,7 +158,7 @@ const useVirtualContributorWizard = (): useVirtualContributorWizardProvided => {
           spaceId: space.id,
         },
       });
-      const availableSubspaces = subspaceData?.data?.lookup.space?.subspaces?.filter(hasReadAboutPrivilege) ?? [];
+      const availableSubspaces = subspaceData?.data?.lookup.space?.subspaces?.filter(hasReadPrivilege) ?? [];
 
       result.push({
         ...space,
