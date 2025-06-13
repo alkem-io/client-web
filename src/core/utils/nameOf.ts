ffbf5a010f8b91393b5ddd698d1557ed277b3c2f
Carlos Cano <carlos@alkem.io>
2025-06-13 19:09:55 +0200

    repurposed nameOf clause

--- src/core/utils/nameOf.ts
+++ src/core/utils/nameOf.ts
@@ -1,19 +1,13 @@
-// const nameOf = <T>(name: Extract<keyof T, string>): string => name;
-/***
- * Unsupported in IE
- * https://stackoverflow.com/a/63891494/11207901
- */
-const nameOf = <T>() => {
-  return new Proxy(
-    {},
-    {
-      get: function (_target, prop) {
-        return prop;
-      },
-    }
-  ) as {
-    [P in keyof T]: P;
-  };
-};
+// Helper type: recursively builds all valid property paths as strings
+type PathImpl<T, Key extends keyof T> = Key extends string
+  ? T[Key] extends Record<string, unknown>
+    ? `${Key}` | `${Key}.${PathImpl<T[Key], Exclude<keyof T[Key], keyof unknown[]>>}`
+    : `${Key}`
+  : never;
 
-export default nameOf;
+type Path<T> = PathImpl<T, keyof T>;
+
+// Usage: nameOf<YourType>('property.subProperty')
+export function nameOf<T>(path: Path<T>): Path<T> {
+  return path;
+}
