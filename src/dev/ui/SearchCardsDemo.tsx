47bdc402f3da73082f66c57eaf17a454d332ef5a
bobbykolev <bobbykolev@abv.bg>
2025-07-03 16:06:24 +0300

    fix lint error

--- src/dev/ui/SearchCardsDemo.tsx
+++ src/dev/ui/SearchCardsDemo.tsx
@@ -9,6 +9,7 @@ import CalloutCard, { CalloutCardCallout } from '@/domain/collaboration/callout/
 import SearchResultsCalloutCardFooter, {
   SearchResultsCalloutCardFooterProps,
 } from '@/main/search/searchResults/searchResultsCallout/SearchResultsCalloutCardFooter';
+import { SpaceLevel } from '@/core/apollo/generated/graphql-schema';
 
 const loremIpsum =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
