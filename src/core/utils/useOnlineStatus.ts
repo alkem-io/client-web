aaaa29ef59322d79ebc9be9bdb43e9e03abd7ecb
Bobby Kolev <bobbykolev@abv.bg>
2025-08-08 16:43:17 +0300

    Memo events handling + (#8571)
    * WiP improve the handling of collaboration events, the readonly logic and disabled states
    * Handle the Synchronization gracefully. Updated readOnly and overall footer messaging.
    * correct storagebucketId
    * Hide the saveAsTemplate option for memos.
    * clean up logs
    * Offline check to prevent edits.
    ---------
    
    Co-authored-by: hero101 <svetoslav@alkem.io>

---
+++ src/core/utils/useOnlineStatus.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+/**
+ * A custom hook that detects and returns the online status of the browser.
+ *
+ * @returns {boolean} - True if the browser is online, false otherwise.
+ */
+export const useOnlineStatus = (): boolean => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  return isOnline;
+};
