21bf8359cc94574dae69ed1aa38b912c00364f63
Bobby Kolev <bobbykolev@abv.bg>
2025-08-13 14:01:30 +0300

    Simplify the DialogWithGrid height logic. Fix hidden content on mobile. (#8584)

--- src/core/ui/dialog/DialogWithGrid.tsx
+++ src/core/ui/dialog/DialogWithGrid.tsx
@@ -1,7 +1,7 @@
 import { MouseEventHandler, ReactNode } from 'react';
 import { Dialog as MuiDialog, DialogProps as MuiDialogProps, Paper, PaperProps } from '@mui/material';
 import GridContainer from '../grid/GridContainer';
-import { MAX_CONTENT_WIDTH_WITH_GUTTER_PX, useGlobalGridColumns, useScreenSize } from '../grid/constants';
+import { MAX_CONTENT_WIDTH_WITH_GUTTER_PX, useGlobalGridColumns } from '../grid/constants';
 import GridProvider from '../grid/GridProvider';
 import GridItem, { GridItemProps } from '../grid/GridItem';
 
@@ -40,6 +40,7 @@ const DialogContainer = ({
       alignItems={centeredVertically ? 'center' : 'start'}
       onClick={handleContainerClick}
       disablePadding={fullScreen}
+      sx={{ paddingY: fullScreen ? 0 : 4 }}
     >
       <GridProvider columns={containerColumns} force>
         <GridItem columns={columns}>
@@ -67,7 +68,6 @@ const DialogWithGrid = ({
   fullScreen,
   ...dialogProps
 }: DialogWithGridProps) => {
-  const { isSmallScreen } = useScreenSize();
   const { sx } = dialogProps;
 
   return (
@@ -93,16 +93,10 @@ const DialogWithGrid = ({
         '.MuiDialog-paper': {
           maxWidth: '100vw',
           margin: 0,
-          height: fullHeight ? '100%' : 'auto',
-          maxHeight:
-            fullScreen || isSmallScreen
-              ? '100vh'
-              : fullHeight
-                ? 'calc(100vh - 32px)' /* 64px for header height */
-                : '100%',
+          height: 'auto',
+          maxHeight: fullScreen ? '100vh' : '100%',
           ...(fullHeight && {
-            height: 'auto',
-            minHeight: 'auto', // Allows dialog to be smaller when content is minimal
+            minHeight: 'auto',
           }),
         },
         ...sx,
