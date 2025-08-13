0d84cb3d00c5aebc1363fa3ec54f7f4522859fb9
bobbykolev <bobbykolev@abv.bg>
2025-08-13 13:25:25 +0300

    Fix the dashboard layout on non-desktop, non-mobile res.

--- src/core/ui/content/ContentColumn.tsx
+++ src/core/ui/content/ContentColumn.tsx
@@ -1,8 +1,11 @@
 import PageContentColumn from './PageContentColumn';
 import { BoxProps } from '@mui/material';
+import { useColumns } from '../grid/GridContext';
+import { GRID_COLUMNS_MOBILE } from '../grid/constants';
 
 // GRID COLUMNS - INFO_COLUMNS => 12 - 3
 const CONTENT_COLUMNS = 9;
+const INFO_COLUMNS = 3;
 
 const ContentColumn = ({
   ref,
@@ -10,10 +13,24 @@ const ContentColumn = ({
   ...props
 }: BoxProps & {
   ref?: React.Ref<HTMLDivElement>;
-}) => (
-  <PageContentColumn columns={CONTENT_COLUMNS} {...props} ref={ref}>
-    {children}
-  </PageContentColumn>
-);
+}) => {
+  const availableColumns = useColumns();
+
+  let columnsToUse = CONTENT_COLUMNS;
+
+  if (availableColumns === GRID_COLUMNS_MOBILE) {
+    // On mobile, use full width
+    columnsToUse = GRID_COLUMNS_MOBILE;
+  } else if (availableColumns < 12) {
+    // On tablet (8 columns), use remaining columns after info column
+    columnsToUse = availableColumns - INFO_COLUMNS;
+  }
+
+  return (
+    <PageContentColumn columns={columnsToUse} {...props} ref={ref}>
+      {children}
+    </PageContentColumn>
+  );
+};
 
 export default ContentColumn;
