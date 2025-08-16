50b548a4cf5e67c7ccd09099af35a62df32e014d
Neil Smyth <neil@thesmyths.eu>
2025-08-16 09:32:37 +0100

    re-enabled in app notifications with right types

--- src/main/inAppNotifications/InAppNotificationItem.tsx
+++ src/main/inAppNotifications/InAppNotificationItem.tsx
@@ -1,18 +1,23 @@
 import { warn as logWarn, TagCategoryValues } from '@/core/logging/sentry/log';
 import { InAppNotificationModel } from './model/InAppNotificationModel';
+import { CollaborationCalloutPublishedView } from './views/CollaborationCalloutPublishedView';
+import { CommunicationUserMentionView } from './views/CommunicationUserMentionView';
+import { NotificationEvent } from '@/core/apollo/generated/graphql-schema';
+import { CommunityNewMemberAdminView } from './views/CommunityNewMemberAdminView';
+import { CommunityNewMemberView } from './views/CommunityNewMemberView';
 
 // TODO:NotificationEventType fix
 export const InAppNotificationItem = ({ ...item }: InAppNotificationModel) => {
   switch (item.type) {
-    // case NotificationEventType.CollaborationCalloutPublished:
-    //   return <CollaborationCalloutPublishedView {...item} />;
-    // case NotificationEventType.CommunicationUserMention:
-    //   return <CommunicationUserMentionView {...item} />;
-    // case NotificationEventType.CommunityNewMember:
-    //   if (item.category === InAppNotificationCategory.Admin) {
-    //     return <CommunityNewMemberAdminView {...item} />;
-    //   }
-    //   return <CommunityNewMemberView {...item} />;
+    case NotificationEvent.SpaceCollaborationCalloutPublished:
+      return <CollaborationCalloutPublishedView {...item} />;
+    case NotificationEvent.UserMention:
+      return <CommunicationUserMentionView {...item} />;
+    case NotificationEvent.SpaceCommunityNewMember:
+      return <CommunityNewMemberView {...item} />;
+    case NotificationEvent.SpaceCommunityNewMemberAdmin:
+      return <CommunityNewMemberAdminView {...item} />;
+
     default:
       logWarn(`Unsupported Notification type: ${item.type}`, {
         category: TagCategoryValues.NOTIFICATIONS,
