b37eadc560eb1470db9d44be15235aeb7534e966
Svetoslav <svetoslav@alkem.io>
2025-08-26 18:27:42 +0300

    added more styling to markdown

--- src/domain/collaboration/memo/MemoPreview/MemoPreview.tsx
+++ src/domain/collaboration/memo/MemoPreview/MemoPreview.tsx
@@ -4,6 +4,9 @@ import { useTranslation } from 'react-i18next';
 import { MouseEventHandler } from 'react';
 import CroppedMarkdown from '@/core/ui/markdown/CroppedMarkdown';
 import { gutters } from '@/core/ui/grid/utils';
+import { Box } from '@mui/material';
+import { alpha, styled } from '@mui/material';
+import { Caption } from '@/core/ui/typography';
 
 type MemoPreviewProps = {
   displayName?: string;
@@ -28,26 +31,39 @@ const MemoPreview = ({ displayName, memo, onClick }: MemoPreviewProps) => {
       />
     );
   } else {
+    // add quote styling at the start - avoid having double quote blocks
+    const quotedMarkdown = memo.markdown.replace(/^(?!>)|^>>/gm, '> ');
     return (
-      <CroppedMarkdown
-        backgroundColor="paper"
-        maxHeightGutters={10}
-        minHeightGutters={10}
-        sx={{ cursor: 'pointer' }}
-        containerProps={{
-          marginX: gutters(1),
-          onClick: onClick,
-        }}
-        // overflowMarker={
-        //   <CaptionContainer>
-        //     <Caption sx={{ color: theme => theme.palette.primary.main }}>{t('callout.memo.clickToSee')}</Caption>
-        //   </CaptionContainer>
-        // }
-      >
-        {memo.markdown}
-      </CroppedMarkdown>
+      <Box onClick={onClick} sx={{ cursor: onClick ? 'pointer' : 'default' }}>
+        <CroppedMarkdown
+          backgroundColor="paper"
+          maxHeightGutters={10}
+          minHeightGutters={10}
+          sx={{ cursor: 'pointer' }}
+          containerProps={{
+            marginX: gutters(1),
+          }}
+          overflowMarker={
+            <CaptionContainer>
+              <Caption sx={{ color: theme => theme.palette.primary.main }}>{t('callout.memo.clickToSee')}</Caption>
+            </CaptionContainer>
+          }
+        >
+          {quotedMarkdown}
+        </CroppedMarkdown>
+      </Box>
     );
   }
 };
 
 export default MemoPreview;
+
+const CaptionContainer = styled(Box)(({ theme }) => ({
+  position: 'absolute',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: gutters(2)(theme),
+  backgroundColor: alpha(theme.palette.common.white, 0.8),
+}));
