f4891d41fdd61db2b19a0cbb1a834f5fce0f750c
Neil Smyth <neil@thesmyths.eu>
2025-08-29 17:08:42 +0200

    tidy up naming + add one new event handling

--- src/main/inAppNotifications/InAppNotificationItem.tsx
+++ src/main/inAppNotifications/InAppNotificationItem.tsx
@@ -3,19 +3,19 @@ import { InAppNotificationModel } from './model/InAppNotificationModel';
 import { InAppSpaceCollaborationCalloutPublishedView } from './views/InAppSpaceCollaborationCalloutPublishedView';
 import { InAppUserMentionView } from './views/InAppUserMentionView';
 import { NotificationEvent } from '@/core/apollo/generated/graphql-schema';
-import { InAppSpaceCommunityNewMemberAdminView } from './views/InAppSpaceCommunityNewMemberAdminView';
-import { InAppSpaceCommunityNewMemberView } from './views/InAppSpaceCommunityNewMemberView';
+import { InAppSpaceCommunityNewMemberAdminView } from './views/InAppSpaceAdminCommunityNewMemberView';
+import { InAppUserSpaceCommunityJoinedView } from './views/InAppUserSpaceCommunityJoinedView';
 
 export const InAppNotificationItem = ({ ...item }: InAppNotificationModel) => {
   switch (item.type) {
+    case NotificationEvent.SpaceAdminCommunityNewMember:
+      return <InAppSpaceCommunityNewMemberAdminView {...item} />;
     case NotificationEvent.SpaceCollaborationCalloutPublished:
       return <InAppSpaceCollaborationCalloutPublishedView {...item} />;
     case NotificationEvent.UserMentioned:
       return <InAppUserMentionView {...item} />;
-    case NotificationEvent.SpaceAdminCommunityNewMember:
-      return <InAppSpaceCommunityNewMemberAdminView {...item} />;
     case NotificationEvent.UserSpaceCommunityJoined:
-      return <InAppSpaceCommunityNewMemberView {...item} />;
+      return <InAppUserSpaceCommunityJoinedView {...item} />;
 
     default:
       logWarn(`Unsupported Notification type: ${item.type}`, {
--- src/main/inAppNotifications/views/InAppUserSpaceCommunityJoinedView.tsx
+++ src/main/inAppNotifications/views/InAppUserSpaceCommunityJoinedView.tsx
@@ -1,7 +1,7 @@
 import { InAppNotificationModel } from '../model/InAppNotificationModel';
 import { InAppNotificationBaseView } from './InAppNotificationBaseView';
 
-export const InAppSpaceCommunityNewMemberView = (notification: InAppNotificationModel) => {
+export const InAppUserSpaceCommunityJoinedView = (notification: InAppNotificationModel) => {
   const { payload } = notification;
 
   // do not display notification if these are missing
