745fa49924918f0979a024fb8d278e1523bf8ab3
Carlos Cano <carlos@alkem.io>
2025-09-29 11:14:03 +0200

    navigate to cards

--- src/domain/collaboration/calloutContributions/calloutContributionPreview/CalloutContributionPreview.tsx
+++ src/domain/collaboration/calloutContributions/calloutContributionPreview/CalloutContributionPreview.tsx
@@ -13,15 +13,15 @@ import { contributionIcons } from '../../callout/icons/calloutIcons';
 import { CalloutDetailsModelExtended } from '../../callout/models/CalloutDetailsModel';
 import Loading from '@/core/ui/loading/Loading';
 import WhiteboardPreview from '@/domain/collaboration/whiteboard/WhiteboardPreview/WhiteboardPreview';
-import { useNavigate } from 'react-router-dom';
+import useNavigate from '@/core/routing/useNavigate';
 
 interface CalloutContributionPreviewProps {
   callout: CalloutDetailsModelExtended;
   contributionId: string;
-  onClose?: () => void;
+  // onClose?: () => void;
 }
 
-const CalloutContributionPreview = ({ callout, contributionId, onClose }: CalloutContributionPreviewProps) => {
+const CalloutContributionPreview = ({ callout, contributionId }: CalloutContributionPreviewProps) => {
   const theme = useTheme();
   const navigate = useNavigate();
   const { data, loading } = useCalloutContributionQuery({
@@ -30,63 +30,78 @@ const CalloutContributionPreview = ({ callout, contributionId, onClose }: Callou
       includeLink: callout.settings.contribution.allowedTypes.includes(CalloutContributionType.Link),
       includePost: callout.settings.contribution.allowedTypes.includes(CalloutContributionType.Post),
       includeWhiteboard: callout.settings.contribution.allowedTypes.includes(CalloutContributionType.Whiteboard),
-    }
+    },
   });
 
-  const contributionType = data?.lookup.contribution?.post ? CalloutContributionType.Post
-    : data?.lookup.contribution?.link ? CalloutContributionType.Link
-      : data?.lookup.contribution?.whiteboard ? CalloutContributionType.Whiteboard
+  const contributionType = data?.lookup.contribution?.post
+    ? CalloutContributionType.Post
+    : data?.lookup.contribution?.link
+      ? CalloutContributionType.Link
+      : data?.lookup.contribution?.whiteboard
+        ? CalloutContributionType.Whiteboard
         : undefined;
   const Icon = contributionType ? contributionIcons[contributionType] : undefined;
 
-  const displayName =
-    loading ? <Skeleton variant="text" width={gutters(10)(theme)} /> : (
-      (contributionType === CalloutContributionType.Post && data?.lookup.contribution?.post?.profile.displayName) ||
-      (contributionType === CalloutContributionType.Whiteboard && data?.lookup.contribution?.whiteboard?.profile.displayName));
+  const displayName = loading ? (
+    <Skeleton variant="text" width={gutters(10)(theme)} />
+  ) : (
+    (contributionType === CalloutContributionType.Post && data?.lookup.contribution?.post?.profile.displayName) ||
+    (contributionType === CalloutContributionType.Whiteboard &&
+      data?.lookup.contribution?.whiteboard?.profile.displayName)
+  );
 
-  const author = (contributionType === CalloutContributionType.Post && data?.lookup.contribution?.post?.createdBy?.profile.displayName) ||
-    (contributionType === CalloutContributionType.Whiteboard && data?.lookup.contribution?.whiteboard?.createdBy?.profile.displayName);
+  const author =
+    (contributionType === CalloutContributionType.Post &&
+      data?.lookup.contribution?.post?.createdBy?.profile.displayName) ||
+    (contributionType === CalloutContributionType.Whiteboard &&
+      data?.lookup.contribution?.whiteboard?.createdBy?.profile.displayName);
 
   const formattedCreatedDate =
-    (contributionType === CalloutContributionType.Post && data?.lookup.contribution?.post?.createdDate &&
+    (contributionType === CalloutContributionType.Post &&
+      data?.lookup.contribution?.post?.createdDate &&
       formatDateTime(data.lookup.contribution.post.createdDate)) ||
-    (contributionType === CalloutContributionType.Whiteboard && data?.lookup.contribution?.whiteboard?.createdDate &&
+    (contributionType === CalloutContributionType.Whiteboard &&
+      data?.lookup.contribution?.whiteboard?.createdDate &&
       formatDateTime(data.lookup.contribution.whiteboard.createdDate));
-
 
   return (
     <Gutters>
       <PageContentBlock>
-        <PageContentBlockHeaderCardLike icon={Icon} title={displayName} subtitle={author} actions={
-          <>
-            <Caption>{formattedCreatedDate}</Caption>
-            {/* TODO: Here the comments to the post */}
-            <IconButton onClick={onClose}>
-              <CloseOutlined />
-            </IconButton>
-          </>
-        }
+        <PageContentBlockHeaderCardLike
+          icon={Icon}
+          title={displayName}
+          subtitle={author}
+          actions={
+            <>
+              <Caption>{formattedCreatedDate}</Caption>
+              {/* TODO: Here the comments to the post */}
+              <IconButton onClick={() => navigate(callout.framing.profile.url)}>
+                <CloseOutlined />
+              </IconButton>
+            </>
+          }
         />
         {loading && <Loading />}
-        {contributionType === CalloutContributionType.Post &&
+        {contributionType === CalloutContributionType.Post && (
           <Box bgcolor={theme => theme.palette.background.default} padding={gutters()} margin={gutters(-1)}>
             <WrapperMarkdown>{data?.lookup.contribution?.post?.profile.description ?? ''}</WrapperMarkdown>
           </Box>
-        }
-        {contributionType === CalloutContributionType.Whiteboard &&
+        )}
+        {contributionType === CalloutContributionType.Whiteboard && (
           <Box padding={gutters()} margin={gutters(-1)}>
             <WhiteboardPreview
               whiteboard={data?.lookup.contribution?.whiteboard}
-              onClick={() => data?.lookup.contribution?.whiteboard?.profile.url && navigate(data?.lookup.contribution?.whiteboard?.profile.url)}  //!! this is not correct
+              onClick={() =>
+                data?.lookup.contribution?.whiteboard?.profile.url &&
+                navigate(data?.lookup.contribution?.whiteboard?.profile.url)
+              } //!! this is not correct
               onClose={onClose}
             />
           </Box>
-        }
-
+        )}
       </PageContentBlock>
     </Gutters>
   );
-}
+};
 
 export default CalloutContributionPreview;
-
