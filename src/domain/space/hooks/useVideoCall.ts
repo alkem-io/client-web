ac113a54d0d0f87ef0424bf8462ca833f0602769
polibb <p.boneva17@gmail.com>
2025-10-01 17:56:20 +0300

    get space settings only with specific privileges, defaults to READ

--- src/domain/space/hooks/useVideoCall.ts
+++ src/domain/space/hooks/useVideoCall.ts
@@ -1,15 +1,23 @@
-import { useSpaceSettingsQuery } from '@/core/apollo/generated/apollo-hooks';
+import { usePlatformLevelAuthorizationQuery, useSpaceSettingsQuery } from '@/core/apollo/generated/apollo-hooks';
 import { defaultSpaceSettings } from '../../spaceAdmin/SpaceAdminSettings/SpaceDefaultSettings';
+import { AuthorizationPrivilege } from '@/core/apollo/generated/graphql-schema';
 
-export const useVideoCall = (spaceId?: string) => {
-  const { data: spaceSettings, loading } = useSpaceSettingsQuery({
+export const useVideoCall = (spaceId?: string, requiredPrivilege = AuthorizationPrivilege.Read) => {
+  const { data, loading: loadingAuthorizationPrivileges } = usePlatformLevelAuthorizationQuery();
+
+  const hasRequiredPrivilege = data?.platform?.authorization?.myPrivileges?.includes(requiredPrivilege);
+
+  const { data: spaceSettings, loading: loadingSpaceSettings } = useSpaceSettingsQuery({
     variables: { spaceId: spaceId || '' },
-    skip: !spaceId,
+    skip: !spaceId || !hasRequiredPrivilege || loadingAuthorizationPrivileges,
   });
 
   const allowMembersToVideoCall =
     spaceSettings?.lookup?.space?.settings.collaboration.allowMembersToVideoCall ??
     defaultSpaceSettings.collaboration.allowMembersToVideoCall;
 
-  return { isVideoCallEnabled: allowMembersToVideoCall, loading };
+  return {
+    isVideoCallEnabled: allowMembersToVideoCall,
+    loading: loadingSpaceSettings || loadingAuthorizationPrivileges,
+  };
 };
