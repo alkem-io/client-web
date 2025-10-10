18acb16955d15707e82207ec04e811311f12d659
Svetoslav <svetoslav@alkem.io>
2025-10-10 18:06:44 +0300

    deleting deprecated components

--- src/domain/platformAdmin/domain/space/AdminSpaceListPage/index.ts
+++ src/domain/platformAdmin/domain/space/AdminSpaceListPage/index.ts
@@ -1,5 +1,4 @@
 // New optimized V2 components with lazy loading
-export { default as AdminSpacesPageV2 } from './AdminSpacesPage';
 export { default as SpaceListV2 } from './SpaceListV2';
 export { default as SpaceListItemV2 } from './SpaceListItemV2';
 export { default as ManageLicensePlansDialog } from './ManageLicensePlansDialog';
