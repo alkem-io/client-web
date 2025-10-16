c71e8baab4965457d9194cb503cb4bc7a795bbad
Carlos Cano <carlos@alkem.io>
2025-10-16 14:08:25 +0200

    add comment

--- src/domain/collaboration/calloutContributions/link/models/LinkContribution.ts
+++ src/domain/collaboration/calloutContributions/link/models/LinkContribution.ts
@@ -3,6 +3,9 @@ import { LinkDetails } from './LinkDetails';
 import type { Authorizable } from '@/core/utils/Authorizable';
 
 export interface LinkContribution extends Identifiable {
-  link?: LinkDetails & Identifiable & Authorizable;
+  link?: Identifiable & // Makes id mandatory, in LinkDetails is optional
+    Authorizable & // Adds { authorization }
+    LinkDetails;
+
   sortOrder: number;
 }
