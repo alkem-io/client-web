ab15a18b0b2200757f46c4d4fc9045a3143e7dd5
bobbykolev <bobbykolev@abv.bg>
2025-11-07 11:39:16 +0200

    Deletion of memo contributions.

--- src/domain/collaboration/calloutContributions/memo/CalloutContributionDialogMemo.tsx
+++ src/domain/collaboration/calloutContributions/memo/CalloutContributionDialogMemo.tsx
@@ -1,5 +1,7 @@
 import { CalloutContributionPreviewDialogProps } from '../interfaces/CalloutContributionPreviewDialogProps';
 import MemoDialog from '@/domain/collaboration/memo/MemoDialog/MemoDialog';
+import { useDeleteContributionMutation } from '@/core/apollo/generated/apollo-hooks';
+import useEnsurePresence from '@/core/utils/ensurePresence';
 
 export interface CalloutContributionDialogMemoProps extends CalloutContributionPreviewDialogProps {}
 
@@ -8,12 +10,40 @@ const CalloutContributionDialogMemo = ({
   onClose,
   contribution,
   calloutId,
+  onContributionDeleted,
 }: CalloutContributionDialogMemoProps) => {
+  const ensurePresence = useEnsurePresence();
+
+  const [deleteContribution] = useDeleteContributionMutation();
+
+  const handleMemoDeleted = async () => {
+    const contributionId = ensurePresence(contribution?.id, 'ContributionId');
+    await deleteContribution({
+      variables: {
+        contributionId,
+      },
+      awaitRefetchQueries: true,
+      refetchQueries: ['CalloutDetails', 'CalloutContributions'],
+      onCompleted: data => {
+        onContributionDeleted(data.deleteContribution.id);
+      },
+    });
+  };
+
   if (!open || !contribution?.memo || !calloutId) {
     return null;
   }
 
-  return <MemoDialog open={open} onClose={onClose} memoId={contribution.memo.id} calloutId={calloutId} />;
+  return (
+    <MemoDialog
+      open={open}
+      onClose={onClose}
+      memoId={contribution.memo.id}
+      calloutId={calloutId}
+      onDelete={handleMemoDeleted}
+      isContribution
+    />
+  );
 };
 
 export default CalloutContributionDialogMemo;
