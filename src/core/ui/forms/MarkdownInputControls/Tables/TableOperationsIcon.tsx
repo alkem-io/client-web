2259c1641bbeac232d73f11566bd9770899ced22
Carlos Cano <carlos@alkem.io>
2025-11-25 08:34:01 +0100

    TableOperations icon

--- src/core/ui/forms/MarkdownInputControls/Tables/TableOperationsIcon.tsx
+++ src/core/ui/forms/MarkdownInputControls/Tables/TableOperationsIcon.tsx
@@ -1,11 +1,5 @@
-// import { SvgIcon, SvgIconProps } from '@mui/material';
-// import TableOperationsIconSvg from './TableOperationsIcon.svg?react';
-
 import { TableChart } from '@mui/icons-material';
-//!! Icon?
-// const TableOperationsIcon = (props: SvgIconProps) => (
-//   <SvgIcon component={TableOperationsIconSvg} inheritViewBox {...props} />
-// );
+
 const TableOperationsIcon = TableChart;
 TableOperationsIcon.muiName = 'TableOperationsIcon';
 
