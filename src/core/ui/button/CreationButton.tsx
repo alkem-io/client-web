65feb3c07858e5184f31bc4341c8f335405cb800
bobbykolev <bobbykolev@abv.bg>
2025-12-08 13:04:39 +0200

    pr fix

--- src/core/ui/button/CreationButton.tsx
+++ src/core/ui/button/CreationButton.tsx
@@ -7,7 +7,7 @@ const CreationButton = ({
   disabled,
   buttonComponent,
 }: {
-  disabledTooltip: React.ReactNode;
+  disabledTooltip?: React.ReactNode;
   buttonComponent: React.ReactNode;
   disabled?: boolean;
 }) => {
