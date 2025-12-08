1f9cbc09347033549fa23b31e33d834ba3539fea
Carlos Cano <carlos@alkem.io>
2025-12-08 11:30:35 +0100

    Post process the whiteboard preview to add padding

--- src/domain/collaboration/whiteboard/WhiteboardVisuals/getWhiteboardPreviewImage.ts
+++ src/domain/collaboration/whiteboard/WhiteboardVisuals/getWhiteboardPreviewImage.ts
@@ -4,8 +4,7 @@ import type { exportToCanvas as ExcalidrawExportToCanvas } from '@alkemio/excali
 import { WhiteboardPreviewVisualDimensions } from './WhiteboardVisualsDimensions';
 import { error as logError } from '@/core/logging/sentry/log';
 import createFallbackWhiteboardPreview from './createFallbackWhiteboardPreview';
-
-const EXPORT_PADDING = 0.1; // 10% padding of the biggest dimension as padding
+import { padImage } from '@/core/utils/images/padImage';
 
 type ExcalidrawUtils = {
   exportToCanvas: typeof ExcalidrawExportToCanvas;
@@ -24,24 +23,6 @@ const getWhiteboardPreviewImage = async (
   const appState = excalidrawAPI.getAppState(),
     elements = excalidrawAPI.getSceneElements(),
     files = excalidrawAPI.getFiles();
-
-  // Calculate the bounding box of all elements
-  const exportPadding = (elements => {
-    if (elements.length === 0) {
-      return 0;
-    } else {
-      const { minX, minY, maxX, maxY } = elements.reduce(
-        (acc, element) => ({
-          minX: Math.min(acc.minX, element.x),
-          minY: Math.min(acc.minY, element.y),
-          maxX: Math.max(acc.maxX, element.x + element.width),
-          maxY: Math.max(acc.maxY, element.y + element.height),
-        }),
-        { minX: Infinity, minY: Infinity, maxX: -Infinity, maxY: -Infinity }
-      );
-      return Math.max(maxX - minX, maxY - minY) * EXPORT_PADDING;
-    }
-  })(elements);
 
   const getDimensions = (width: number, height: number) => {
     if (width <= WhiteboardPreviewVisualDimensions.minWidth || height <= WhiteboardPreviewVisualDimensions.minHeight) {
@@ -66,12 +47,16 @@ const getWhiteboardPreviewImage = async (
     elements,
     files: files ?? null,
     getDimensions,
-    exportPadding,
-  }).catch(error => {
-    errorGenerating = true;
-    logError(error);
-    return createFallbackWhiteboardPreview();
-  });
+    exportPadding: 10,
+  })
+    .then(canvas => {
+      return padImage(canvas, WhiteboardPreviewVisualDimensions.aspectRatio);
+    })
+    .catch(error => {
+      errorGenerating = true;
+      logError(error);
+      return createFallbackWhiteboardPreview();
+    });
 
   return {
     image: canvas,
