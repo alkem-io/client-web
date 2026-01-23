a5bd8f0c430d1ee0f8ffd24db290f0c682d8d457
bobbykolev <bobbykolev@abv.bg>
2026-01-23 16:25:41 +0200

    chat available to all authenticated users

--- src/main/userMessaging/UserMessagingContext.tsx
+++ src/main/userMessaging/UserMessagingContext.tsx
@@ -1,6 +1,5 @@
 import { useCurrentUserContext } from '@/domain/community/userCurrent/useCurrentUserContext';
 import { createContext, useState, useContext, ReactNode } from 'react';
-import { RoleName } from '@/core/apollo/generated/graphql-schema';
 
 interface UserMessagingContextProps {
   isEnabled: boolean;
@@ -29,14 +28,14 @@ const defaultState: UserMessagingContextProps = {
 const UserMessagingContext = createContext<UserMessagingContextProps>(defaultState);
 
 export const UserMessagingProvider = ({ children }: { children: ReactNode }) => {
-  const { userModel, platformRoles } = useCurrentUserContext();
+  const { userModel } = useCurrentUserContext();
   const [isOpen, setIsOpen] = useState(false);
   const [selectedConversationId, setSelectedConversationId] = useState<string | null>(null);
   const [selectedRoomId, setSelectedRoomId] = useState<string | null>(null);
   const [totalUnreadCount, setTotalUnreadCount] = useState(0);
 
-  // Feature is enabled only for BETA_TESTER role
-  const isEnabled = userModel?.id ? platformRoles.includes(RoleName.PlatformBetaTester) : false;
+  // Feature is enabled for all registered users
+  const isEnabled = !!userModel?.id;
 
   const handleSetIsOpen = (open: boolean) => {
     setIsOpen(open);
