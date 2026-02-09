7fe3e05d457c46cc9c36ad0a7a42e5efdb51506d
Carlos Cano <carlos@alkem.io>
2026-02-09 12:10:52 +0100

    Lazy load lightgallery

--- src/core/ui/gallery/MediaGallery.tsx
+++ src/core/ui/gallery/MediaGallery.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Box, styled } from '@mui/material';
 import { MediaGalleryItem } from './types';
 import { useTranslation } from 'react-i18next';
 
-import LightGallery from 'lightgallery/react';
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-thumbnail.css';
 import './lightgallery.css';
 
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
+
+const LightGallery = lazy(() => import('lightgallery/react').then(module => ({ default: module.default })));
 
 const GalleryWrapper = styled(Box)(({ theme }) => ({
   '& .custom-lightgallery': {
@@ -53,35 +54,37 @@ const MediaGallery = ({ title, items }: MediaGalleryProps) => {
 
   return (
     <GalleryWrapper>
-      <LightGallery
-        speed={500}
-        plugins={[lgThumbnail]}
-        mode="lg-fade"
-        elementClassNames="custom-lightgallery"
-        zoomFromOrigin
-        allowMediaOverlap={false}
-      >
-        {items.map(item => {
-          const isVideo = item.type === 'video';
-          const videoSource = isVideo
-            ? JSON.stringify({
-                source: [{ src: item.url, type: 'video/mp4' }],
-                attributes: { preload: false, controls: true },
-              })
-            : undefined;
+      <Suspense fallback={<Box sx={{ minHeight: '200px' }} />}>
+        <LightGallery
+          speed={500}
+          plugins={[lgThumbnail]}
+          mode="lg-fade"
+          elementClassNames="custom-lightgallery"
+          zoomFromOrigin
+          allowMediaOverlap={false}
+        >
+          {items.map(item => {
+            const isVideo = item.type === 'video';
+            const videoSource = isVideo
+              ? JSON.stringify({
+                  source: [{ src: item.url, type: 'video/mp4' }],
+                  attributes: { preload: false, controls: true },
+                })
+              : undefined;
 
-          return (
-            <GalleryItem key={item.id} data-src={isVideo ? undefined : item.url} data-video={videoSource}>
-              <img
-                src={item.thumbnailUrl || item.url}
-                alt={
-                  title || item.alt || item.title || t('components.callout-creation.framing.mediaGallery.galleryItem')
-                }
-              />
-            </GalleryItem>
-          );
-        })}
-      </LightGallery>
+            return (
+              <GalleryItem key={item.id} data-src={isVideo ? undefined : item.url} data-video={videoSource}>
+                <img
+                  src={item.thumbnailUrl || item.url}
+                  alt={
+                    title || item.alt || item.title || t('components.callout-creation.framing.mediaGallery.galleryItem')
+                  }
+                />
+              </GalleryItem>
+            );
+          })}
+        </LightGallery>
+      </Suspense>
     </GalleryWrapper>
   );
 };
