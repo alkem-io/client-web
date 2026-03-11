6158c59d5064307ea3f01dd9704c56a0c6592ff2
bobbykolev <bobbykolev@abv.bg>
2026-03-11 10:57:38 +0200

    robots.txt

--- vite.config.mjs
+++ vite.config.mjs
@@ -54,6 +54,7 @@ export default defineConfig({
               req.url === '/home' ||
               // the files below might not work - headersSent is true
               req.url?.startsWith('/meta.json') ||
+              req.url?.startsWith('/robots.txt') ||
               req.url?.startsWith('/env-config.js') ||
               (!req.url.includes('.') && !req.url.startsWith('/api/') && !req.url.startsWith('/@')));
 
